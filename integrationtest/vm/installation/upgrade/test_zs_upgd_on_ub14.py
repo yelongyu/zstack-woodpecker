39405564deb1cfb501575d363d5720c6193bcdbf
youyk <yongkang.you@gmail.com>
2015-07-13 14:05:43 +0800

    add 2 new test cases for zstack upgrading test on centos7 and ubuntu14

---
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_on_ub14.py
@@ -0,0 +1,45 @@
+'''
+
+@author: Youyk
+'''
+import os
+import tempfile
+import uuid
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+tmp_file = '/tmp/%s' % uuid.uuid1().get_hex()
+
+
+def test():
+    test_util.test_dsc('Create test vm to test zstack upgrade by -u on ubuntu system.')
+    image_name = os.environ.get('imageName_i_u14')
+    vm = test_stub.create_vlan_vm(image_name)
+    test_obj_dict.add_vm(vm)
+    vm.check()
+
+    vm_inv = vm.get_vm()
+    vm_ip = vm_inv.vmNics[0].ip
+    target_file = '/root/zstack-all-in-one.tgz'
+    test_stub.prepare_test_env(vm_inv, target_file)
+    ssh_cmd = 'ssh  -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm_ip
+    test_stub.copy_id_dsa(vm_inv, ssh_cmd, tmp_file)
+    test_stub.copy_id_dsa_pub(vm_inv)
+    test_stub.execute_all_install(ssh_cmd, target_file, tmp_file)
+    test_stub.check_installation(ssh_cmd, tmp_file)
+
+    test_stub.upgrade_zstack(ssh_cmd, target_file, tmp_file)
+    test_stub.check_installation(ssh_cmd, tmp_file)
+
+    os.system('rm -f %s' % tmp_file)
+    vm.destroy()
+    test_util.test_pass('ZStack upgrade Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    os.system('rm -f %s' % tmp_file)
+    test_lib.lib_error_cleanup(test_obj_dict)
