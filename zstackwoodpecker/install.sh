dd4f1d20373579248262618616ba78c2afab498e
youyk <yongkang.you@gmail.com>
2015-07-30 11:43:57 +0800

    use rm -rf to delete woodpecker old source and install new source.

--- zstackwoodpecker/install.sh
+++ zstackwoodpecker/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-virtualenv_file="/var/lib/zstack/virtualenv/woodpecker/bin/activate"
+woodpecker_virtualenv="/var/lib/zstack/virtualenv/woodpecker"
+virtualenv_file="${woodpecker_virtualenv}/bin/activate"
 if [ ! -f $virtualenv_file ];then
     echo "Not find virutalenv in /var/lib/zstack/virtualenv/woodpecker/. It should be created by virtualenv and install apibinding and zstacklib firstly. The easiest way is to run \`install_woodpecker_env.sh zstacklib.tar.gz apibinding.tar.gz\` or \`./zstest.py -b\`"
     exit 1
@@ -13,7 +14,8 @@ cd $root_dir
 rm -rf build dist zstackwoodpecker.egg-info zstackwoodpecker-*dev*/
 python setup.py sdist
 
-pip uninstall -y zstackwoodpecker
+#pip uninstall -y zstackwoodpecker
+rm -rf $woodpecker_virtualenv/usr/lib/python*/site-packages/zstackwoodpecker*
 pip install dist/*.tar.gz 
 cp zstack-woodpecker /bin/
 
