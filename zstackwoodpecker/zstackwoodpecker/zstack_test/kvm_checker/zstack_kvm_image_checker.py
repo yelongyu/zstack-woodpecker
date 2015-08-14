ebf74505d10f96d5784dcbab01f050038208ba25
youyk <yongkang.you@gmail.com>
2015-08-14 12:00:52 +0800

    do not check image file when image is deleted in ceph

--- zstackwoodpecker/zstackwoodpecker/zstack_test/kvm_checker/zstack_kvm_image_checker.py
+++ zstackwoodpecker/zstackwoodpecker/zstack_test/kvm_checker/zstack_kvm_image_checker.py
@@ -3,6 +3,7 @@ import sys
 import traceback
 
 import zstackwoodpecker.header.checker as checker_header
+import zstackwoodpecker.header.image as image_header
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
@@ -27,6 +28,11 @@ class zstack_kvm_image_file_checker(checker_header.TestChecker):
             image_url = backupStorages[0].installPath
             self.judge(test_lib.lib_check_file_exist(host, image_url))
         elif bs.type == inventory.CEPH_BACKUP_STORAGE_TYPE:
+            if self.test_obj.state == image_header.DELETED:
+                #https://github.com/zstackorg/zstack/issues/93#issuecomment-130935998
+                test_util.test_logger("skip ceph image delete check, since the image won't be deleted until no vms refer to it.")
+                return self.judge(self.exp_result)
+
             ceph_host, username, password = test_lib.lib_get_ceph_info(os.environ.get('cephBackupStorageMonUrls'))
             image_installPath = bs_one.installPath.split('ceph://')[1]
 
