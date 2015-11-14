ce5943e173cc292b7a9f992a968b1d3ecdd1bc15
youyk <yongkang.you@gmail.com>
2015-11-14 20:49:07 +0800

    fix a typ

--- zstackwoodpecker/zstackwoodpecker/test_lib.py
+++ zstackwoodpecker/zstackwoodpecker/test_lib.py
@@ -2309,7 +2309,7 @@ def lib_get_volume_host(volume):
                 primaryStorageUuid)
 
         ps_inv = res_ops.query_resource(res_ops.PRIMARY_STORAGE, conditions, None)[0]
-        if ps_inv.type == inventory.LOCAL_STORAGE_TYPE::
+        if ps_inv.type == inventory.LOCAL_STORAGE_TYPE:
            vm = volume.get_target_vm().get_vm()
            host = lib_get_vm_host(vm)
            return host
