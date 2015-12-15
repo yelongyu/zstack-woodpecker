8c2d6b1a44e54242487e49fc381c6192a5b7bda1
youyk <yongkang.you@gmail.com>
2015-12-15 22:10:19 +0800

    enable install monitor for test

--- zstackwoodpecker/zstackwoodpecker/setup_actions.py
+++ zstackwoodpecker/zstackwoodpecker/setup_actions.py
@@ -510,7 +510,7 @@ default one' % self.zstack_properties)
         #cmd = 'WEBSITE=localhost bash %s -f %s -r %s -a -z' % \
         #        (self.zstack_install_script, self.zstack_pkg, \
         #        self.install_path)
-        cmd = 'bash %s -z -r %s' % (self.zstack_pkg, self.install_path)
+        cmd = 'bash %s -z -r %s -m' % (self.zstack_pkg, self.install_path)
         if self.db_admin_password:
             cmd = '%s -P %s' % (cmd, self.db_admin_password)
         if self.db_password:
