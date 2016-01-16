48d6bd384ff48b421131a3a1dc4a149d4209dfde
youyk <yongkang.you@gmail.com>
2016-01-16 15:15:14 +0800

    recover basic/test_create_vm.py

--- integrationtest/vm/basic/test_create_vm.py
+++ integrationtest/vm/basic/test_create_vm.py
@@ -13,9 +13,9 @@ vm = None
 def test():
     global vm
     vm = test_stub.create_vm()
-    #vm.check()
-    #vm.destroy()
-    #vm.check()
+    vm.check()
+    vm.destroy()
+    vm.check()
     test_util.test_pass('Create VM Test Success')
 
 #Will be called only if exception happens in test().
