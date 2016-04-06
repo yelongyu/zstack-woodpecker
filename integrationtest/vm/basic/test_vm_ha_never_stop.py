efa51f3554e0aace8ecf3763f587c3d3032d7d50
quarkonics <quarkonics@gmail.com>
2016-04-06 22:48:10 +0800

    Add basic/test_vm_ha_never_stop.py

---
+++ integrationtest/vm/basic/test_vm_ha_never_stop.py
@@ -0,0 +1,33 @@
+'''
+
+New Integration Test for VM ha never stop operation
+
+@author: Youyk
+'''
+
+import zstackwoodpecker.test_util as test_util
+import test_stub
+import zstackwoodpecker.header.vm as vm_header
+import zstackwoodpecker.operations.ha_operations as ha_ops
+import time
+
+vm = None
+
+def test():
+    global vm
+    vm = test_stub.create_vm()
+    vm.check()
+    ha_ops.set_vm_instance_ha_level(vm.uuid, "NeverStop")
+    vm.stop()
+    time.sleep(60)
+    vm.set_state(test_vm_header.RUNNING)
+    vm.check()
+    vm.destroy()
+    vm.check()
+    test_util.test_pass('VM ha never stop Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    if vm:
+        vm.destroy()
