7cca2222218ba6944a789b6aae3d3408f96ac998
quarkonics <quarkonics@gmail.com>
2016-04-07 19:42:09 +0800

    Add basic/ha/test_vm_ha_never_stop_del_ha.py

---
+++ integrationtest/vm/basic/test_vm_ha_never_stop_del_ha.py
@@ -0,0 +1,36 @@
+'''
+
+New Integration Test for VM could stop after delete ha never stop
+
+@author: Youyk
+'''
+
+import zstackwoodpecker.test_util as test_util
+import test_stub
+import zstackwoodpecker.header.vm as vm_header
+import zstackwoodpecker.operations.ha_operations as ha_ops
+import time
+
+vm = None
+
+def test():
+    global vm
+    vm = test_stub.create_vm()
+    vm.check()
+    ha_ops.set_vm_instance_ha_level(vm.get_vm().uuid, "NeverStop")
+    vm.stop()
+    time.sleep(60)
+    vm.set_state(vm_header.RUNNING)
+    vm.check()
+    ha_ops.del_vm_instance_ha_level(vm.get_vm().uuid)
+    vm.stop()
+    vm.check()
+    vm.destroy()
+    vm.check()
+    test_util.test_pass('VM delete ha never stop Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    if vm:
+        vm.destroy()
