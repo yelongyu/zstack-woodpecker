805c1426987c03f565720bd1e75b1fd7ac91595c
quarkonics <quarkonics@gmail.com>
2016-04-15 13:08:44 +0800

    Only start VR VM if it's in stop state

--- integrationtest/vm/multihosts/ha/test_vm_ha_self_fence.py
+++ integrationtest/vm/multihosts/ha/test_vm_ha_self_fence.py
@@ -46,7 +46,8 @@ def test():
     l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
     vrs = test_lib.lib_find_vr_by_l3_uuid(l3_net_uuid)
     for vr in vrs:
-	vm_ops.start_vm(vr.uuid)
+	if test_lib.lib_is_vm_running(vr) == True:
+	    vm_ops.start_vm(vr.uuid)
     time.sleep(60)
 
     conditions = res_ops.gen_query_conditions('type', '=', 'UserVm')
