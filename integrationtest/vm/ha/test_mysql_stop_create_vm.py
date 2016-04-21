8634381963e3e8dfc67bae1142269e2d4f8a5b44
quarkonics <quarkonics@gmail.com>
2016-04-21 15:35:33 +0800

    Add ha/test_mysql_stop_create_vm.py

---
+++ integrationtest/vm/ha/test_mysql_stop_create_vm.py
@@ -0,0 +1,51 @@
+'''
+
+Integration Test for creating KVM VM in HA mode with mysql stop on one node.
+
+@author: Quarkonics
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.resource_operations as res_ops
+import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm_header
+import time
+import os
+
+vm = None
+node_ip = None
+
+def test():
+    global vm
+    global node_ip
+    node_ip = os.environ.get('node1Ip')
+    test_util.test_logger("stop mysql on node: %s" % (node_ip))
+    cmd = "service mysql stop"
+    host_username = os.environ.get('nodeUserName')
+    host_password = os.environ.get('nodePassword')
+    rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
+    test_util.test_logger("create vm to check if it still works")
+    vm.create()
+    #time.sleep(5)
+    vm.check()
+    vm.destroy()
+    cmd = "service mysql start"
+    rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
+
+    test_util.test_pass('Create VM Test Mysql Stop on one node Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    global node_ip
+    cmd = "service mysql start"
+    host_username = os.environ.get('nodeUserName')
+    host_password = os.environ.get('nodePassword')
+    rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
+
+    if vm:
+        try:
+            vm.destroy()
+        except:
+            pass
