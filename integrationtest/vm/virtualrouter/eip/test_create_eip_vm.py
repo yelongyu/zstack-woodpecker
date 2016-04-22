753c7cced95ef5e7e92f6b96f11146c50171bc2a
quarkonics <quarkonics@gmail.com>
2016-04-22 19:58:22 +0800

    Revert "In Eip test, use l3NoVlanNetworkName1 for pub and l3VlanNetworkName1 for pri"
    This reverts commit d7d8f8d383f44cc4af8f840bf8f4d3387ece98b8.

--- integrationtest/vm/virtualrouter/eip/test_create_eip_vm.py
+++ integrationtest/vm/virtualrouter/eip/test_create_eip_vm.py
@@ -23,14 +23,44 @@ def test():
     vm = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
     test_obj_dict.add_vm(vm)
     
-    l3_name = os.environ.get('l3NoVlanNetworkName1')
-    l3_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
-
     l3_name = os.environ.get('l3VlanNetworkName1')
-    pri_l3_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    vr1_l3_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    vrs = test_lib.lib_find_vr_by_l3_uuid(vr1_l3_uuid)
+    temp_vm1 = None
+    if not vrs:
+        #create temp_vm1 for getting vlan1's vr for test pf_vm portforwarding
+        temp_vm1 = test_stub.create_vlan_vm()
+        test_obj_dict.add_vm(temp_vm1)
+        vr1 = test_lib.lib_find_vr_by_vm(temp_vm1.vm)[0]
+    else:
+        vr1 = vrs[0]
+
+    l3_name = os.environ.get('l3NoVlanNetworkName1')
+    vr2_l3_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    vrs = test_lib.lib_find_vr_by_l3_uuid(vr2_l3_uuid)
+    temp_vm2 = None
+    if not vrs:
+        #create temp_vm2 for getting novlan's vr for test pf_vm portforwarding
+        temp_vm2 = test_stub.create_user_vlan_vm()
+        test_obj_dict.add_vm(temp_vm2)
+        vr2 = test_lib.lib_find_vr_by_vm(temp_vm2.vm)[0]
+    else:
+        vr2 = vrs[0]
+
+    #we do not need temp_vm1 and temp_vm2, since we just use their VRs.
+    if temp_vm1:
+        temp_vm1.destroy()
+        test_obj_dict.rm_vm(temp_vm1)
+    if temp_vm2:
+        temp_vm2.destroy()
+        test_obj_dict.rm_vm(temp_vm2)
 
     vm_nic = vm.vm.vmNics[0]
     vm_nic_uuid = vm_nic.uuid
+    pri_l3_uuid = vm_nic.l3NetworkUuid
+    vr = test_lib.lib_find_vr_by_l3_uuid(pri_l3_uuid)[0]
+    vr_pub_nic = test_lib.lib_find_vr_pub_nic(vr)
+    l3_uuid = vr_pub_nic.l3NetworkUuid
     vip = test_stub.create_vip('create_eip_test', l3_uuid)
     test_obj_dict.add_vip(vip)
     eip = test_stub.create_eip('create eip test', vip_uuid=vip.get_vip().uuid, vnic_uuid=vm_nic_uuid, vm_obj=vm)
