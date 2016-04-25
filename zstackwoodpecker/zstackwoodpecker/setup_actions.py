218fa1e150e8823b32a4acde6a4ff3ecec44e29e
youyk <yongkang.you@gmail.com>
2016-04-25 14:50:03 +0800

    update zstack lib version in woodpecker

--- zstackwoodpecker/zstackwoodpecker/setup_actions.py
+++ zstackwoodpecker/zstackwoodpecker/setup_actions.py
@@ -670,7 +670,7 @@ default one' % self.zstack_properties)
             ansible.check_and_install_ansible()
 
             lib_files = ['testagent/zstacktestagent-1.0.0.tar.gz', \
-                    'testagent/zstacklib-1.1.tar.gz' ]
+                    'testagent/zstacklib-1.2.tar.gz' ]
 
             if not target:
                 #default will deploy all test hosts.
@@ -679,7 +679,7 @@ default one' % self.zstack_properties)
                     print('Deploy test agent in host: [%s] \n' % h.managementIp_)
                     ansible_cmd_args = "host=%s \
                             pkg_testagent=zstacktestagent-1.0.0.tar.gz \
-                            pkg_zstacklib=zstacklib-1.1.tar.gz \
+                            pkg_zstacklib=zstacklib-1.2.tar.gz \
                             pypi_source_tar=pypi.tar.bz" % \
                             h.managementIp_
 
@@ -715,7 +715,7 @@ default one' % self.zstack_properties)
                 print('Deploy test agent in host: %s \n' % target.managementIp)
                 ansible_cmd_args = "host=%s \
                         pkg_testagent=zstacktestagent-1.0.0.tar.gz \
-                        pkg_zstacklib=zstacklib-1.1.tar.gz \
+                        pkg_zstacklib=zstacklib-1.2.tar.gz \
                         pypi_source_tar=pypi.tar.bz" % \
                         target.managementIp
                 if ENV_HTTP_PROXY:
