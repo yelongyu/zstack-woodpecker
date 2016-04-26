3689ce44e41f39310dcbf2667c3e75aa6d2a4d3b
quarkonics <quarkonics@gmail.com>
2016-04-26 13:24:28 +0800

    Update some ha tests

--- integrationtest/vm/ha/test_mysql_stop_create_vm.py
+++ integrationtest/vm/ha/test_mysql_stop_create_vm.py
@@ -14,40 +14,53 @@ import test_stub
 import time
 import os
 
-vm = None
+vm1 = None
+vm2 = None
 node_ip = None
 
 def test():
-    global vm
+    global vm1
+    global vm2
     global node_ip
-    vm = test_stub.create_vm()
+    vm1 = test_stub.create_basic_vm()
     node_ip = os.environ.get('node1Ip')
     test_util.test_logger("stop mysql on node: %s" % (node_ip))
     cmd = "service mysql stop"
     host_username = os.environ.get('nodeUserName')
     host_password = os.environ.get('nodePassword')
     rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
+    time.sleep(60)
     test_util.test_logger("create vm to check if it still works")
-    vm.create()
-    #time.sleep(5)
-    vm.check()
-    vm.destroy()
+    vm1.check()
+    vm1.destroy()
+    vm2 = test_stub.create_basic_vm()
+    vm2.check()
+    vm2.destroy()
     cmd = "service mysql start"
     rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
+    time.sleep(60)
 
     test_util.test_pass('Create VM Test Mysql Stop on one node Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
-    global vm
+    global vm1
+    global vm2
     global node_ip
     cmd = "service mysql start"
     host_username = os.environ.get('nodeUserName')
     host_password = os.environ.get('nodePassword')
     rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
+    time.sleep(60)
 
-    if vm:
+    if vm1:
         try:
-            vm.destroy()
+            vm1.destroy()
+        except:
+            pass
+
+    if vm2:
+        try:
+            vm2.destroy()
         except:
             pass
