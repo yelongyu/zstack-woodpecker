2ec6f433e6f2eefc1a9c4177e9150606dd2b454e
quarkonics <quarkonics@gmail.com>
2016-04-26 15:15:29 +0800

    Wait for more time for node and host recovery

--- integrationtest/vm/ha/test_one_node_recovery_create_vm.py
+++ integrationtest/vm/ha/test_one_node_recovery_create_vm.py
@@ -10,6 +10,7 @@ import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm_header
+import test_stub
 import time
 import os
 
@@ -26,25 +27,11 @@ def test():
     host_password = os.environ.get('nodePassword')
     rsp = test_lib.lib_execute_ssh_cmd(node_ip, host_username, host_password, cmd, 180)
     test_util.test_logger("wait for 2 minutes to see if http api still works well")
-    time.sleep(120)
-    image_name = os.environ.get('imageName_s')
-    image_uuid = test_lib.lib_get_image_by_name(image_name).uuid
-    l3_name = os.environ.get('l3VlanNetworkName1')
-    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
-    conditions = res_ops.gen_query_conditions('type', '=', 'UserVm')
-    instance_offering_uuid = res_ops.query_resource(res_ops.INSTANCE_OFFERING, conditions)[0].uuid
-    vm_creation_option = test_util.VmOption()
-    vm_creation_option.set_l3_uuids([l3_net_uuid])
-    vm_creation_option.set_image_uuid(image_uuid)
-    vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
-    vm_creation_option.set_name('ha_basic_vm')
-    vm = test_vm_header.ZstackTestVm()
-    vm.set_creation_option(vm_creation_option)
+    time.sleep(180)
 
     test_util.test_logger("recover node: %s, and create vm" % (node_ip))
     os.system('bash -ex %s %s' % (os.environ.get('nodeRecoverScript'), node_ip))
-    vm.create()
-    #time.sleep(5)
+    vm = test_stub.create_basic_vm()
     vm.check()
     vm.destroy()
 
