6de743a32cf989e13adaaed635c1f782620469f3
quarkonics <quarkonics@gmail.com>
2016-05-07 21:59:12 +0800

    Dont do force clean up when using existing management node for installation test

--- integrationtest/vm/installation/suite_teardown.py
+++ integrationtest/vm/installation/suite_teardown.py
@@ -7,13 +7,15 @@ Integration Test Teardown case
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.clean_util as clean_util
 import zstackwoodpecker.test_lib as test_lib
+import os
 
 def test():
-    clean_util.cleanup_all_vms_violently()
-    clean_util.cleanup_none_vm_volumes_violently()
-    clean_util.umount_all_primary_storages_violently()
-    clean_util.cleanup_backup_storage()
-    test_lib.setup_plan.stop_node()
+    if os.environ.get('zstackManagementIp') == None:
+        clean_util.cleanup_all_vms_violently()
+        clean_util.cleanup_none_vm_volumes_violently()
+        clean_util.umount_all_primary_storages_violently()
+        clean_util.cleanup_backup_storage()
+        test_lib.setup_plan.stop_node()
     
-    test_lib.lib_cleanup_host_ip_dict()
+        test_lib.lib_cleanup_host_ip_dict()
     test_util.test_pass('ZStack Installation Teardown Success')
