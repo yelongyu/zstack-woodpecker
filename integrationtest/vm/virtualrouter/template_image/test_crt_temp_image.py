72657eb51ecbe36c29d084829eb4fdcfc88025b6
quarkonics <quarkonics@gmail.com>
2016-05-25 17:40:26 +0800

    Add delay when running virtualrouter/template_image/test_crt_temp_image with ceph

--- integrationtest/vm/virtualrouter/template_image/test_crt_temp_image.py
+++ integrationtest/vm/virtualrouter/template_image/test_crt_temp_image.py
@@ -9,12 +9,18 @@ import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.zstack_test.zstack_test_image as test_image
 import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm
+import zstackwoodpecker.operations.config_operations as conf_ops
 
 test_stub = test_lib.lib_get_test_stub()
 #test_obj_dict is to track test resource. They will be cleanup if there will be any exception in testing.
 test_obj_dict = test_state.TestStateDict()
+origin_interval = None
+bs_type = None
 
 def test():
+    global origin_interval
+    global bs_type
+
     test_util.test_dsc('Create test vm and check')
     vm1 = test_stub.create_vlan_vm()
     #Without this checking, the image (created later) might be not able to get a DHCP IP, when using to create a new vm. 
@@ -26,6 +32,9 @@ def test():
     backup_storage_list = test_lib.lib_get_backup_storage_list_by_vm(vm1.vm)
     image_creation_option.set_backup_storage_uuid_list([backup_storage_list[0].uuid])
     image_creation_option.set_root_volume_uuid(vm1.vm.rootVolumeUuid)
+    bs_type = backup_storage_list[0].type
+    if bs_type == 'Ceph':
+        origin_interval = conf_ops.change_global_config('ceph', 'imageCache.cleanup.interval', '1')
 
     image = test_image.ZstackTestImage()
     image.set_creation_option(image_creation_option)
@@ -53,12 +62,22 @@ def test():
 
     vm1.destroy()
     image.delete()
+    if bs_type == 'Ceph':
+        time.sleep(60)
     image.check()
+
+    if bs_type == 'Ceph':
+        conf_ops.change_global_config('ceph', 'imageCache.cleanup.interval', origin_internal)
 
     test_util.test_pass('Create Image Template Test Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
     global test_obj_dict
+    global origin_interval
+    global bs_type
+    if bs_type == 'Ceph':
+        conf_ops.change_global_config('ceph', 'imageCache.cleanup.interval', origin_interval)
+
     test_lib.lib_error_cleanup(test_obj_dict)
 
