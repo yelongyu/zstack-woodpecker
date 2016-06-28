f4adc81821c3f38b8e3f0df70d2bcb05b5558291
quarkonics <quarkonics@gmail.com>
2016-06-28 11:17:54 +0800

    Add multihosts/migrate/test_migrate_vm_console_access.py

---
+++ integrationtest/vm/multihosts/migrate/test_migrate_vm_console_access.py
@@ -0,0 +1,41 @@
+'''
+
+New Integration test for testing console access after vm migration between hosts.
+
+@author: Quarkonics
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.account_operations as acc_ops
+
+vm = None
+test_stub = test_lib.lib_get_test_stub()
+
+def test():
+    global vm
+    vm = test_stub.create_vr_vm('migrate_vm', 'imageName_s', 'l3VlanNetwork2')
+    vm.check()
+
+    test_stub.migrate_vm_to_random_host(vm)
+
+    vm.check()
+    session_uuid = acc_ops.login_as_admin()
+    console = test_lib.lib_request_console_access(vm.get_vm().uuid, session_uuid)
+    if test_lib.lib_network_check(console.hostname, console.port):
+        test_util.test_logger('[vm:] %s console on %s:%s is connectable' % (vm.get_vm().uuid, console.hostname, console.port))
+    else:
+        test_util.test_fail('[vm:] %s console on %s:%s is not connectable' % (vm.get_vm().uuid, console.hostname, console.port))
+    acc_ops.logout(session_uuid)
+
+    vm.destroy()
+    test_util.test_pass('Migrate VM Console Access Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    if vm:
+        try:
+            vm.destroy()
+        except:
+            pass
