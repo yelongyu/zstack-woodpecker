916856291e34bf903d49457af51e6d5d12a0600d
youyk <yongkang.you@gmail.com>
2016-07-09 11:51:18 +0800

    add new test case to test 100 times image committment.

--- integrationtest/vm/image_stor/test_create_100_images.py
+++ integrationtest/vm/image_stor/test_create_100_images.py
@@ -15,9 +15,17 @@ import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm_header
 import zstackwoodpecker.zstack_test.zstack_test_image as test_image
 import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm
+import zstackwoodpecker.operations.config_operations as conf_ops
+
+_config_ = {
+        'timeout' : 2200,
+        'noparallel' : True
+        }
 
 test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
+default_snapshot_depth = None
+test_depth = 120
 
 def test():
     vm1 = test_stub.create_vm(vm_name = 'basic-test-vm')
@@ -32,34 +40,31 @@ def test():
     else:
         test_util.test_skip('Not find image store type backup storage.')
 
+    global default_snapshot_depth
+    default_snapshot_depth = conf_ops.change_global_config('volumeSnapshot', \
+            'incrementalSnapshot.maxNum', test_depth)
     image_creation_option.set_root_volume_uuid(vm1.vm.rootVolumeUuid)
-    image_creation_option.set_name('test_create_vim_images_vm3')
+    test_img_num = 1
+    while (test_img_num < 101):
+        image_creation_option.set_name('test_create_img_store_img_vm%d' % test_img_num)
     #image_creation_option.set_platform('Linux')
-    bs_type = backup_storage_list[0].type
-    if bs_type == 'Ceph':
-        origin_interval = conf_ops.change_global_config('ceph', 'imageCache.cleanup.interval', '1')
+        image = test_image.ZstackTestImage()
+        image.set_creation_option(image_creation_option)
+        image.create()
+        image.check()
+        test_obj_dict.add_image(image)
+        test_img_num += 1
 
-    image1 = test_image.ZstackTestImage()
-    image1.set_creation_option(image_creation_option)
-    image1.create()
-    image1.check()
-    test_obj_dict.add_image(image1)
-    vm2 = test_stub.create_vm(image_name = 'test_create_vim_images_vm3')
+    vm2 = test_stub.create_vm(image_name = 'test_create_img_store_img_vm100')
     test_obj_dict.add_vm(vm2)
-    image_creation_option.set_root_volume_uuid(vm2.vm.rootVolumeUuid)
-    image_creation_option.set_name('test_create_vim_images_vm4')
-    image2 = test_image.ZstackTestImage()
-    image2.set_creation_option(image_creation_option)
-    image2.create()
-    test_obj_dict.add_image(image2)
-    image2.check()
-    vm3 = test_stub.create_vm(image_name = 'test_create_vim_images_vm4')
-    test_obj_dict.add_vm(vm3)
     vm2.check()
-    vm3.check()
     test_lib.lib_robot_cleanup(test_obj_dict)
-    test_util.test_pass('Create VM Image in Image Store Success')
+    test_util.test_pass('Create 100 images Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
+    global default_snapshot_depth
+    conf_ops.change_global_config('volumeSnapshot', \
+            'incrementalSnapshot.maxNum', default_snapshot_depth)
+
     test_lib.lib_error_cleanup(test_obj_dict)
