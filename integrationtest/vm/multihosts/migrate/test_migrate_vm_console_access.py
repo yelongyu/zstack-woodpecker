dbfea263486ceb3f72c7ebb58dc48c06892cac5e
quarkonics <quarkonics@gmail.com>
2016-07-18 17:10:47 +0800

    Use GetVmConsoleAddress for testing in multihosts suite

--- integrationtest/vm/multihosts/migrate/test_migrate_vm_console_access.py
+++ integrationtest/vm/multihosts/migrate/test_migrate_vm_console_access.py
@@ -21,11 +21,11 @@ def test():
 
     vm.check()
     session_uuid = acc_ops.login_as_admin()
-    console = test_lib.lib_request_console_access(vm.get_vm().uuid, session_uuid)
-    if test_lib.lib_network_check(console.hostname, console.port):
-        test_util.test_logger('[vm:] %s console on %s:%s is connectable' % (vm.get_vm().uuid, console.hostname, console.port))
+    console = test_lib.lib_get_vm_console_address(vm.get_vm().uuid, session_uuid)
+    if test_lib.lib_network_check(console.hostIp, console.port):
+        test_util.test_logger('[vm:] %s console on %s:%s is connectable' % (vm.get_vm().uuid, console.hostIp, console.port))
     else:
-        test_util.test_fail('[vm:] %s console on %s:%s is not connectable' % (vm.get_vm().uuid, console.hostname, console.port))
+        test_util.test_fail('[vm:] %s console on %s:%s is not connectable' % (vm.get_vm().uuid, console.hostIp, console.port))
     acc_ops.logout(session_uuid)
 
     vm.destroy()
