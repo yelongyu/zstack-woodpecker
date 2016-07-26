fd7ab78814a9754e30ea847db2a1d3c707940018
quarkonics <quarkonics@gmail.com>
2016-07-26 15:11:44 +0800

    Skip multihosts/migrate/test_migrate_stopped_vm.py if not using localstorage

--- integrationtest/vm/multihosts/migrate/test_migrate_stopped_vm.py
+++ integrationtest/vm/multihosts/migrate/test_migrate_stopped_vm.py
@@ -8,13 +8,17 @@ New Integration test for testing stopped vm migration between hosts.
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.operations.volume_operations as vol_ops
+import apibinding.inventory as inventory
 
 vm = None
 test_stub = test_lib.lib_get_test_stub()
 
 def test():
     global vm
-    vm = test_stub.create_vr_vm('migrate_stopped_m', 'imageName_s', 'l3VlanNetwork2')
+    vm = test_stub.create_vr_vm('migrate_stopped_vm', 'imageName_s', 'l3VlanNetwork2')
+    ps = test_lib.lib_get_primary_storage_by_uuid(vm.get_vm().allVolumes[0].primaryStorageUuid)
+    if ps.type != inventory.LOCAL_STORAGE_TYPE:
+        test_util.test_skip('Skip test on non-localstorage')
     vm.check()
     target_host = test_lib.lib_find_random_host(vm.vm)
     vm.stop()
