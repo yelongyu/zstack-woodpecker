ad5e6e74e4c648bc9794b72b5f5b0c46599a0913
quarkonics <quarkonics@gmail.com>
2016-08-06 17:31:00 +0800

    Add update_scheduler() and fix one typo for delete_scheduler()

--- zstackwoodpecker/zstackwoodpecker/operations/scheduler_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/scheduler_operations.py
@@ -15,9 +15,25 @@ import os
 import inspect
 
 def delete_scheduler(uuid, session_uuid = None):
-    action = api_actions.DeleteAccountAction()
+    action = api_actions.DeleteSchedulerAction()
     action.uuid = uuid
     test_util.action_logger('Delete [Scheduler:] %s' % uuid)
     evt = account_operations.execute_action_with_session(action, session_uuid) 
     test_util.test_logger('[Scheduler:] %s is deleted.' % uuid)
+    return evt
+
+def update_scheduler(uuid, type, name, start_date=None, interval=None, repeatCount=None, cron=None, session_uuid = None):
+    action = api_actions.UpdateSchedulerAction()
+    action.uuid = uuid
+    action.schedulerType = type
+    action.schedulerName = name
+    action.startDate = startDate
+    action.schedulerInterval = interval
+    action.repeatCount = repeatCount
+    action.cronScheduler = cron
+
+    test_util.action_logger('Update [Scheduler:] %s [schdulerType:] % [schdulerName:] %s [startDate:] %s [interval:] %s [repeatCount:] %s [cron:] %s' \
+                    % (uuid, type, name, start_date, interval, repeatCount, cron))
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+    test_util.test_logger('[Scheduler:] %s is updated.' % uuid)
     return evt
