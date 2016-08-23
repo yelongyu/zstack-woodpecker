ad6362cd67e970ff3a4f3b97ce539606db592178
youyk <yongkang.you@gmail.com>
2016-08-23 14:55:23 +0800

    increase vm clone timeout to 1000s

--- zstackwoodpecker/zstackwoodpecker/operations/vm_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/vm_operations.py
@@ -211,7 +211,7 @@ def clone_vm(vm_uuid, vm_names, session_uuid = None):
     action = api_actions.CloneVmInstanceAction()
     action.vmInstanceUuid = vm_uuid
     action.names=vm_names
-    action.timeout = 300000
+    action.timeout = 1000000
     test_util.action_logger('Clone VM [uuid:] %s to %s' % (vm_uuid, vm_names))
     evt = account_operations.execute_action_with_session(action, session_uuid)
     test_util.test_logger('%s VMs have been cloned from %s' % (evt.result.numberOfClonedVm, vm_uuid))
