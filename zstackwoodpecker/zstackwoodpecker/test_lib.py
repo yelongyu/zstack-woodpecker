244e5ad9ed111e12e944cd6060026f8a0da31c7b
quarkonics <quarkonics@gmail.com>
2016-08-28 23:46:50 +0800

    Not care about return value

--- zstackwoodpecker/zstackwoodpecker/test_lib.py
+++ zstackwoodpecker/zstackwoodpecker/test_lib.py
@@ -4530,7 +4530,7 @@ def lib_count_in_local_management_server_log(timestamp, *keywords):
 
 def lib_find_in_remote_management_server_log(node_ip, node_username, node_password, timestamp, *keywords):
     datetime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
-    cmd = 'grep "%s" %s' % (datetime, lib_get_remote_management_server_log_path(node_ip, node_username, node_password).strip())
+    cmd = 'grep "%s" %s | cat' % (datetime, lib_get_remote_management_server_log_path(node_ip, node_username, node_password).strip())
     try:
         out = lib_execute_ssh_cmd(node_ip, node_username, node_password, cmd, 180)
     except:
