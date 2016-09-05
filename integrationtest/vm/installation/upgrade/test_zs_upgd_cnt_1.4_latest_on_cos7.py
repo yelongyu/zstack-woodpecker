0aa5d4adc0f1e98b0845ee0affd7b1497c8486f1
Meng Lai <bluestar3@yeah.net>
2016-09-05 15:29:48 +0800

    update installation upgrade cases,cnt(1.2, 1.3, 1.4, 1.5)

--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.4_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.4_latest_on_cos7.py
@@ -48,7 +48,7 @@ def test():
         test_util.test_dsc('Upgrade zstack to %s' % pkg_num)
         upgrade_target_file = '/root/zstack-upgrade-all-in-one.tgz'
         upgrade_pkg = os.environ.get('zstackPkg_%s' % pkg_num)
-        test_stub.prepare_upgrade_test_env(vm_inv, upgrade_target_file, install_pkg)
+        test_stub.prepare_upgrade_test_env(vm_inv, upgrade_target_file, upgrade_pkg)
         test_stub.upgrade_zstack(ssh_cmd, upgrade_target_file, tmp_file)
         test_stub.check_installation(ssh_cmd, tmp_file, vm_inv)
         pkg_num = pkg_num + 0.1
