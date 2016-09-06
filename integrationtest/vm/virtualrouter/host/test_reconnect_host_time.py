7420ba6437a304c598ed6b3183c1a938640e6484
Meng Lai <bluestar3@yeah.net>
2016-09-06 16:45:15 +0800

    update virtualrouter host case, reconnect host with x seconds

--- integrationtest/vm/virtualrouter/host/test_reconnect_host_time.py
+++ integrationtest/vm/virtualrouter/host/test_reconnect_host_time.py
@@ -20,7 +20,8 @@ test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
 
 def test():
-    test_util.test_dsc('Test Host Reconnect within 5 Seconds')
+    recnt_timeout=5000
+    test_util.test_dsc('Test Host Reconnect within %s ms' % recnt_timeout)
     vm = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
     test_obj_dict.add_vm(vm)
 
@@ -28,13 +29,12 @@ def test():
     host = test_lib.lib_get_vm_host(vm.get_vm())
     host_uuid = host.uuid
 
-    recnt_timeout=10000
     host_ops.reconnect_host(host_uuid, timeout=recnt_timeout)
    
     vm.destroy()
     test_obj_dict.rm_vm(vm)
 
-    test_util.test_pass('Reconnect Host within %s Seconds' % recnt_timeout)
+    test_util.test_pass('Reconnect Host within %s ms' % recnt_timeout)
 
 #Will be called only if exception happens in test().
 def error_cleanup():
