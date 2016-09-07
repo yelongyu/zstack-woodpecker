751a76b3c4913bcc44792f742562a052b6df1d26
Meng Lai <bluestar3@yeah.net>
2016-09-07 17:44:01 +0800

    update virtualrouter scheduler case, delete vm and check scheduler status

--- integrationtest/vm/virtualrouter/scheduler/test_delete_vm_check_simple_scheduler_state.py
+++ integrationtest/vm/virtualrouter/scheduler/test_delete_vm_check_simple_scheduler_state.py
@@ -11,29 +11,59 @@ import zstackwoodpecker.operations.vm_operations as vm_ops
 import zstackwoodpecker.operations.scheduler_operations as schd_ops
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.resource_operations as res_ops
+
+_config_ = {
+        'timeout' : 2000,
+        'noparallel' : True
+        }
+
 test_stub = test_lib.lib_get_test_stub()
 vm = None
 schd1 = None
 schd2 = None
 
-
 def test():
     global vm
     global schd1
     global schd2
+
+    delete_policy = test_lib.lib_get_delete_policy('vm')
     vm = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
+    vm.set_delete_policy('Delay')
+
     start_date = int(time.time())
     schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+60, 120)
     schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+120, 120)
     test_stub.sleep_util(start_date+58)
 
-    query_schd1 = schd_ops.query_scheduler(schd1.uuid)
-    test_util.test_dsc('schd1_status: %s' % schd1_status.state)
+    conditions = res_ops.gen_query_conditions('uuid', '=', schd1.uuid)
+    schd1_state = res_ops.query_resource(res_ops.SCHEDULER, conditions)[0].state
+    test_util.test_dsc('schd1_status: %s' % schd1_state)
+    if schd1_state != 'Enabled':
+         test_util.test_fail('check scheduler state, it is expected to be Enabled, but it is %s' % schd1_state)
+   
+    vm.destroy()
 
+    conditions = res_ops.gen_query_conditions('uuid', '=', schd1.uuid)
+    schd1_state = res_ops.query_resource(res_ops.SCHEDULER, conditions)[0].state
+    test_util.test_dsc('after destroy vm schd1_status: %s' % schd1_state)
+    if schd1_state != 'Disabled':
+         test_util.test_fail('check scheduler state, it is expected to be Disabled, but it is %s' % schd1_state)  
+
+    vm.recover()
+
+    conditions = res_ops.gen_query_conditions('uuid', '=', schd1.uuid)
+    schd1_state = res_ops.query_resource(res_ops.SCHEDULER, conditions)[0].state
+    test_util.test_dsc('after recover vm schd1_status: %s' % schd1_state)
+    if schd1_state != 'Enabled':
+         test_util.test_fail('check scheduler state, it is expected to be Enabled, but it is %s' % schd1_state)
+ 
     schd_ops.delete_scheduler(schd1.uuid)
     schd_ops.delete_scheduler(schd2.uuid)
     vm.destroy()
-    test_util.test_pass('Create Simple VM Stop Start Scheduler Success')
+    vm.set_delete_policy(delete_policy)
+    test_util.test_pass('Check Scheduler State after DSuccess')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
@@ -41,6 +71,7 @@ def error_cleanup():
     global schd1
     global schd2
 
+    vm.set_delete_policy(delete_policy)
     if vm:
         vm.destroy()
 
