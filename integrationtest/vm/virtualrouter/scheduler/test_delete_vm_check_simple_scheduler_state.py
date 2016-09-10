f23129095e8f4abf635390f471317217d1797658
Meng Lai <bluestar3@yeah.net>
2016-09-10 15:27:13 +0800

    update virtualrouter scheduler case, delete and recover vm, check scheduler state

--- integrationtest/vm/virtualrouter/scheduler/test_delete_vm_check_simple_scheduler_state.py
+++ integrationtest/vm/virtualrouter/scheduler/test_delete_vm_check_simple_scheduler_state.py
@@ -23,7 +23,7 @@ vm = None
 schd1 = None
 schd2 = None
 
-def check_scheduler_state(schd,target_state):
+def check_scheduler_state(schd, target_state):
     conditions = res_ops.gen_query_conditions('uuid', '=', schd.uuid)
     schd_state = res_ops.query_resource(res_ops.SCHEDULER, conditions)[0].state
     if schd_state != target_state:
@@ -32,10 +32,14 @@ def check_scheduler_state(schd,target_state):
     return True        
 
 def check_scheduler_msg(msg, timestamp):
-    if not test_lib.lib_find_in_local_management_server_log(timestamp, msg, vm.get_vm().uuid):
-        return False
+    msg_mismatch = 0
+    for i in range(0, 20):
+        if test_lib.lib_find_in_local_management_server_log(timestamp + i, msg, vm.get_vm().uuid):
+            msg_mismatch = 1
+            return True
 
-    return True
+    if msg_mismatch == 0:
+    	return False
 
 def test():
     global vm
@@ -47,59 +51,63 @@ def test():
     vm.set_delete_policy('Delay')
 
     start_date = int(time.time())
-    schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+60, 120)
-    schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+120, 120)
+    schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+10, 20)
+    schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+20, 20)
 
-    test_stub.sleep_util(start_date+125)
+    test_stub.sleep_util(start_date+45)
 
     test_util.test_dsc('check scheduler state after create scheduler')
     check_scheduler_state(schd1, 'Enabled')
     check_scheduler_state(schd2, 'Enabled')
-    if not check_scheduler_msg('[msg received]: {"org.zstack.header.vm.StopVmInstanceMsg', start_date+60):
-        test_util.test_fail('StopVmInstanceMsg not executed at expected timestamp %s' % start_date+60)
-    if not check_scheduler_msg('[msg received]: {"org.zstack.header.vm.StartVmInstanceMsg', start_date+120):
-        test_util.test_fail('StartVmInstanceMsg not executed at expected timestamp %s ' % start_date+120)
+    if not check_scheduler_msg('run scheduler for job: StopVmInstanceJob', start_date+10):
+        test_util.test_fail('StopVmInstanceJob not executed at expected timestamp range')
+    if not check_scheduler_msg('run scheduler for job: StartVmInstanceJob', start_date+20):
+        test_util.test_fail('StartVmInstanceJob not executed at expected timestamp range')
 
     vm.destroy()
-   
+    
+    test_stub.sleep_util(20)
+
     current_time = int(time.time())
-    except_start_time =  start_date + 120 * (((current_time - start_date) % 120) + 1) 
-    test_stub.sleep_util(except_start_time+5)
- 
+    except_start_time =  start_date + 20 * (((current_time - start_date) % 20) + 1)
+    test_stub.sleep_util(except_start_time + 45)
+
     test_util.test_dsc('check scheduler state after destroy vm')
     check_scheduler_state(schd1, 'Disabled')
     check_scheduler_state(schd2, 'Disabled')
-#    if check_scheduler_msg('[msg received]: {"org.zstack.header.vm.StopVmInstanceMsg', except_start_time+60):
-#        test_util.test_fail('StopVmInstanceMsg executed at unexpected timestamp %s' % except_start_time+60)
-#    if check_scheduler_msg('[msg received]: {"org.zstack.header.vm.StartVmInstanceMsg', except_start_time+120):
-#        test_util.test_fail('StartVmInstanceMsg executed at unexpected timestamp %s ' % except_start_time+120)
+    if check_scheduler_msg('run scheduler for job: StopVmInstanceJob', except_start_time+10):
+        test_util.test_fail('StopVmInstanceJob executed at unexpected timestamp range')
+    if check_scheduler_msg('run scheduler for job: StartVmInstanceJob', except_start_time+20):
+        test_util.test_fail('StartVmInstanceJob executed at unexpected timestamp range')
 
     vm.recover()
 
+    test_stub.sleep_util(20)
+
     current_time = int(time.time())
-    except_start_time =  start_date + 120 * (((current_time - start_date) % 120) + 1)
-    test_stub.sleep_util(except_start_time+5)
+    except_start_time =  start_date + 20 * (((current_time - start_date) % 20) + 1)
+    test_stub.sleep_util(except_start_time + 45)
 
     test_util.test_dsc('check scheduler state after recover vm')
     check_scheduler_state(schd1, 'Enabled')
     check_scheduler_state(schd2, 'Enabled')
-#    if not check_scheduler_msg('[msg received]: {"org.zstack.header.vm.StopVmInstanceMsg', except_start_time+60):
-#        test_util.test_fail('StopVmInstanceMsg not executed at expected timestamp %s' % except_start_time+60)
-#    if not check_scheduler_msg('[msg received]: {"org.zstack.header.vm.StartVmInstanceMsg', except_start_time+120):
-#        test_util.test_fail('StartVmInstanceMsg not executed at expected timestamp %s ' % except_start_time+120)
+    if not check_scheduler_msg('run scheduler for job: StopVmInstanceJob', except_start_time+10):
+        test_util.test_fail('StopVmInstanceJob not executed at expected timestamp range')
+    if not check_scheduler_msg('run scheduler for job: StartVmInstanceJob', except_start_time+20):
+        test_util.test_fail('StartVmInstanceJob not executed at expected timestamp range' )
 
     schd_ops.delete_scheduler(schd1.uuid)
     schd_ops.delete_scheduler(schd2.uuid)
     vm.set_delete_policy(delete_policy)
     vm.destroy()
 
-#    conditions = res_ops.gen_query_conditions('uuid', '=', schd1.uuid)
-#    if res_ops.query_resource(res_ops.SCHEDULER, conditions)[0] is None:
-#        test_util.test_fail('check stop vm scheduler, it is expected to be destroied, but it still exists')
-#   
-#    conditions = res_ops.gen_query_conditions('uuid', '=', schd2.uuid)
-#    if res_ops.query_resource(res_ops.SCHEDULER, conditions)[0] is None:
-#        test_util.test_fail('check start vm scheduler, it is expected to be destroied, but it still exists')
+    conditions = res_ops.gen_query_conditions('uuid', '=', schd1.uuid)
+    if len(res_ops.query_resource(res_ops.SCHEDULER, conditions)) > 0:
+        test_util.test_fail('check stop vm scheduler, it is expected to be destroied, but it still exists')
+   
+    conditions = res_ops.gen_query_conditions('uuid', '=', schd2.uuid)
+    if len(res_ops.query_resource(res_ops.SCHEDULER, conditions)) > 0:
+        test_util.test_fail('check start vm scheduler, it is expected to be destroied, but it still exists')
 
     test_util.test_pass('Check Scheduler State after Destroy and Recover VM Success')
 
