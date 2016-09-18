e323a57540a441e2550c3baa260efef3fa02fe88
quarkonics <quarkonics@gmail.com>
2016-09-18 20:23:41 +0800

    Fix delete account created for ldap testing

--- integrationtest/vm/basic/test_ldap_login.py
+++ integrationtest/vm/basic/test_ldap_login.py
@@ -13,9 +13,11 @@ import zstackwoodpecker.test_lib as test_lib
 import os
 
 ldap_server_uuid = None
+new_account_uuid = None
 
 def test():
     global ldap_server_uuid
+    global new_account_uuid
 
     ldap_server = ldp_ops.add_ldap_server('ldap1', 'ldap for test', os.environ.get('ldapServerUrl'), os.environ.get('ldapServerBase'), os.environ.get('ldapServerUsername'), os.environ.get('ldapServerPassword'))
     ldap_server_uuid = ldap_server.inventory.uuid
@@ -31,6 +33,7 @@ def test():
         test_util.test_fail('should not be able to bind ldapuid to admin account')
 
     new_account = acc_ops.create_account('new_account', 'password', 'Normal')
+    new_account_uuid = new_account.uuid
     ldap_account = ldp_ops.bind_ldap_account(os.environ.get('ldapUid'), new_account.uuid)
     ldap_account_uuid = ldap_account.inventory.uuid
     session_uuid = acc_ops.login_by_ldap(os.environ.get('ldapUid'), os.environ.get('ldapPassword'))
@@ -44,5 +47,8 @@ def test():
 #Will be called only if exception happens in test().
 def error_cleanup():
     global ldap_server_uuid
+    global new_account_uuid
     if ldap_server_uuid:
         ldp_ops.delete_ldap_server(ldap_server_uuid)
+    if new_account_uuid:
+        acc_ops.delete_account(new_account_uuid)
