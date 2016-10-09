6541f3ca3f539e665b5d6c68e11b070d24ac2518
quarkonics <quarkonics@gmail.com>
2016-10-09 22:57:39 +0800

    Fix multihosts/test_host_active_profile.py

--- integrationtest/vm/multihosts/test_host_active_profile.py
+++ integrationtest/vm/multihosts/test_host_active_profile.py
@@ -10,6 +10,7 @@ import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.header.host as host_header
+import zstacklib.utils.ssh as ssh
 
 _config_ = {
         'timeout' : 600,
@@ -21,13 +22,13 @@ test_obj_dict = test_state.TestStateDict()
 
 def test():
     test_util.test_dsc('Check host active profile')
-    conditions = res_ops.gen_query_conditions('state', '=', host_header.ENABLED, conditions)
+    conditions = res_ops.gen_query_conditions('state', '=', host_header.ENABLED)
     conditions = res_ops.gen_query_conditions('status', '=', host_header.CONNECTED, conditions)
     all_hosts = res_ops.query_resource(res_ops.HOST, conditions)
     if len(all_hosts) < 1:
         test_util.test_skip('Not available host to check')
 
-    command = 'tuned-adm active'
+    command = '/usr/sbin/tuned-adm active'
     for host in all_hosts:
         eout = ''
         if host.sshPort != None:
