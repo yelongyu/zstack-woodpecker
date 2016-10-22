8dfb4b74f0bf62d1d07a2796954f20452841914c
youyk <yongkang.you@gmail.com>
2016-10-22 18:44:05 +0800

    add new cases to test bug: https://github.com/zxwing/premium/issues/1169

--- integrationtest/vm/virtualrouter/snapshot/test_crt_root_sp_in_live_vm_and_data.py
+++ integrationtest/vm/virtualrouter/snapshot/test_crt_root_sp_in_live_vm_and_data.py
@@ -51,68 +51,32 @@ def test():
     volume_creation_option = test_util.VolumeOption()
     volume_creation_option.set_name('volume for snapshot testing')
     volume_creation_option.set_disk_offering_uuid(disk_offering.uuid)
-    volume = test_stub.create_volume(volume_creation_option)
-    test_obj_dict.add_volume(volume)
+    volume1 = test_stub.create_volume(volume_creation_option)
+    test_obj_dict.add_volume(volume1)
+    volume2 = test_stub.create_volume(volume_creation_option)
+    test_obj_dict.add_volume(volume2)
     #make sure utility vm is starting and running
     vm.check()
 
     volume.attach(vm1)
 
-    test_util.test_dsc('create snapshot and check')
-    #snapshots = zstack_sp_header.ZstackVolumeSnapshot()
-    #snapshots.set_target_volume(volume)
-    #test_obj_dict.add_volume_snapshot(snapshots)
-    snapshots = test_obj_dict.get_volume_snapshot(volume.get_volume().uuid)
+    test_util.test_dsc('create snapshot for root')
+    vm_root_volume_inv = test_lib.lib_get_root_volume(vm1.get_vm())
+    snapshots = test_obj_dict.get_volume_snapshot(vm_root_volume_inv.uuid)
     snapshots.set_utility_vm(vm)
-    snapshots.create_snapshot('create_snapshot1')
-    snapshots.check()
+    snapshots.create_snapshot('create_root_volume_snapshot1')
+    volume1.check()
+    volume2.check()
 
-    snapshot1 = snapshots.get_current_snapshot()
-    snapshots.create_snapshot('create_snapshot2')
+    snapshots2 = test_obj_dict.get_volume_snapshot(volume1.get_volume().uuid)
+    snapshots2.set_utility_vm(vm)
+    snapshots2.create_snapshot('create_data_volume_snapshot1')
     snapshots.check()
-    snapshots.create_snapshot('create_snapshot3')
-    snapshots.check()
-    snapshot3 = snapshots.get_current_snapshot()
-
-    #even with live snapshot function, the revert need stop vm.
-    vm1.stop()    
-    snapshots.use_snapshot(snapshot1)
-    vm1.start()
-    snapshots.create_snapshot('create_snapshot1.1.1')
-    snapshots.check()
-    snapshots.create_snapshot('create_snapshot1.1.2')
-    snapshots.check()
-
-    vm1.stop()
-    snapshots.use_snapshot(snapshot1)
-    vm1.start()
-    snapshots.create_snapshot('create_snapshot1.2.1')
-    snapshots.check()
-    snapshot1_2_1 = snapshots.get_current_snapshot()
-    snapshots.create_snapshot('create_snapshot1.2.2')
-    snapshots.check()
-
-    vm1.stop()
-    snapshots.use_snapshot(snapshot3)
-    snapshots.check()
-    snapshots.create_snapshot('create_snapshot4')
-    snapshots.check()
-    #delay start vm
-    vm1.start()
-
-    test_util.test_dsc('Delete snapshot, volume and check')
-    snapshots.delete_snapshot(snapshot3)
-    snapshots.check()
-
-    snapshots.delete_snapshot(snapshot1_2_1)
-    snapshots.check()
-
-    snapshots.delete()
-    test_obj_dict.rm_volume_snapshot(snapshots)
-    volume.check()
+    volume1.check()
+    volume2.check()
 
     test_lib.lib_robot_cleanup(test_obj_dict)
-    test_util.test_pass('Create Snapshot test Success')
+    test_util.test_pass('Create root Snapshot and test data volume status test Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
