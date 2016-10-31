ba18d6d29f18076b4c3e247f794e7246c961ba80
quarkonics <quarkonics@gmail.com>
2016-10-31 16:51:48 +0800

    Add virtualrouter/novlan/test_create_vm_on_specified_ps.py

---
+++ integrationtest/vm/virtualrouter/novlan/test_create_vm_on_specified_ps.py
@@ -0,0 +1,30 @@
+'''
+
+@author: Quarkonics
+'''
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.resource_operations as res_ops
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+def test():
+    global test_obj_dict
+    test_util.test_dsc('Create test vm on each ps and check')
+    cond = res_ops.gen_query_conditions('state', '=', 'Enabled')
+    cond = res_ops.gen_query_conditions('status', '=', 'Connected', cond)
+    pss = res_ops.query_resource_with_num(res_ops.PRIMARY_STORAGE, cond)
+    for ps in pss:
+        vm = test_stub.create_specified_ps_vm(ps.uuid)
+        test_obj_dict.add_vm(vm)
+        vm.check()
+        vm.destroy()
+
+    test_util.test_pass('Create VirtualRouter VM on each PS Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global test_obj_dict
+    test_lib.lib_error_cleanup(test_obj_dict)
