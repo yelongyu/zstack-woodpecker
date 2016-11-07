74d8110fc2a0090537e791cf9e2b2b95f4d87297
quarkonics <quarkonics@gmail.com>
2016-11-07 19:40:07 +0800

    Add virtualrouter/novlan/test_reinit_vm_raw.py

---
+++ integrationtest/vm/virtualrouter/novlan/test_reinit_vm_raw.py
@@ -0,0 +1,35 @@
+'''
+
+New Integration Test for reinit VM.
+
+@author: quarkonics
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.test_lib as test_lib
+import os
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+def test():
+    image_name = os.environ.get('imageName_s')
+    image_uuid = test_lib.lib_get_image_by_name(image_name).uuid
+    l3_name = os.environ.get('l3NoVlanNetworkName1')
+
+    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    vm = test_stub.create_vm([l3_net_uuid], image_uuid, 'user_vlan_vm_s')
+
+    test_obj_dict.add_vm(vm)
+    vm.check()
+    vm.stop()
+    vm.reinit()
+    vm.update()
+    vm.check()
+    vm.destroy()
+    test_util.test_pass('Re-init VM Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    test_lib.lib_error_cleanup(test_obj_dict)
