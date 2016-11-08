e638e704d88f9451ea093d340a1bddbc3eabe254
quarkonics <quarkonics@gmail.com>
2016-11-08 14:50:02 +0800

    Recover VR after PS maintain mode testing

--- integrationtest/vm/virtualrouter/ps/test_maintain_ps.py
+++ integrationtest/vm/virtualrouter/ps/test_maintain_ps.py
@@ -8,6 +8,8 @@ import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.operations.primarystorage_operations as ps_ops
 import zstackwoodpecker.operations.host_operations as host_ops
+import zstackwoodpecker.operations.vm_operations as vm_ops
+import zstackwoodpecker.header.vm as vm_header
 
 _config_ = {
         'timeout' : 1000,
@@ -18,18 +20,25 @@ test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
 ps_uuid = None
 host_uuid = None
+vr_uuid = None
 
 def test():
     global test_obj_dict
     global ps_uuid
     global host_uuid
+    global vr_uuid
     test_util.test_dsc('Create test vm and check')
-    vm = test_stub.create_user_vlan_vm()
+    l3_1_name = os.environ.get('l3VlanNetworkName1')
+    vm = test_stub.create_vlan_vm(l3_name=l3_1_name)
+    vr = test_lib.lib_find_vr_by_l3_uuid(l3_1.uuid)[0]
+    vr_uuid = vr.uuid
+    
+    l3_1 = test_lib.lib_get_l3_by_name(l3_1_name)
     host = test_lib.lib_get_vm_host(vm.get_vm())
     host_uuid = host.uuid
     test_obj_dict.add_vm(vm)
     vm.check()
-    ps = lib_get_primary_storage_by_vm(vm)
+    ps = test_lib.lib_get_primary_storage_by_vm(vm.get_vm())
     ps_uuid = ps.uuid
     ps_ops.change_primary_storage_state(ps_uuid, 'maintain')
     if not test_lib.lib_wait_target_down(vm.get_vm().vmNics[0].ip, '22', 90):
@@ -39,7 +48,7 @@ def test():
     vm.check()
     ps_ops.change_primary_storage_state(ps_uuid, 'Enabled')
     host_ops.reconnect_host(host_uuid)
-
+    vm_ops.reconnect_vr(vr_uuid)
     vm.destroy()
     test_util.test_pass('PS maintain mode Test Success')
 
@@ -51,5 +60,8 @@ def error_cleanup():
     global host_uuid
     if host_uuid != None:
         host_ops.reconnect_host(host_uuid)
+    global vr_uuid
+    if vr_uuid != None:
+        vm_ops.reconnect_vr(vr_uuid)
     global test_obj_dict
     test_lib.lib_error_cleanup(test_obj_dict)
