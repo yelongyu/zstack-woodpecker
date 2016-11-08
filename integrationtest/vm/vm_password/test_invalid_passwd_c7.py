37e8636e7630004456fa20000b5e912a45ac7ff9
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-11-09 00:00:37 +0800

    fix invalid password on centos 7

--- integrationtest/vm/vm_password/test_invalid_passwd_c7.py
+++ integrationtest/vm/vm_password/test_invalid_passwd_c7.py
@@ -29,16 +29,17 @@ def test():
 
     for (usr,passwd) in zip(users, passwds):
 
-        test_util.test_dsc("root_password: \"%s\"" %(root_password))
+        test_util.test_dsc("username:%s, password: \"%s\"" %(usr, passwd))
 
         #Create VM API
         try:
-            vm = test_stub.create_vm(vm_name = root_password +'-c7-vm', image_name = "imageName_i_c7", root_password=root_password)
-            test_util.test_fail("the invaild password: %s successfully be set", root_password)
+            vm = test_stub.create_vm(vm_name = passwd +'-c7-vm', image_name = "imageName_i_c7", root_password=root_password)
+            test_util.test_fail("the invaild password: %s successfully be set", passwd)
         except:
             pass
 
         #Check bs type
+        vm = test_stub.create_vm(vm_name = passwd +'-c7-vm', image_name = "imageName_i_c7")
         backup_storage_list = test_lib.lib_get_backup_storage_list_by_vm(vm.vm)
         for bs in backup_storage_list:
             if bs.type == inventory.IMAGE_STORE_BACKUP_STORAGE_TYPE:
@@ -57,7 +58,7 @@ def test():
         #Change VM API && Running
         try:
             vm_ops.change_vm_password(vm.get_vm().uuid, usr, passwd, skip_stopped_vm = None, session_uuid = None)
-            test_util.test_fail("the invaild password: %s successfully be set", root_password)
+            test_util.test_fail("the invaild password: %s successfully be set", passwd)
         except:
             pass
 
@@ -70,11 +71,11 @@ def test():
         except:
             pass
 
-        if not test_lib.lib_check_login_in_vm(vm.get_vm(), "root", root_password):
-            test_util.test_fail("create vm with root password: %s failed", root_password)
+        if not test_lib.lib_check_login_in_vm(vm.get_vm(), "root", "password"):
+            test_util.test_fail("create vm with root password: \"password\" failed")
 
-        vm.start()
-        vm.check()
+        #vm.start()
+        #vm.check()
         vm.destroy()
         vm.expunge()
         vm.check()
