e20916a738698bb670803b808db4538260d1a10d
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-11-10 10:40:56 +0800

    set over provision to handle os 40GB vs ps 100-30GB

--- integrationtest/vm/vm_password/suite_setup.py
+++ integrationtest/vm/vm_password/suite_setup.py
@@ -26,5 +26,7 @@ def test():
         test_lib.lib_set_ha_selffencer_maxattempts('60')
 	test_lib.lib_set_ha_selffencer_storagechecker_timeout('60')
     test_lib.lib_set_primary_storage_imagecache_gc_interval(1)
+    test_lib.lib_set_provision_storage_rate(6)
+    test_lib.lib_set_provision_memory_rate(3)
     test_util.test_pass('Suite Setup Success')
 
