0c03a9a318e74f4d29660d3fa592b17c88e371f7
quarkonics <quarkonics@gmail.com>
2016-11-18 13:28:19 +0800

    Add ipsec_operations.py

---
+++ zstackwoodpecker/zstackwoodpecker/operations/ipsec_operations.py
@@ -0,0 +1,40 @@
+'''
+
+All ipsec operations for test.
+
+@author: Quarkonics
+'''
+
+import apibinding.api_actions as api_actions
+import zstackwoodpecker.operations.account_operations as acc_ops
+import zstackwoodpecker.test_util as test_util
+import account_operations
+import apibinding.inventory as inventory
+
+import sys
+import traceback
+
+def create_ipsec_connection(name, l3_uuid, peer_address, auth_key, vip_uuid, policy_mode, description, session_uuid=None):
+    action = api_actions.CreateIPsecConnectionAction()
+    action.timeout = 30000
+    action.name = name
+    action.l3NetworkUuid = l3_uuid
+    action.peerAddress = peer_address
+    action.authKey = auth_key
+    action.vipUuid = vip_uuid
+    action.peerCidrs = peer_address
+    action.policyMode = policy_mode
+    action.description = description
+    evt = account_operations.execute_action_with_session(action, session_uuid)
+    test_util.action_logger('Create Ipsec Connection [name:] %s [l3NetworkUuid:] %s [peerAddress:] %s' % \
+            (name, l3_uuid, peer_address))
+    return evt.inventory
+
+def delete_ipsec_connection(uuid, session_uuid=None):
+    action = api_actions.DeleteIPsecConnectionAction()
+    action.timeout = 30000
+    action.uuid = uuid
+    evt = account_operations.execute_action_with_session(action, session_uuid)
+    test_util.action_logger('delete Ipsec Connection [uuid:] %s' % \
+            (uuid))
+    return evt
