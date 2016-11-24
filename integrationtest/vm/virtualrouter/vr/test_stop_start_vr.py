34c1af5ffb569762ee7cc58cc88204bf7f4079bf
quarkonics <quarkonics@gmail.com>
2016-11-24 17:07:57 +0800

    Fix virtualrouter/vr/test_stop_start_vr.py

--- integrationtest/vm/virtualrouter/vr/test_stop_start_vr.py
+++ integrationtest/vm/virtualrouter/vr/test_stop_start_vr.py
@@ -13,6 +13,7 @@ import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.operations.config_operations as con_ops
 import zstackwoodpecker.operations.vm_operations as vm_ops
 import zstackwoodpecker.operations.net_operations as net_ops
+import zstackwoodpecker.operations.ha_operations as ha_ops
 import zstackwoodpecker.operations.account_operations as acc_ops
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
@@ -40,15 +41,23 @@ def test():
     else:
         vr1 = vrs[0]
 
-    vr1.stop()
-    vr1.start()
+    vm_ops.stop_vm(vr1.uuid)
+    if ha_ops.get_vm_instance_ha_level(vr1.uuid) != "NeverStop":
+        vm_ops.start_vm(vr1.uuid)
 
-    cond = res_ops.gen_query_conditions('uuid', '=', vr1.uuid) 
-    vms = res_ops.query_resource(res_ops.VM_INSTANCE, cond)
-    if not vms[0].status == 'Connected':
-        test_util.test_fail('VR VM: %s is not connected' % vr1.uuid)
+    vr1_mgmt_ip = test_lib.lib_find_vr_mgmt_ip(vr1)
+    if not test_lib.lib_wait_target_up(vr1_mgmt_ip, '7272', 120):
+        test_util.test_fail('vr: %s is not startup in 120 seconds. Fail to reboot it. ' % vr1.uuid)
 
-    test_util.test_pass('Test Reconnect VR VM start VRs success')
+    timeout = time.time() + 60
+    while time.time() < timeout:
+        cond = res_ops.gen_query_conditions('uuid', '=', vr1.uuid) 
+        vms = res_ops.query_resource(res_ops.VM_INSTANCE, cond)
+        if vms[0].status == 'Connected':
+            test_util.test_pass('Test Reconnect VR VM start VRs success')
+        time.sleep(1)
+
+    test_util.test_fail('VR VM: %s is not connected within 60 seconds' % vr1.uuid)
 
 def error_cleanup():
     global session_uuid
