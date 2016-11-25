ed0f6f1cea14522adf0b49686f4cd05951393699
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-11-25 09:48:42 +0800

    add missing the claim of vm_ops

--- integrationtest/vm/vm_password/test_no_tag_crt_chg_vm_passwd_c7.py
+++ integrationtest/vm/vm_password/test_no_tag_crt_chg_vm_passwd_c7.py
@@ -7,6 +7,7 @@ import apibinding.inventory as inventory
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.vm_operations as vm_ops
 import test_stub
 
 
