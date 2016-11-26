61f1d7225766d68b70d345364af8c1d6047a72b9
Meng Lai <bluestar3@yeah.net>
2016-11-26 17:25:30 +0800

    update ntp case

--- integrationtest/vm/virtualrouter/host/test_ntp.py
+++ integrationtest/vm/virtualrouter/host/test_ntp.py
@@ -13,19 +13,19 @@ def test():
     test_util.test_dsc('Test Host ntp')
     cmd = 'hostname'
     mn = res_ops.query_resource(res_ops.MANAGEMENT_NODE)[0]
-    for host in test_lib.lib_get_all_hosts_from_plan():
-        if mn.hostName == host.managementIp_:
-            mn_hostname = test_lib.lib_execute_ssh_cmd(host.managementIp_, host.username_, host.password_, cmd, timeout=30).strip()
+#    for host in test_lib.lib_get_all_hosts_from_plan():
+#        if mn.hostName == host.managementIp_:
+#            mn_hostname = test_lib.lib_execute_ssh_cmd(host.managementIp_, host.username_, host.password_, cmd, timeout=30).strip()
 
     cmd = 'ntpq -p'
     for host in test_lib.lib_get_all_hosts_from_plan():
         if host.managementIp_ == mn.hostName:
             output = test_lib.lib_execute_ssh_cmd(host.managementIp_, host.username_, host.password_, cmd, timeout=30)
-            if output.find(mn_hostname) >= 0:
+            if output.find(mn.hostName) >= 0:
                 test_util.test_fail('if host and MN are same host, its not expected to use itself')
         else:
             output = test_lib.lib_execute_ssh_cmd(host.managementIp_, host.username_, host.password_, cmd, timeout=30)
-            if output.find(mn_hostname) < 0:
+            if output.find(mn.hostName) < 0:
                 test_util.test_fail('all host expect to use MN ntp service')
 
     test_util.test_pass('Test Host ntp Pass')
