0e78355d5f07676e47ca895c182a833a52ac9475
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-11-26 15:44:34 +0800

    add check of vm and qemu-ga alive logic

--- integrationtest/vm/vm_password/test_chg_vm_passwd_u16.py
+++ integrationtest/vm/vm_password/test_chg_vm_passwd_u16.py
@@ -18,9 +18,27 @@ users   = ["root",      "root",     ]
 passwds = ["password",  "95_aaapcn" ]
 
 vm = None
+cur_usr = None
+cur_passwd = None
+
+def check_vm_is_alive(vm):
+    global cur_usr, cur_passwd
+    cmd = "pwd"
+    ret, output, stderr = ssh.execute(cmd, vm.get_vm().vmNics[0].ip, cur_usr, cur_passwd, False, 22)
+    if ret != 0:
+        test_util.test_logger("VM is not alived when exception triggered: ip:%s; cmd:%s; user:%s; password:%s; stdout:%s, stderr:%s" %(vm.get_vm().vmNics[0].ip, cmd, cur_usr, cur_passwd, output, stderr))
+
+def check_qemu_ga_is_alive(vm):
+    global cur_usr, cur_passwd
+    cmd = "ps -aux|grep ga|grep qemu"
+    ret, output, stderr = ssh.execute(cmd, vm.get_vm().vmNics[0].ip, cur_usr, cur_passwd, False, 22)
+    if ret != 0:
+        test_util.test_logger("qemu-ga is not alived when exception triggered: ip:%s; cmd:%s; user:%s; password:%s; stdout:%s, stderr:%s" %(vm.get_vm().vmNics[0].ip, cmd, cur_usr, cur_passwd, output, stderr))
+
+
 
 def test():
-    global vm, exist_users
+    global vm, exist_users, cur_usr, cur_passwd
     test_util.test_dsc('change VM with assigned password test')
 
     vm = test_stub.create_vm(vm_name = 'ckvmpswd-u16-64', image_name = "imageName_i_u16")
@@ -38,21 +56,26 @@ def test():
         vm.destroy()
         test_util.test_skip('Not find image store type backup storage.')
 
+    cur_usr = "root"
+    cur_passwd = "password"
+
     for (usr,passwd) in zip(users, passwds):
 
         #When vm is running:
-        inv = vm_ops.change_vm_password(vm.get_vm().uuid, usr, passwd, skip_stopped_vm = None, session_uuid = None)
-        if not inv:
-            test_util.test_fail("change vm password failed")
+        vm_ops.change_vm_password(vm.get_vm().uuid, usr, passwd, skip_stopped_vm = None, session_uuid = None)
+
+        cur_usr = usr
+        cur_passwd = passwd
 
         if not test_lib.lib_check_login_in_vm(vm.get_vm(), usr, passwd):
             test_util.test_fail("create vm with user:%s password: %s failed", usr, passwd)
         
         #When vm is stopped:
         #vm.stop()
-        inv = vm_ops.change_vm_password(vm.get_vm().uuid, "root", test_stub.original_root_password)
-        if not inv:
-            test_util.test_fail("recover vm password failed")
+        vm_ops.change_vm_password(vm.get_vm().uuid, "root", test_stub.original_root_password)
+
+        cur_usr = "root"
+        cur_passwd = "password"
 
         #vm.start()
         vm.check()
@@ -70,6 +93,10 @@ def test():
 #Will be called only if exception happens in test().
 def error_cleanup():
     global vm
+
+    check_vm_is_alive(vm)
+    check_qemu_ga_is_alive(vm)
+
     if vm:
         vm.destroy()
         vm.expunge()
