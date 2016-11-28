9c8a6b98fbf88e758adb48ce284240826138aded
quarkonics <quarkonics@gmail.com>
2016-11-28 16:49:34 +0800

    Wait for extra 2 seconds before press esc

--- integrationtest/vm/basic/test_support_vm_boot_option.py
+++ integrationtest/vm/basic/test_support_vm_boot_option.py
@@ -10,6 +10,7 @@ import zstackwoodpecker.test_lib as test_lib
 import zstacklib.utils.shell as shell
 import test_stub
 import os
+import time
 import zstackwoodpecker.operations.account_operations as acc_ops
 from vncdotool import api
 
@@ -25,19 +26,19 @@ def test():
     signal.alarm(30)
     boot_option_picture = os.environ.get('bootOptionPicture')
     
-    try:
-        vm = test_stub.create_vm()
-        console = test_lib.lib_get_vm_console_address(vm.get_vm().uuid)
-        test_util.test_logger('[vm:] %s console is on %s:%s' % (vm.get_vm().uuid, console.hostIp, console.port))
-        display = str(int(console.port)-5900)
+    vm = test_stub.create_vm()
+    console = test_lib.lib_get_vm_console_address(vm.get_vm().uuid)
+    test_util.test_logger('[vm:] %s console is on %s:%s' % (vm.get_vm().uuid, console.hostIp, console.port))
+    display = str(int(console.port)-5900)
 
-        client = api.connect(console.hostIp+":"+display)
-        client.keyPress('esc')
-        #client.captureRegion('/root/boot.png',0,100,600,600)
-        client.expectRegion(boot_option_picture,0,100)
-        test_util.test_logger('[vm:] %s support boot option' % (vm.get_vm().uuid))
-    except:
-        test_util.test_fail('[vm:] %s is expected to support boot option' % (vm.get_vm().uuid))
+    client = api.connect(console.hostIp+":"+display)
+    time.sleep(2)
+    client.keyPress('esc')
+    #client.captureRegion('/root/boot.png',0,100,600,600)
+    client.expectRegion(boot_option_picture,0,100)
+    test_util.test_logger('[vm:] %s support boot option' % (vm.get_vm().uuid))
+#    except:
+#        test_util.test_fail('[vm:] %s is expected to support boot option' % (vm.get_vm().uuid))
 
     vm.destroy()
 
