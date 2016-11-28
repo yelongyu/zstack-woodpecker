33ea8ed50f3357007fe8c70fc393877d7b364adb
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-11-28 13:35:04 +0800

    fix due to disable creating vm change password

--- integrationtest/vm/vm_password/test_no_tag_crt_chg_vm_passwd_c7.py
+++ integrationtest/vm/vm_password/test_no_tag_crt_chg_vm_passwd_c7.py
@@ -21,7 +21,8 @@ def test():
 
     for root_password in root_password_list:
         test_util.test_dsc("root_password: \"%s\"" %(root_password))
-        vm = test_stub.create_vm(vm_name = 'c7-vm-no-sys-tag', image_name = "imageName_i_c7_no_tag", root_password=root_password)
+        #vm = test_stub.create_vm(vm_name = 'c7-vm-no-sys-tag', image_name = "imageName_i_c7_no_tag", root_password=root_password)
+        vm = test_stub.create_vm(vm_name = 'c7-vm-no-sys-tag', image_name = "imageName_i_c7_no_tag")
 
         backup_storage_list = test_lib.lib_get_backup_storage_list_by_vm(vm.vm)
         for bs in backup_storage_list:
@@ -35,14 +36,17 @@ def test():
             vm.destroy()
             test_util.test_skip('Not find image store type backup storage.')
 
-        if not test_lib.lib_check_login_in_vm(vm.get_vm(), "root", root_password):
-            test_util.test_fail("create vm with root password: %s failed", root_password)
+        #if not test_lib.lib_check_login_in_vm(vm.get_vm(), "root", root_password):
+        #    test_util.test_fail("create vm with root password: %s failed", root_password)
 
         # stop vm && change vm password
         #vm.stop()
-        inv = vm_ops.change_vm_password(vm.get_vm().uuid, "root", test_stub.original_root_password)
-        if not inv:
-            test_util.test_fail("recover vm password failed")
+        vm.check()
+        vm_ops.change_vm_password(vm.get_vm().uuid, "root", root_password)
+        if not test_lib.lib_check_login_in_vm(vm.get_vm(), "root", root_password):
+            test_util.test_fail("create vm with root password: %s failed", root_password)
+
+        vm_ops.change_vm_password(vm.get_vm().uuid, "root", test_stub.original_root_password)
 
         #vm.start()
         vm.check()
