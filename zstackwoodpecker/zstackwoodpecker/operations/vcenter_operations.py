0dca367ab878e8b9251d003112a054964fac1bb6
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-12-02 14:24:37 +0800

    add vcenter oprations module

---
+++ zstackwoodpecker/zstackwoodpecker/operations/vcenter_operations.py
@@ -0,0 +1,36 @@
+'''
+
+All vcenter operations for test.
+
+@author: SyZhao
+'''
+
+import apibinding.inventory as inventory
+import apibinding.api_actions as api_actions
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.operations.account_operations as account_operations
+
+
+
+def add_vcenter(name, domain_name, username, password, https, zone_uuid, timeout=240000, session_uuid=None):
+    action = api_actions.AddVCenterAction()
+    action.name = name
+    action.domainName = domain_name
+    action.username = username
+    action.password = password
+    action.https = https
+    action.zoneUuid = zone_uuid
+    action.timeout = timeout
+    test_util.action_logger('Add VCenter [uuid:] %s' % vm_uuid)
+    evt = account_operations.execute_action_with_session(action, session_uuid)
+    return evt.inventory
+
+
+def delete_vcenter(vcenter_uuid, timeout=240000, session_uuid=None):
+    action = api_actions.DeleteVCenterAction()
+    action.vcenter_uuid = vcenter_uuid
+    action.timeout = timeout
+    test_util.action_logger('Delete VCenter [uuid:] %s' % vm_uuid)
+    evt = account_operations.execute_action_with_session(action, session_uuid)
+    return evt.inventory
+
