5500a1fcaa32666f8fed6eb31dc4d48f7fd6aa74
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-12-08 20:03:10 +0800

    add template for vcenter

---
+++ integrationtest/vm/vcenter/test_add_del_vcenter.py
@@ -0,0 +1,21 @@
+'''
+test for add and delete vcenter
+@author: SyZhao
+'''
+
+import apibinding.inventory as inventory
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.vm_operations as vm_ops
+import zstacklib.utils.ssh as ssh
+import test_stub
+
+
+
+def test():
+    pass
+
+
+def error_cleanup():
+    pass
