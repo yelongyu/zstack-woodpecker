64144905366556d8ef885c576671abbdeacaf4e1
quarkonics <quarkonics@gmail.com>
2016-12-14 10:09:00 +0800

    Add support for GetTaskProgress

--- zstackwoodpecker/zstackwoodpecker/operations/resource_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/resource_operations.py
@@ -460,3 +460,10 @@ def get_resource_owner(resourceUuid, session_uuid = None):
     action.resourceUuids = resourceUuid
     ret = account_operations.execute_action_with_session(action, session_uuid)
     return ret.inventories[resourceUuid[0]].uuid
+
+def get_task_progress(resourceUuid, processType=None, session_uuid = None):
+    action = api_actions.GetTaskProgressAction()
+    action.resourceUuids = resourceUuid
+    action.processType = processType
+    evt = account_operations.execute_action_with_session(action, session_uuid)
+    return evt
