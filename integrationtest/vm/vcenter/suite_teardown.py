7daf1d28fb9b8c17ce285378fde9a31fb6f9a46a
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-12-21 09:49:28 +0800

    fix teardown for vcenter

--- integrationtest/vm/vcenter/suite_teardown.py
+++ integrationtest/vm/vcenter/suite_teardown.py
@@ -21,17 +21,4 @@ def test():
     clean_util.umount_all_primary_storages_violently()
     clean_util.cleanup_backup_storage()
     test_lib.setup_plan.stop_node()
-    cmd = host_plugin.DeleteVlanDeviceCmd()
-    
-    hosts = test_lib.lib_get_all_hosts_from_plan()
-    if type(hosts) != type([]):
-        hosts = [hosts]
-
-    for host in hosts:
-        cmd.vlan_ethname = 'eth0.10'
-        http.json_dump_post(testagent.build_http_path(host.managementIp_, host_plugin.DELETE_VLAN_DEVICE_PATH), cmd)
-        cmd.vlan_ethname = 'eth0.11'
-        http.json_dump_post(testagent.build_http_path(host.managementIp_, host_plugin.DELETE_VLAN_DEVICE_PATH), cmd)
-
-    test_lib.lib_cleanup_host_ip_dict()
-    test_util.test_pass('VirtualRouter Teardown Success')
+    test_util.test_pass('VCenter Teardown Success')
