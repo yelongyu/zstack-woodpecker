10dff2e74a80e4aeba8c68cae4db6449ee7dc82e
zsyzsyhao <zsyzsyhao@foxmail.com>
2016-12-22 21:11:28 +0800

    add test of sync for vcenter vm create delete start and stop

---
+++ integrationtest/vm/vcenter/test_vcenter_sync_vm_create_delete.py
@@ -0,0 +1,90 @@
+'''
+test for checking if vm exist in newly add vcenter
+@author: SyZhao
+'''
+
+import apibinding.inventory as inventory
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.vm_operations as vm_ops
+import zstackwoodpecker.operations.vcenter_operations as vct_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+import zstacklib.utils.ssh as ssh
+import test_stub
+import os
+
+
+vcenter1_name = "VCENTER1"
+vcenter1_domain_name = "172.20.76.251"
+vcenter1_username = "administrator@vsphere.local"
+vcenter1_password = "Testing%123"
+
+vcenter_uuid = None
+
+def shadow_create_vm():
+    """
+    This function is to execute a create vm on another remote woodpecker, 
+    which also attached the same vcenter.
+    """
+
+    shadow_vm_ip = os.environ.get('serverIp2')
+    cmd = "cd /home/%s/zstack-woodpecker/dailytest/ && python ./zstest.py -c vcenter/remote_vm_create.py" %(shadow_vm_ip)
+    ret, output, stderr = ssh.execute(cmd, shadow_vm_ip, "root", "password", False, 22)
+    if ret != 0:
+        test_util.test_fail("remote create vm failed: output:%s; stderr:%s" %(output, stderr))
+
+
+def shadow_delete_vm():
+    """
+    This function is to execute a delete the vm named "vm-crt-via-rmt-mevoco" on another remote woodpecker, 
+    which also attached the same vcenter.
+    """
+
+    shadow_vm_ip = os.environ.get('serverIp2')
+    cmd = "cd /home/%s/zstack-woodpecker/dailytest/ && python ./zstest.py -c vcenter/remote_vm_delete.py" %(shadow_vm_ip)
+    ret, output, stderr = ssh.execute(cmd, shadow_vm_ip, "root", "password", False, 22)
+    if ret != 0:
+        test_util.test_fail("remote delete vm 'vm-crt-via-rmt-mevoco' failed: output:%s; stderr:%s" %(output, stderr))
+
+
+
+vm_name = "vm-crt-via-rmt-mevoco"
+
+def test():
+    global vcenter_uuid
+
+    #add vcenter senario1:
+    zone_uuid = res_ops.get_resource(res_ops.ZONE)[0].uuid
+    inv = vct_ops.add_vcenter(vcenter1_name, vcenter1_domain_name, vcenter1_username, vcenter1_password, True, zone_uuid)
+    vcenter_uuid = inv.uuid
+
+    if vcenter_uuid == None:
+        test_util.test_fail("vcenter_uuid is None")
+
+
+    shadow_create_vm()
+
+    vm_cond = res_ops.gen_query_conditions("name", '=', vm_name)
+    vm_uuid = res_ops.query_resource_fields(res_ops.VM_INSTANCE, vm_cond, None, fields=['uuid'])[0].uuid
+    if not vm_uuid:
+        test_util.test_fail("vcenter sync create vm test failed.")
+
+
+    shadow_delete_vm()
+
+    vm_uuid = res_ops.query_resource_fields(res_ops.VM_INSTANCE, vm_cond, None, fields=['uuid'])[0].uuid
+    if vm_uuid:
+        test_util.test_fail("vcenter sync delete vm test failed.")
+
+    vct_ops.delete_vcenter(vcenter_uuid)
+    test_util.test_pass("vcenter sync create and delete vm test passed.")
+
+
+
+def error_cleanup():
+    global vcenter_uuid
+
+    shadow_delete_vm()
+    if vcenter_uuid:
+        vct_ops.delete_vcenter(vcenter_uuid)
