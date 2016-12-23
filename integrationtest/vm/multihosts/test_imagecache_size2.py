d7cbd3e4081fb23e8feb811b0150cb3c4dd76f3e
quarkonics <quarkonics@gmail.com>
2016-12-23 19:09:51 +0800

    Fix multihosts/test_imagecache_size2.py

--- integrationtest/vm/multihosts/test_imagecache_size2.py
+++ integrationtest/vm/multihosts/test_imagecache_size2.py
@@ -13,6 +13,7 @@ import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.zstack_test.zstack_test_image as test_image
 import zstackwoodpecker.operations.resource_operations as res_ops
+import zstackwoodpecker.operations.image_operations as img_ops
 import zstackwoodpecker.operations.primarystorage_operations as ps_ops
 import zstackwoodpecker.zstack_test.zstack_test_image as zstack_image_header
 import apibinding.inventory as inventory
@@ -42,7 +43,9 @@ def test():
     host = test_lib.lib_find_host_by_vm(vm2.get_vm())
     ps = test_lib.lib_get_primary_storage_by_vm(vm2.get_vm())
     image = test_lib.lib_get_image_by_name('test_create_image_template_imagecache')
-    image.delete()
+    img_ops.sync_image_size(image.uuid)
+    image = test_lib.lib_get_image_by_name('test_create_image_template_imagecache')
+    img_ops.delete_image(image.uuid)
 
     if ps.type == inventory.LOCAL_STORAGE_TYPE or ps.type == inventory.NFS_PRIMARY_STORAGE_TYPE or ps.type == 'SharedMountPoint':
         image_cache_path = "%s/imagecache/template/%s/" % (ps.mountPath, image.uuid)
@@ -54,10 +57,10 @@ def test():
         test_util.test_skip("Skip test when primary storage is not local or NFS")
 #    elif ps.type == inventory.CEPH_PRIMARY_STORAGE_TYPE:
 
-    test_lib.lib_error_clean_up(test_obj_dict)
+    test_lib.lib_error_cleanup(test_obj_dict)
     test_util.test_pass('imagecache cleanup Pass.')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
-    test_lib.lib_error_clean_up(test_obj_dict)
+    test_lib.lib_error_cleanup(test_obj_dict)
     pass
