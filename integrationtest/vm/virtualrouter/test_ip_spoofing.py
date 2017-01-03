e3544f77a8212db59f1443b1e83b63e3f3990493
Meng Lai <bluestar3@yeah.net>
2017-01-03 16:06:13 +0800

    update case, ip spoofing

--- integrationtest/vm/virtualrouter/test_ip_spoofing.py
+++ integrationtest/vm/virtualrouter/test_ip_spoofing.py
@@ -7,6 +7,7 @@ import time
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.config_operations as con_ops
 
 test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
@@ -14,11 +15,19 @@ test_obj_dict = test_state.TestStateDict()
 node_ip = os.environ.get('node1Ip')
 test_file_src = "/home/%s/zstack-woodpecker/integrationtest/vm/virtualrouter/change_ip_test_template.sh" % node_ip
 test_file_des = "/home/%s/zstack-woodpecker/integrationtest/vm/virtualrouter/change_ip_test.sh" % node_ip
+ct_original = None
 
 def test():
     global test_obj_dict
     global test_file_src
     global test_file_des
+    global ct_original
+
+    if con_ops.get_global_config_value('vm', 'cleanTraffic') == 'false' :
+        ct_original='false'
+        con_ops.change_global_config('vm', 'cleanTraffic', 'true')  
+    else:
+        ct_original='true'
 
     vm = test_stub.create_basic_vm()
     test_obj_dict.add_vm(vm)
@@ -56,6 +65,7 @@ def test():
     
     vm.destroy()
     test_obj_dict.rm_vm(vm)
+    con_ops.change_global_config('vm', 'cleanTraffic', ct_original)
     os.system('rm -f %s' % test_file_des)
     test_util.test_pass('IP Spoofing Test Success')
 
@@ -63,5 +73,7 @@ def test():
 def error_cleanup():
     global test_obj_dict
     global test_file_des
+    global ct_original
+    con_ops.change_global_config('vm', 'cleanTraffic', ct_original)
     os.system('rm -f %s' % test_file_des) 
     test_lib.lib_error_cleanup(test_obj_dict)
