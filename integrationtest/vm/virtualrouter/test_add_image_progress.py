12454040020d2d6e53679646f0f60dd047257ce2
quarkonics <quarkonics@gmail.com>
2017-01-08 20:57:51 +0800

    Add image progress test only on imagestore and ceph BS

--- integrationtest/vm/virtualrouter/test_add_image_progress.py
+++ integrationtest/vm/virtualrouter/test_add_image_progress.py
@@ -24,10 +24,14 @@ new_image = None
 def add_image():
     global new_image
     bs_cond = res_ops.gen_query_conditions("status", '=', "Connected")
-    bss = res_ops.query_resource_fields(res_ops.CEPH_BACKUP_STORAGE, bs_cond, \
+    bss = res_ops.query_resource_fields(res_ops.BACKUP_STORAGE, bs_cond, \
             None, fields=['uuid'])
     if not bss:
-        test_util.test_skip("not find available ceph backup storage. Skip test")
+        test_util.test_skip("not find available backup storage. Skip test")
+
+    if bss[0].type != inventory.CEPH_BACKUP_STORAGE_TYPE:
+        if hasattr(inventory, 'IMAGE_STORE_BACKUP_STORAGE_TYPE') and bss[0].type != inventory.IMAGE_STORE_BACKUP_STORAGE_TYPE:
+            test_util.test_skip("not find available imagestore or ceph backup storage. Skip test")
 
     image_option = test_util.ImageOption()
     image_option.set_name('test_add_image_progress')
