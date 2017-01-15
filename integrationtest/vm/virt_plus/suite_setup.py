6adcced6e4f2d81e088ea3348633f0cbf89f485e
quarkonics <quarkonics@gmail.com>
2017-01-16 00:35:27 +0800

    Allow to run extra host setup for virt_plus

--- integrationtest/vm/virt_plus/suite_setup.py
+++ integrationtest/vm/virt_plus/suite_setup.py
@@ -15,13 +15,22 @@ import zstackwoodpecker.test_util as test_util
 
 USER_PATH = os.path.expanduser('~')
 EXTRA_SUITE_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_suite_setup_config.sh' % USER_PATH
+EXTRA_HOST_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_host_setup_config.sh' % USER_PATH
 def test():
     #If test execution machine is not the same one as Host machine, deploy work is needed to separated to 2 steps(deploy_test_agent, execute_plan_without_deploy_test_agent). And it can not directly call SetupAction.run()
+    hosts = test_lib.lib_get_all_hosts_from_plan()
+    if type(hosts) != type([]):
+        hosts = [hosts]
+
     test_lib.setup_plan.deploy_test_agent()
     test_lib.setup_plan.execute_plan_without_deploy_test_agent()
     deploy_operations.deploy_initial_database(test_lib.deploy_config)
     if os.path.exists(EXTRA_SUITE_SETUP_SCRIPT):
         os.system("bash %s" % EXTRA_SUITE_SETUP_SCRIPT)
+
+    for host in hosts:
+        os.system("bash %s %s" % (EXTRA_HOST_SETUP_SCRIPT, host.managementIp_))
+
     if test_lib.lib_get_ha_selffencer_maxattempts() != None:
         test_lib.lib_set_ha_selffencer_maxattempts('60')
 	test_lib.lib_set_ha_selffencer_storagechecker_timeout('60')
