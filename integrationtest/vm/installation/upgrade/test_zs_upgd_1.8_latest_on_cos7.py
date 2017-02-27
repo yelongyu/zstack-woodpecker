99ced6f50fd24e9798d6667aee8e39c499443660
Meng Lai <bluestar3@yeah.net>
2017-02-27 21:15:26 +0800

    update upgrade case 1.8

--- integrationtest/vm/installation/upgrade/test_zs_upgd_1.8_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_1.8_latest_on_cos7.py
@@ -16,15 +16,16 @@ test_obj_dict = test_state.TestStateDict()
 tmp_file = '/tmp/%s' % uuid.uuid1().get_hex()
 
 node_ip = os.environ.get('node1Ip')
-update_file = "/home/%s/zstack-woodpecker/integrationtest/vm/installation/update_iso.sh" % node_ip
 
 def test():
     test_util.test_dsc('Create test vm to test zstack upgrade by -u.')
 
     if os.path.exists('/home/installation-package/zstack'):
         image_name = os.environ.get('imageName_i_c7_z_1.8')
+        update_file = "/home/%s/zstack-woodpecker/integrationtest/vm/installation/zstack_update_iso.sh" % node_ip
     elif os.path.exists('/home/installation-package/mevoco'):
         image_name = os.environ.get('imageName_i_c7_m_1.8')
+        update_file = "/home/%s/zstack-woodpecker/integrationtest/vm/installation/mevoco_update_iso.sh" % node_ip
 
     vm = test_stub.create_vlan_vm(image_name)
     test_obj_dict.add_vm(vm)
@@ -43,21 +44,22 @@ def test():
     test_stub.copy_id_dsa(vm_inv,ssh_cmd,tmp_file)
     test_stub.copy_id_dsa_pub(vm_inv)
 
-    test_stub.update_iso(ssh_cmd, tmp_file, vm_inv, update_file)
-
     test_util.test_dsc('Update MN IP')
     cmd='%s "zstack-ctl change_ip --ip="%s'%(ssh_cmd,vm_ip)
     test_stub.execute_shell_in_process(cmd, tmp_file)
-#    cmd='%s "zstack-ctl start"'%ssh_cmd
-#    test_stub.execute_shell_in_process(cmd, tmp_file)
-#    test_stub.check_installation(ssh_cmd,tmp_file,vm_inv)
+    cmd='%s "zstack-ctl start"'%ssh_cmd
+    test_stub.execute_shell_in_process(cmd, tmp_file)
+    test_stub.check_installation(ssh_cmd,tmp_file,vm_inv)
 
     test_util.test_dsc('Upgrade zstack to latest')
+    test_stub.update_iso(ssh_cmd, tmp_file, vm_inv, update_file)
     upgrade_target_file='/root/uzstack-upgrade-all-in-one.tgz'
     test_stub.prepare_test_env(vm_inv,upgrade_target_file)
     test_stub.upgrade_zstack(ssh_cmd,upgrade_target_file,tmp_file)
     zstack_latest_version = os.environ.get('zstackLatestVersion')
     test_stub.check_zstack_version(ssh_cmd, tmp_file, vm_inv, zstack_latest_version)
+    cmd = '%s "zstack-ctl start"' % ssh_cmd
+    process_result = test_stub.execute_shell_in_process(cmd, tmp_file)
     test_stub.check_installation(ssh_cmd,tmp_file,vm_inv)
 
     os.system('rm -rf -%s'%tmp_file)
@@ -68,5 +70,3 @@ def test():
 def error_cleanup():
     os.system('rm -rf -%s'%tmp_file)
     test_lib.lib_error_clean_up(test_obj_dict)
-
-
