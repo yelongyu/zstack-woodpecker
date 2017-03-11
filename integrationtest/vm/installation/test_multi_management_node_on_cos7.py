860367e208fa4b8eccf34d417136beb08cbca422
Meng Lai <bluestar3@yeah.net>
2017-03-11 15:21:30 +0800

    update case, add multi managements

--- integrationtest/vm/installation/test_multi_management_node_on_cos7.py
+++ integrationtest/vm/installation/test_multi_management_node_on_cos7.py
@@ -11,51 +11,70 @@ import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
 import zstacklib.utils.ssh as ssh
+import zstackwoodpecker.operations.resource_operations as res_ops
+import zstackwoodpecker.operations.scenario_operations as sce_ops
 
 test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
 tmp_file = '/tmp/%s' % uuid.uuid1().get_hex()
+zstack_management_ip = os.environ.get('zstackManagementIp')
+vm1_inv = None
+vm2_inv = None
+vm3_inv = None
 
+def create_vm(image):
+    l3_name = os.environ.get('l3PublicNetworkName')
+    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    image_uuid = image.uuid
+    vm_name = 'zs_install_%s' % image.name
+    vm_instrance_offering_uuid = os.environ.get('instanceOfferingUuid')
+
+    vm_creation_option = test_util.VmOption()
+    vm_creation_option.set_instance_offering_uuid(vm_instrance_offering_uuid)
+    vm_creation_option.set_l3_uuids([l3_net_uuid])
+    vm_creation_option.set_image_uuid(image_uuid)
+    vm_creation_option.set_name(vm_name)
+    vm_inv = sce_ops.create_vm(zstack_management_ip, vm_creation_option)
+    
+    return vm_inv
 
 def test():
-    def test_fail(msg):
-        os.system('rm -f %s' % tmp_file)
-        test_util.test_fail(msg)
+    global vm1_inv
+    global vm2_inv
+    global vm3_inv
 
     test_util.test_dsc('Create 3 CentOS7 vm to test multi management node installation')
 
-#    image_name = os.environ.get('imageName_i_c7')
-    image_name = os.environ.get('imageName_i_offline')
+    conditions = res_ops.gen_query_conditions('name', 'like', os.environ.get('imageNameBase_o'))
+    image = res_ops.query_resource(res_ops.IMAGE, conditions)[0]
 
-    vm1 = test_stub.create_vlan_vm(image_name)
-    test_obj_dict.add_vm(vm1)
-    vm2 = test_stub.create_vlan_vm(image_name)
-    test_obj_dict.add_vm(vm2)
-    vm3 = test_stub.create_vlan_vm(image_name)
-    test_obj_dict.add_vm(vm3)
-    if os.environ.get('zstackManagementIp') == None:
-        vm1.check()
-        vm2.check()
-        vm3.check()
-    else:
-        time.sleep(60)
+    if os.path.exists('/home/installation-package/zstack'):
+        conditions1 = res_ops.gen_query_conditions('name', 'like', os.environ.get('imageNameBase_Z'))
+    elif os.path.exists('/home/installation-package/mevoco'):
+        conditions1 = res_ops.gen_query_conditions('name', 'like', os.environ.get('imageNameBase_M'))
+    image1 = res_ops.query_resource(res_ops.IMAGE, conditions1)[0]
 
-    vm1_inv = vm1.get_vm()
+    vm1_inv = create_vm(image1) 
+    vm2_inv = create_vm(image)
+    vm3_inv = create_vm(image)
+
     vm1_ip = vm1_inv.vmNics[0].ip
-    vm2_inv = vm2.get_vm()
     vm2_ip = vm2_inv.vmNics[0].ip
-    vm3_inv = vm3.get_vm()
     vm3_ip = vm3_inv.vmNics[0].ip
+
+    time.sleep(60)
+
     target_file = '/root/zstack-all-in-one.tgz'
     test_stub.prepare_test_env(vm1_inv, target_file)
     ssh_cmd1 = 'ssh  -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm1_ip
     ssh_cmd2 = 'ssh  -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm2_ip
     ssh_cmd3 = 'ssh  -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm3_ip
 
-    test_util.test_dsc('Install ZStack on vm1')
-    test_stub.copy_id_dsa(vm1_inv, ssh_cmd1, tmp_file)
-    test_stub.copy_id_dsa_pub(vm1_inv)
-    test_stub.execute_all_install(ssh_cmd1, target_file, tmp_file)
+    test_util.test_dsc('Update MN IP and check installation on vm1')
+    cmd='%s "zstack-ctl change_ip --ip="%s'%(ssh_cmd1,vm1_ip)
+    test_stub.execute_shell_in_process(cmd, tmp_file)
+    cmd='%s "zstack-ctl start"'%ssh_cmd1
+    test_stub.execute_shell_in_process(cmd, tmp_file)
     test_stub.check_installation(ssh_cmd1, tmp_file, vm1_inv)
 
     test_util.test_dsc('Install multi management node on vm2 and vm3')
@@ -102,15 +121,20 @@ def test():
     test_stub.check_installation(ssh_cmd3, tmp_file, vm3_inv)
 
     os.system('rm -f %s' % tmp_file)
-    vm1.destroy()
-    test_obj_dict.rm_vm(vm1)
-    vm2.destroy()
-    test_obj_dict.rm_vm(vm2)
-    vm3.destroy()
-    test_obj_dict.rm_vm(vm3)
+    sce_ops.destroy_vm(zstack_management_ip, vm1_inv.uuid)
+    sce_ops.destroy_vm(zstack_management_ip, vm2_inv.uuid)
+    sce_ops.destroy_vm(zstack_management_ip, vm3_inv.uuid)
+
     test_util.test_pass('ZStack multi management nodes installation Test Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
+    global vm1_inv
+    global vm2_inv
+    global vm3_inv
+    
     os.system('rm -f %s' % tmp_file)
+    sce_ops.destroy_vm(zstack_management_ip, vm1_inv.uuid)
+    sce_ops.destroy_vm(zstack_management_ip, vm2_inv.uuid)
+    sce_ops.destroy_vm(zstack_management_ip, vm3_inv.uuid)
     test_lib.lib_error_cleanup(test_obj_dict)
