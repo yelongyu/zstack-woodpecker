861ec8a83e2818faba13a0bf8254557ccbffbe03
ads6ads6 <ads5ads5@yeah.net>
2017-05-12 00:48:40 +0800

    Update HotPlugin case

--- integrationtest/vm/virt_plus/Hot_plug/Hot_plug_common.py
+++ integrationtest/vm/virt_plus/Hot_plug/Hot_plug_common.py
@@ -4,7 +4,7 @@
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.operations.resource_operations as res_ops
-import os
+
 
 def create_onlinechange_vm(test_stub=None, test_obj_dict=None):
     cond = res_ops.gen_query_conditions('state', '=', 'Enabled')
@@ -16,14 +16,9 @@ def create_onlinechange_vm(test_stub=None, test_obj_dict=None):
     host = host[0]
     test_offering = test_lib.lib_create_instance_offering(cpuNum=1, cpuSpeed=16,
                                                           memorySize=1024 * 1024 * 1024, name='test_offering')
-
-    l3_name = os.environ.get('l3VlanNetworkName1')
-    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
-    l3_net_list = [l3_net_uuid]
     test_obj_dict.add_instance_offering(test_offering)
     vm = test_stub.create_vm(vm_name='test_update_instance_offering', host_uuid=host.uuid,
-                             instance_offering_uuid=test_offering.uuid,
-                             l3_name=l3_net_list)
+                             instance_offering_uuid=test_offering.uuid)
     test_obj_dict.add_vm(vm)
     vm.check()
     return vm
@@ -32,22 +27,29 @@ def create_onlinechange_vm(test_stub=None, test_obj_dict=None):
 def check_cpu_mem(vm):
     zone_uuid = vm.get_vm().zoneUuid
 
-    total_cpu_used, total_mem_used = check_total_cpu_mem(zone_uuid)
+    available_cpu, available_memory = check_available_cpu_mem(zone_uuid)
     vm_outer_cpu, vm_outer_mem = vm.get_vm().cpuNum, vm.get_vm().memorySize
     vm_internal_cpu, vm_internal_mem = check_vm_internal_cpu_mem(vm)
 
-    return total_cpu_used, total_mem_used, vm_outer_cpu, vm_outer_mem, vm_internal_cpu, vm_internal_mem
+    return available_cpu, available_memory, vm_outer_cpu, vm_outer_mem, vm_internal_cpu, vm_internal_mem
 
 
-def check_total_cpu_mem(zone_uuid):
-    total_cpu_used = test_lib.lib_get_cpu_memory_capacity([zone_uuid]).totalCpu
-    total_mem_used = test_lib.lib_get_cpu_memory_capacity([zone_uuid]).totalMemory
-    return total_cpu_used, total_mem_used
+def check_available_cpu_mem(zone_uuid):
+    available_cpu = test_lib.lib_get_cpu_memory_capacity([zone_uuid]).availableCpu
+    available_memory = test_lib.lib_get_cpu_memory_capacity([zone_uuid]).availableMemory
+    return available_cpu, available_memory
 
 
 def check_vm_internal_cpu_mem(vm):
-    out = test_lib.lib_execute_command_in_vm(vm.get_vm(), "cat /proc/cpuinfo| grep 'processor'| wc -l")
-    vm_cpu = int(out.strip())
-    out = test_lib.lib_execute_command_in_vm(vm.get_vm(), "free -m |grep Mem")
-    vm_mem = int(out.split(" ")[1])
+    managerip = test_lib.lib_find_host_by_vm(vm.get_vm()).managementIp
+    vm_ip = vm.get_vm().vmNics[0].ip
+    get_cpu_cmd = "cat /proc/cpuinfo| grep 'processor'| wc -l"
+    get_mem_cmd = "free -m |grep Mem"
+    res = test_lib.lib_ssh_vm_cmd_by_agent(managerip, vm_ip, 'root',
+                'password', get_cpu_cmd)
+    vm_cpu = int(res.result.strip())
+    res = test_lib.lib_ssh_vm_cmd_by_agent(managerip, vm_ip, 'root',
+                'password', get_mem_cmd)
+    vm_mem = int(res.result.split()[1])
     return vm_cpu, vm_mem
+
--- integrationtest/vm/virt_plus/Hot_plug/test_hotplugin_cpu.py
+++ integrationtest/vm/virt_plus/Hot_plug/test_hotplugin_cpu.py
@@ -18,19 +18,19 @@ test_obj_dict = test_state.TestStateDict()
 
 def test():
     vm = hot_plug_common.create_onlinechange_vm(test_stub, test_obj_dict)
-    (total_cpu_before, total_mem_before, vm_outer_cpu_before, vm_outer_mem_before,
-     vm_interal_cpu_before, vm_interal_mem_before) =hot_plug_common.check_cpu_mem(vm)
+    (available_cpu_before, available_memory_before, vm_outer_cpu_before, vm_outer_mem_before,
+     vm_interal_cpu_before, vm_interal_mem_before) = hot_plug_common.check_cpu_mem(vm)
 
     vm_instance_offering = test_lib.lib_get_instance_offering_by_uuid(vm.get_vm().instanceOfferingUuid)
     vm_ops.update_vm(vm.get_vm().uuid, vm_instance_offering.cpuNum + 1, None)
     vm.update()
     time.sleep(10)
 
-    (total_cpu_after, total_mem_after, vm_outer_cpu_after, vm_outer_mem_after,
+    (available_cpu_after, available_memory_after, vm_outer_cpu_after, vm_outer_mem_after,
      vm_interal_cpu_after, vm_internal_mem_after) = hot_plug_common.check_cpu_mem(vm)
 
-    assert total_cpu_before == total_cpu_after - 1
-    assert total_mem_before == total_mem_after
+    assert available_cpu_before == available_cpu_after + 1
+    assert available_memory_before == available_memory_after
     assert vm_outer_cpu_before == vm_outer_cpu_after - 1
     assert vm_outer_mem_before == vm_outer_mem_after
     assert vm_interal_cpu_before == vm_interal_cpu_after - 1
