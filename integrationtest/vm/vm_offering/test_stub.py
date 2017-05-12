d0f54302847a41d91f4a280a86cc0c4c5dbb69b1
quarkonics <quarkonics@gmail.com>
2017-05-12 14:59:43 +0800

    Add vm_offering/test_hotplugin_cpu_ci{6,7}.py

--- integrationtest/vm/vm_offering/test_stub.py
+++ integrationtest/vm/vm_offering/test_stub.py
@@ -112,4 +112,32 @@ def share_admin_resource(account_uuid_list):
     acc_ops.share_resources(account_uuid_list, share_list)
 
 
+def check_cpu_mem(vm):
+    zone_uuid = vm.get_vm().zoneUuid
+
+    available_cpu, available_memory = check_available_cpu_mem(zone_uuid)
+    vm_outer_cpu, vm_outer_mem = vm.get_vm().cpuNum, vm.get_vm().memorySize
+    vm_internal_cpu, vm_internal_mem = check_vm_internal_cpu_mem(vm)
+
+    return available_cpu, available_memory, vm_outer_cpu, vm_outer_mem, vm_internal_cpu, vm_internal_mem
+
+
+def check_available_cpu_mem(zone_uuid):
+    available_cpu = test_lib.lib_get_cpu_memory_capacity([zone_uuid]).availableCpu
+    available_memory = test_lib.lib_get_cpu_memory_capacity([zone_uuid]).availableMemory
+    return available_cpu, available_memory
+
+
+def check_vm_internal_cpu_mem(vm):
+    managerip = test_lib.lib_find_host_by_vm(vm.get_vm()).managementIp
+    vm_ip = vm.get_vm().vmNics[0].ip
+    get_cpu_cmd = "cat /proc/cpuinfo| grep 'processor'| wc -l"
+    get_mem_cmd = "free -m |grep Mem"
+    res = test_lib.lib_ssh_vm_cmd_by_agent(managerip, vm_ip, 'root',
+                'password', get_cpu_cmd)
+    vm_cpu = int(res.result.strip())
+    res = test_lib.lib_ssh_vm_cmd_by_agent(managerip, vm_ip, 'root',
+                'password', get_mem_cmd)
+    vm_mem = int(res.result.split()[1])
+    return vm_cpu, vm_mem
 
