e106bf80aa587216d911da8361a8ee670daacbd3
quarkonics <quarkonics@gmail.com>
2017-05-14 14:44:47 +0800

    Update memory expectation with alignment

--- integrationtest/vm/vm_offering/test_hotplugin_cpu_memory_u14.py
+++ integrationtest/vm/vm_offering/test_hotplugin_cpu_memory_u14.py
@@ -28,7 +28,8 @@ def test():
      vm_interal_cpu_before, vm_interal_mem_before) = test_stub.check_cpu_mem(vm)
 
     vm_instance_offering = test_lib.lib_get_instance_offering_by_uuid(vm.get_vm().instanceOfferingUuid)
-    MEMchange = 128*1024*1024
+    MEMchange = 126*1024*1024
+    AlignedMemChange = 128*1024*1024
     vm_ops.update_vm(vm.get_vm().uuid, vm_instance_offering.cpuNum + 1, vm_instance_offering.memorySize + MEMchange)
     vm.update()
     time.sleep(10)
@@ -39,11 +40,11 @@ def test():
      vm_interal_cpu_after, vm_internal_mem_after) = test_stub.check_cpu_mem(vm)
 
     assert available_cpu_before == available_cpu_after + 1
-    assert available_memory_before == available_memory_after + MEMchange / int(test_lib.lib_get_provision_memory_rate())
+    assert available_memory_after + AlignedMemChange / int(test_lib.lib_get_provision_memory_rate()) in range(available_memory_before-1, available_memory_before+1)
     assert vm_outer_cpu_before == vm_outer_cpu_after - 1
-    assert vm_outer_mem_before == vm_outer_mem_after - MEMchange
+    assert vm_outer_mem_before == vm_outer_mem_after - AlignedMemChange
     assert vm_interal_cpu_before == vm_interal_cpu_after - 1
-    assert vm_interal_mem_before == vm_internal_mem_after - MEMchange/1024/1024
+    assert vm_interal_mem_before == vm_internal_mem_after - AlignedMemChange/1024/1024
     test_lib.lib_error_cleanup(test_obj_dict)
     test_util.test_pass('VM online change instance offering Test Pass')
 
