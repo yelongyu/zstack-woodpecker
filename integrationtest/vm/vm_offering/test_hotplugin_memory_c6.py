cd7f7ee7f2438a1011fbd2259554a1ffc197f5ba
quarkonics <quarkonics@gmail.com>
2017-05-14 12:01:24 +0800

    Adjust some vm_offering case to consider memory alignment

--- integrationtest/vm/vm_offering/test_hotplugin_memory_c6.py
+++ integrationtest/vm/vm_offering/test_hotplugin_memory_c6.py
@@ -35,15 +35,16 @@ def test():
 
     (available_cpu_after, available_memory_after, vm_outer_cpu_after, vm_outer_mem_after,
      vm_interal_cpu_after, vm_internal_mem_after) = test_stub.check_cpu_mem(vm)
+    AlignedMemChange = 128*1024*1024
 
     assert available_cpu_before == available_cpu_after
-    #test_util.test_logger("%s %s %s" % (available_memory_before, available_memory_after, MEMchange))
-    assert available_memory_before == available_memory_after + MEMchange / int(test_lib.lib_get_provision_memory_rate())
+    test_util.test_logger("%s %s %s" % (available_memory_before, available_memory_after, AlignedMemChange))
+    assert available_memory_after + AlignedMemChange / int(test_lib.lib_get_provision_memory_rate()) in range(available_memory_before-1, available_memory_before)
     assert vm_outer_cpu_before == vm_outer_cpu_after
-    assert vm_outer_mem_before == vm_outer_mem_after - MEMchange
+    assert vm_outer_mem_before == vm_outer_mem_after - AlignedMemChange
     assert vm_interal_cpu_before == vm_interal_cpu_after
     #test_util.test_logger("%s %s %s" % (vm_interal_mem_before, vm_internal_mem_after, MEMchange))
-    assert vm_interal_mem_before == vm_internal_mem_after - MEMchange/1024/1024
+    assert vm_interal_mem_before == vm_internal_mem_after - AlignedMemChange/1024/1024
     test_lib.lib_error_cleanup(test_obj_dict)
     test_util.test_pass('VM online change instance offering Test Pass')
 
--- integrationtest/vm/vm_offering/test_hotplugin_memory_u16.py
+++ integrationtest/vm/vm_offering/test_hotplugin_memory_u16.py
@@ -28,7 +28,7 @@ def test():
      vm_interal_cpu_before, vm_interal_mem_before) = test_stub.check_cpu_mem(vm)
 
     vm_instance_offering = test_lib.lib_get_instance_offering_by_uuid(vm.get_vm().instanceOfferingUuid)
-    MEMchange = 128*1024*1024
+    MEMchange = 191*1024*1024
     vm_ops.update_vm(vm.get_vm().uuid, None, vm_instance_offering.memorySize + MEMchange)
     vm.update()
     time.sleep(10)
@@ -36,14 +36,15 @@ def test():
 
     (available_cpu_after, available_memory_after, vm_outer_cpu_after, vm_outer_mem_after,
      vm_interal_cpu_after, vm_internal_mem_after) = test_stub.check_cpu_mem(vm)
+    AlignedMemChange = 128*1024*1024
 
     assert available_cpu_before == available_cpu_after
     #test_util.test_logger("%s %s %s" % (available_memory_before, available_memory_after, MEMchange))
-    assert available_memory_before == available_memory_after + MEMchange / int(test_lib.lib_get_provision_memory_rate())
+    assert available_memory_after + AlignedMemChange / int(test_lib.lib_get_provision_memory_rate()) in range(available_memory_before-1, available_memory_before+1)
     assert vm_outer_cpu_before == vm_outer_cpu_after
-    assert vm_outer_mem_before == vm_outer_mem_after - MEMchange
+    assert vm_outer_mem_before == vm_outer_mem_after - AlignedMemChange
     assert vm_interal_cpu_before == vm_interal_cpu_after
-    assert vm_interal_mem_before == vm_internal_mem_after - MEMchange/1024/1024
+    assert vm_interal_mem_before == vm_internal_mem_after - AlignedMemChange/1024/1024
     test_lib.lib_error_cleanup(test_obj_dict)
     test_util.test_pass('VM online change instance offering Test Pass')
 
