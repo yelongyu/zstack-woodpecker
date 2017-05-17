97ec8ec6e6e498c0833846cc50f6ef1813e853b9
Lijin Xiong <legion.xiong@gmail.com>
2017-05-17 11:29:53 +0800

    1. "Add hotplugin test cases; 2. Update gitignore with '.project' and '.pydevproject'

---
+++ integrationtest/vm/vm_offering/test_hotplugin_cpu_memory_shutdown.py
@@ -0,0 +1,62 @@
+'''
+@author: Lijin Xiong
+'''
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.vm_operations as vm_ops
+import time
+
+_config_ = {
+        'timeout' : 1000,
+        'noparallel' : False
+        }
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+interval = 0.5
+
+def test():
+    new_offering = test_lib.lib_create_instance_offering(cpuNum = 1,\
+            cpuSpeed = 111, memorySize = 1024 * 1024 * 1024)
+    test_obj_dict.add_instance_offering(new_offering)
+    vm = test_stub.create_vm(vm_name = 'ckvmoffering-c7-64', image_name = "imageName_i_c7", instance_offering_uuid=new_offering.uuid)
+    test_obj_dict.add_vm(vm)
+    vm.check()
+
+    (available_cpu_before, available_memory_before, vm_outer_cpu_before, vm_outer_mem_before,
+     vm_interal_cpu_before, vm_interal_mem_before) = test_stub.check_cpu_mem(vm)
+
+    vm_instance_offering = test_lib.lib_get_instance_offering_by_uuid(vm.get_vm().instanceOfferingUuid)
+    MEMchange = 126*1024*1024
+    AlignedMemChange = 128*1024*1024
+    vm_ops.update_vm(vm.get_vm().uuid, vm_instance_offering.cpuNum + 1, vm_instance_offering.memorySize + MEMchange)
+    vm.update()
+    time.sleep(10)
+    vm.check()
+
+    vm_ops.stop_vm(vm.get_vm().uuid)
+    for _ in xrange(60):
+        if vm.get_vm().state == 'Stopped':
+            break
+        else:
+            time.sleep(interval)
+
+    vm_ops.start_vm(vm.get_vm().uuid)
+    vm.check()
+
+    (available_cpu_after, available_memory_after, vm_outer_cpu_after, vm_outer_mem_after,
+     vm_interal_cpu_after, vm_internal_mem_after) = test_stub.check_cpu_mem(vm)
+
+    assert available_cpu_before == available_cpu_after + 1
+    assert available_memory_after + AlignedMemChange / int(test_lib.lib_get_provision_memory_rate()) in range(available_memory_before-1, available_memory_before+1)
+    assert vm_outer_cpu_before == vm_outer_cpu_after - 1
+    assert vm_outer_mem_before == vm_outer_mem_after - AlignedMemChange
+    assert vm_interal_cpu_before == vm_interal_cpu_after - 1
+    assert vm_interal_mem_before == vm_internal_mem_after - AlignedMemChange/1024/1024
+    test_lib.lib_error_cleanup(test_obj_dict)
+    test_util.test_pass('VM online change instance offering Test Pass')
+
+
+def error_cleanup():
+    test_lib.lib_error_cleanup(test_obj_dict)
