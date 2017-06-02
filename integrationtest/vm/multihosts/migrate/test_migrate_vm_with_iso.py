6551ea7aeae6cc4c6fb937e202cdebe753a0562f
sherry546 <546102561@qq.com>
2017-06-02 09:42:45 +0800

    add new case about live migrate with iso

---
+++ integrationtest/vm/multihosts/migrate/test_migrate_vm_with_iso.py
@@ -0,0 +1,63 @@
+'''
+
+New Integration test for testing vm migration between hosts when attch ISO. 
+
+@author: ChenyuanXu 
+'''
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.volume_operations as vol_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+import zstackwoodpecker.operations.image_operations as img_ops
+import zstackwoodpecker.zstack_test.zstack_test_image as test_image
+import zstackwoodpecker.test_state as test_state
+import apibinding.inventory as inventory
+import os
+
+vm = None
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+def test():
+    global vm
+    vm = test_stub.create_vr_vm('migrate_vm', 'imageName_s', 'l3VlanNetwork2')
+    vm.check()
+
+    vm_inv = vm.get_vm()
+    vm_uuid = vm_inv.uuid
+
+    test_util.test_dsc('Add ISO Image')
+    cond = res_ops.gen_query_conditions('name', '=', 'sftp') 
+    bs_uuid = res_ops.query_resource(res_ops.BACKUP_STORAGE, cond)[0].uuid
+
+    img_option = test_util.ImageOption()
+    img_option.set_name('iso')
+    img_option.set_backup_storage_uuid_list([bs_uuid])
+    os.system("echo fake iso for test only >  %s/apache-tomcat/webapps/zstack/static/test.iso" % (os.environ.get('zstackInstallPath')))
+    img_option.set_url('http://%s:8080/zstack/static/test.iso' % (os.environ.get('node1Ip')))
+    image_inv = img_ops.add_iso_template(img_option)
+    image = test_image.ZstackTestImage()
+    image.set_image(image_inv)
+    image.set_creation_option(img_option)
+
+    test_obj_dict.add_image(image)
+
+    test_util.test_dsc('Attach ISO to VM')
+    cond = res_ops.gen_query_conditions('name', '=', 'iso')
+    iso_uuid = res_ops.query_resource(res_ops.IMAGE, cond)[0].uuid
+    img_ops.attach_iso(iso_uuid, vm_uuid)
+
+    test_util.test_dsc('Migrate VM')
+    test_stub.migrate_vm_to_random_host(vm)
+    vm.check()
+
+    img_ops.detach_iso(vm_uuid)
+    image.delete()
+    image.expunge()
+    test_obj_dict.rm_image(image)
+    vm.destroy()
+    test_util.test_pass('Migrate VM Test Success When Attach ISO')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    test_lib.lib_error_cleanup(test_obj_dict)
