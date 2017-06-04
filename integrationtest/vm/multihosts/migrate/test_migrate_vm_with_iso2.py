c266571691986eff39be12ebf016127d8e8b1b15
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-06-04 15:34:46 +0800

    remove sftp backup storage limitation for migrate with iso2

--- integrationtest/vm/multihosts/migrate/test_migrate_vm_with_iso2.py
+++ integrationtest/vm/multihosts/migrate/test_migrate_vm_with_iso2.py
@@ -37,8 +37,8 @@ def test():
 
 
     test_util.test_dsc('Add ISO Image')
-    cond = res_ops.gen_query_conditions('name', '=', 'sftp')
-    bs_uuid = res_ops.query_resource(res_ops.BACKUP_STORAGE, cond)[0].uuid
+    #cond = res_ops.gen_query_conditions('name', '=', 'sftp')
+    bs_uuid = res_ops.query_resource(res_ops.BACKUP_STORAGE)[0].uuid
 
 
     img_option = test_util.ImageOption()
