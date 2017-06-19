377900ffd37b107d5880174b87fdbe3ce92a40a3
Ye Tian <ye.tian@zstack.io>
2017-06-19 19:38:08 +0800

    update zstack_update_iso.sh file

--- integrationtest/vm/installation/zstack_update_iso.sh
+++ integrationtest/vm/installation/zstack_update_iso.sh
@@ -2,13 +2,16 @@
 #set -x
 mkdir -p /opt/zstack-dvd && cd /opt/
 
-wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/ 2>/dev/null
+#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/ 2>/dev/null
+wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.10/latest/ 2>/dev/null
 sleep 3
 latest_iso=$(cat index.html |awk -F '[><]' '/DVD/ {print $5}')
-wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/$latest_iso 2>/dev/null
+#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/$latest_iso 2>/dev/null
+wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.10/latest/$latest_iso 2>/dev/null
 rm -f index.html
 
-wget http://www.mevoco.com/downloads/scripts/zstack-upgrade 2>/dev/null
+#wget http://www.mevoco.com/downloads/scripts/zstack-upgrade 2>/dev/null
+wget http://cdn.zstack.io/product_downloads/scripts/zstack-upgrade 2>/dev/null
 sleep 3
 bash zstack-upgrade -r $latest_iso 2>/dev/null
 
