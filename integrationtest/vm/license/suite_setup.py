c49c60e3cd0a23ace016e15dfa977260c2fdc75f
quarkonics <quarkonics@gmail.com>
2017-06-20 14:54:09 +0800

    Add license test suite

---
+++ integrationtest/vm/license/suite_setup.py
@@ -0,0 +1,31 @@
+'''
+
+@author: Frank
+'''
+import os.path
+import zstacklib.utils.linux as linux
+import zstacklib.utils.http as  http
+import zstacktestagent.plugins.host as host_plugin
+import zstacktestagent.testagent as testagent
+
+import zstackwoodpecker.operations.scenario_operations as scenario_operations
+import zstackwoodpecker.operations.deploy_operations as deploy_operations
+import zstackwoodpecker.operations.config_operations as config_operations
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_util as test_util
+
+USER_PATH = os.path.expanduser('~')
+EXTRA_SUITE_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_suite_setup_config.sh' % USER_PATH
+EXTRA_HOST_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_host_setup_config.sh' % USER_PATH
+def test():
+    if test_lib.scenario_config != None and test_lib.scenario_file != None and not os.path.exists(test_lib.scenario_file):
+        scenario_operations.deploy_scenario(test_lib.all_scenario_config, test_lib.scenario_file, test_lib.deploy_config)
+        test_util.test_skip('Suite Setup Success')
+    if test_lib.scenario_config != None and test_lib.scenario_destroy != None:
+        scenario_operations.destroy_scenario(test_lib.all_scenario_config, test_lib.scenario_destroy)
+
+    test_lib.setup_plan.deploy_test_agent()
+    test_lib.setup_plan.execute_plan_without_deploy_test_agent()
+
+    test_util.test_pass('Suite Setup Success')
+
