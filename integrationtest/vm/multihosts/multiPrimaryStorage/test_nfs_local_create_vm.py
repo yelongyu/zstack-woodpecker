c963a4d13d6bfd8b26dbd7764fbea39db6d822e1
ads6ads6 <ads5ads5@yeah.net>
2017-06-21 20:26:52 +0800

    Add multips nfs local test cases

---
+++ integrationtest/vm/multihosts/multiPrimaryStorage/test_nfs_local_create_vm.py
@@ -0,0 +1,44 @@
+'''
+@author: FangSun
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.resource_operations as res_ops
+import apibinding.inventory as inventory
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.primarystorage_operations as ps_ops
+
+_config_ = {
+        'timeout' : 3000,
+        'noparallel' : True
+        }
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+new_ps_list = []
+VM_COUNT = 10
+
+
+def test():
+
+    test_util.test_dsc("Create {0} vm ".format(VM_COUNT))
+    vm_list = test_stub.create_multi_vms(name_prefix='test-', count=VM_COUNT)
+    for vm in vm_list:
+        test_obj_dict.add_vm(vm)
+
+    test_util.test_dsc("Check all root volumes in LOCAL PS")
+    for vm in vm_list:
+        ps_uuid = vm.get_vm().allVolumes[0].primaryStorageUuid
+        ps = res_ops.get_resource(res_ops.PRIMARY_STORAGE, uuid=ps_uuid)
+        assert ps.type == inventory.LOCAL_STORAGE_TYPE
+
+    test_util.test_pass('Multi PrimaryStorage Test Pass')
+
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
+    if new_ps_list:
+        for new_ps in new_ps_list:
+            ps_ops.detach_primary_storage(new_ps.uuid, new_ps.attachedClusterUuids[0])
+            ps_ops.delete_primary_storage(new_ps.uuid)
---
+++ integrationtest/vm/multihosts/multiPrimaryStorage/test_nfs_local_create_vm_volume.py
@@ -0,0 +1,45 @@
+'''
+@author: FangSun
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.resource_operations as res_ops
+import apibinding.inventory as inventory
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.primarystorage_operations as ps_ops
+
+_config_ = {
+        'timeout' : 3000,
+        'noparallel' : True
+        }
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+new_ps_list = []
+VM_COUNT = 10
+DATA_VOLUME_NUMBER = 10
+
+def test():
+
+    test_util.test_dsc("Create {0} vm ".format(VM_COUNT))
+    vm_list = test_stub.create_multi_vms(name_prefix='test-', count=VM_COUNT, data_volume_number=DATA_VOLUME_NUMBER)
+    for vm in vm_list:
+        test_obj_dict.add_vm(vm)
+
+    test_util.test_dsc("Check all root volumes in LOCAL PS")
+    for vm in vm_list:
+        root_volume = test_lib.lib_get_root_volume(vm)
+        ps_uuid = root_volume.primaryStorageUuid
+        ps = res_ops.get_resource(res_ops.PRIMARY_STORAGE, uuid=ps_uuid)
+        assert ps.type == inventory.LOCAL_STORAGE_TYPE
+
+    test_util.test_pass('Multi PrimaryStorage Test Pass')
+
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
+    if new_ps_list:
+        for new_ps in new_ps_list:
+            ps_ops.detach_primary_storage(new_ps.uuid, new_ps.attachedClusterUuids[0])
+            ps_ops.delete_primary_storage(new_ps.uuid)
