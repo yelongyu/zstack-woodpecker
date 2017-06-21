0380420aa4c5009a5be2831c20f7e6935f8738de
Ye Tian <ye.tian@zstack.io>
2017-06-21 10:24:19 +0800

    update mevoco_update_iso.sh and zstack_update_iso.sh

--- integrationtest/vm/installation/mevoco_update_iso.sh
+++ integrationtest/vm/installation/mevoco_update_iso.sh
@@ -2,10 +2,12 @@
 #set -x
 mkdir -p /opt/zstack-dvd && cd /opt/
 
-wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_1.9/latest/ 2>/dev/null
+#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_1.9/latest/ 2>/dev/null
+wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_2.0/latest/ 2>/dev/null
 sleep 3
 latest_iso=$(cat index.html |awk -F '[><]' '/DVD/ {print $5}')
-wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_1.9/latest/$latest_iso 2>/dev/null
+#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_1.9/latest/$latest_iso 2>/dev/null
+wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_2.0/latest/$latest_iso 2>/dev/null
 rm -f index.html
 
 wget http://cdn.zstack.io/product_downloads/scripts/zstack-upgrade 2>/dev/null
--- integrationtest/vm/installation/zstack_update_iso.sh
+++ integrationtest/vm/installation/zstack_update_iso.sh
@@ -2,12 +2,12 @@
 #set -x
 mkdir -p /opt/zstack-dvd && cd /opt/
 
-wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/ 2>/dev/null
-#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.10/latest/ 2>/dev/null
+#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/ 2>/dev/null
+wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_2.0/latest/ 2>/dev/null
 sleep 3
 latest_iso=$(cat index.html |awk -F '[><]' '/DVD/ {print $5}')
-wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/$latest_iso 2>/dev/null
-#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.10/latest/$latest_iso 2>/dev/null
+#wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_community_iso_1.9/latest/$latest_iso 2>/dev/null
+wget -q –progress=TYPE  http://172.20.198.234/mirror/zstack_enterprise_iso_2.0/latest/$latest_iso 2>/dev/null
 rm -f index.html
 
 #wget http://www.mevoco.com/downloads/scripts/zstack-upgrade 2>/dev/null
