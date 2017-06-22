37e61944e2ba1086073c36a4ec8804bba558acb1
ads6ads6 <ads5ads5@yeah.net>
2017-06-22 10:38:50 +0800

    Add test_nfs_local_volume_attach.py

---
+++ integrationtest/vm/multihosts/multiPrimaryStorage/test_nfs_local_volume_attach.py
@@ -0,0 +1,55 @@
+'''
+@author: FangSun
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.primarystorage_operations as ps_ops
+
+_config_ = {
+        'timeout' : 3000,
+        'noparallel' : True
+        }
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+VM_COUNT = 1
+VOLUME_NUMBER = 10
+new_ps_list = []
+
+
+def test():
+    env = test_stub.TwoPrimaryStorageEnv(test_object_dict=test_obj_dict,
+                                         first_ps_vm_number=VM_COUNT,
+                                         first_ps_volume_number=VOLUME_NUMBER,
+                                         second_ps_volume_number=VOLUME_NUMBER)
+    env.check_env()
+    env.deploy_env()
+    first_ps_vm = env.first_ps_vm_list[0]
+    first_ps_volume_list = env.first_ps_volume_list
+    second_ps_volume_list = env.second_ps_volume_list
+    if env.new_ps:
+        new_ps_list.append(env.second_ps)
+
+    all_volume_list = first_ps_volume_list + second_ps_volume_list
+    test_util.test_dsc("Attach all volumes to first ps vm")
+    for volume in all_volume_list:
+        volume.attach(first_ps_vm)
+
+    for volume in all_volume_list:
+        volume.check()
+
+    for volume in all_volume_list:
+        volume.detach()
+
+    for volume in all_volume_list:
+        volume.check()
+
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
+    if new_ps_list:
+        for new_ps in new_ps_list:
+            ps_ops.detach_primary_storage(new_ps.uuid, new_ps.attachedClusterUuids[0])
+            ps_ops.delete_primary_storage(new_ps.uuid)
