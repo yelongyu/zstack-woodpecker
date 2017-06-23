176e939970afe5eca584bc293e3673398a7d333d
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-06-23 20:07:56 +0800

    fix test_vm_none_mn_net_discon_connect_host_vm_status test

--- integrationtest/vm/multihosts/ha/test_vm_none_mn_net_discon_connect_host_vm_status.py
+++ integrationtest/vm/multihosts/ha/test_vm_none_mn_net_discon_connect_host_vm_status.py
@@ -86,18 +86,28 @@ def test():
     test_util.test_logger("host %s is disconnecting" %(host_ip))
 
     test_stub.down_host_network(host_ip, test_lib.all_scenario_config)
-
     test_util.test_logger("wait for 30 seconds")
     time.sleep(30)
-
     test_stub.up_host_network(host_ip, test_lib.all_scenario_config)
 
-    vm.set_state(vm_header.STOPPED)
-    vm.check()
+    time.sleep(120)
+
+    cmd = "nohup zstack-ctl start &"
+    host_username = os.environ.get('hostUsername')
+    host_password = os.environ.get('hostPassword')
+    if not test_lib.lib_execute_ssh_cmd(mn_ip, host_username, host_password, cmd,  timeout = 300):
+        test_util.test_fail("CMD:%s execute failed on %s" %(cmd, mn_ip))
+
+    time.sleep(120)
+    cond = res_ops.gen_query_conditions('uuid', '=', vm.vm.uuid)
+    if not res_ops.query_resource(res_ops.VM_INSTANCE, cond)[0].state == "Running":
+        test_util.test_fail("vm is not stopped as expected.")
+
     vm.destroy()
 
 
     #check mn service works normally
+    time.sleep(20)
     vm.create()
     vm.check()
     vm.destroy()
