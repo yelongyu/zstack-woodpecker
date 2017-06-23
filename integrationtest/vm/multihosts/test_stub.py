5b53c778fc3eda8295f9df5df2b492f4ef731a8c
ads6ads6 <ads5ads5@yeah.net>
2017-06-23 15:46:49 +0800

    Change VM image for multi ps test case

--- integrationtest/vm/multihosts/test_stub.py
+++ integrationtest/vm/multihosts/test_stub.py
@@ -150,12 +150,12 @@ def create_multi_vms(name_prefix='', count=10, host_uuid=None, ps_uuid=None, dat
     vm_list = []
     for i in xrange(count):
         if not data_volume_number:
-            vm = create_vm_with_random_offering(name_prefix+"{}".format(i), image_name='imageName_s',
+            vm = create_vm_with_random_offering(name_prefix+"{}".format(i), image_name='imageName_net',
                                                 l3_name='l3VlanNetwork2', host_uuid=host_uuid, ps_uuid=ps_uuid)
         else:
             disk_offering_list = res_ops.get_resource(res_ops.DISK_OFFERING)
             disk_offering_uuids = [random.choice(disk_offering_list).uuid for _ in xrange(data_volume_number)]
-            vm = create_vm_with_random_offering(name_prefix+"{}".format(i), image_name='imageName_s',
+            vm = create_vm_with_random_offering(name_prefix+"{}".format(i), image_name='imageName_net',
                                                 l3_name='l3VlanNetwork2', host_uuid=host_uuid, ps_uuid=ps_uuid,
                                                 disk_offering_uuids=disk_offering_uuids)
         vm_list.append(vm)
