6f4fe4f8c3104e8eb9e24dd56a56c8379f02a9f0
quarkonics <quarkonics@gmail.com>
2017-06-28 19:25:30 +0800

    Adjust virtualrouter/scheduler/test_create_reboot_vm_simple_scheduler.py

--- integrationtest/vm/virtualrouter/scheduler/test_create_reboot_vm_simple_scheduler.py
+++ integrationtest/vm/virtualrouter/scheduler/test_create_reboot_vm_simple_scheduler.py
@@ -13,16 +13,21 @@ import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 test_stub = test_lib.lib_get_test_stub()
 vm = None
-schd = None
+schd_job = None
+schd_trigger = None
 
 
 def test():
     global vm
-    global schd
+    global schd_job
+    global schd_trigger
     vm = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
     vm.check()
     start_date = int(time.time())
-    schd = vm_ops.reboot_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_reboot_vm_scheduler', start_date+60, 120)
+    schd_job = schd_ops.create_scheduler_job('simple_reboot_vm_scheduler', 'simple_reboot_vm_scheduler', vm.get_vm().uuid, 'rebootVm', None)
+    schd_trigger = schd_ops.create_scheduler_trigger('simple_reboot_vm_scheduler', start_date+60, None, 120, 'simple')
+    schd_ops.add_scheduler_job_to_trigger(schd_trigger.uuid, schd_job.uuid)
+    #schd = vm_ops.reboot_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_reboot_vm_scheduler', start_date+60, 120)
     test_stub.sleep_util(start_date+58)
     for i in range(0, 58):
         if test_lib.lib_find_in_local_management_server_log(start_date+i, '[msg received]: {"org.zstack.header.vm.RebootVmInstanceMsg', vm.get_vm().uuid):
@@ -36,17 +41,21 @@ def test():
     if not test_lib.lib_find_in_local_management_server_log(start_date+60, '[msg received]: {"org.zstack.header.vm.RebootVmInstanceMsg', vm.get_vm().uuid):
         test_util.test_fail('VM is expected to reboot start from %s' % (start_date+60))
 
-    schd_ops.delete_scheduler(schd.uuid)
+    schd_ops.delete_scheduler_job(schd_job.uuid)
+    schd_ops.delete_scheduler_trigger(schd_trigger.uuid)
     vm.destroy()
     test_util.test_pass('Create Simple VM Reboot Scheduler Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
     global vm
-    global schd
+    global schd_trigger
+    global schd_job
 
     if vm:
         vm.destroy()
 
-    if schd:
-	schd_ops.delete_scheduler(schd.uuid)
+    if schd_job:
+	schd_ops.delete_scheduler_job(schd_job.uuid)
+    if schd_trigger:
+	schd_ops.delete_scheduler_trigger(schd_trigger.uuid)
