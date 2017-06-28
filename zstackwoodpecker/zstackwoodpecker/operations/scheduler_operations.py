a839e9c2607094d96f9452a0e9cf61406950e2fa
quarkonics <quarkonics@gmail.com>
2017-06-28 19:24:48 +0800

    Add new scheduler API

--- zstackwoodpecker/zstackwoodpecker/operations/scheduler_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/scheduler_operations.py
@@ -14,6 +14,59 @@ import config_operations
 import os
 import inspect
 
+def create_scheduler_job(name, description, target_uuid, type, parameters, session_uuid = None):
+    action = api_actions.CreateSchedulerJobAction()
+    action.name = name
+    action.description = description
+    action.targetResourceUuid = target_uuid
+    action.type = type
+    action.parameters = parameters
+    test_util.action_logger('Create [Scheduler Job:] %s [%s] %s' % (name, target_uuid, type))
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+    test_util.test_logger('[Scheduler Job:] %s is created.' % evt.uuid)
+    return evt
+
+def del_scheduler_job(uuid, session_uuid = None):
+    action = api_actions.DeleteSchedulerJobAction()
+    action.uuid = uuid
+    test_util.action_logger('Delete [Scheduler Job:] %s' % uuid)
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+    test_util.test_logger('[Scheduler Job:] %s is deleted.' % uuid)
+
+def create_scheduler_trigger(name, start_time, repeat_count, interval, type, session_uuid = None):
+    action = api_actions.CreateSchedulerTriggerAction()
+    action.name = name
+    action.startTime = start_time
+    action.repeatCount = repeat_count
+    action.schedulerInterval = interval
+    action.schedulerType = type
+    test_util.action_logger('Create [Scheduler Trigger:] %s [%s] %s %s' % (name, start_time, repeat_count, interval))
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+    test_util.test_logger('[Scheduler Trigger:] %s is created.' % evt.uuid)
+    return evt
+
+def del_scheduler_trigger(uuid, session_uuid = None):
+    action = api_actions.DeleteSchedulerTriggerAction()
+    action.uuid = uuid
+    test_util.action_logger('Delete [Scheduler Trigger:] %s' % uuid)
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+    test_util.test_logger('[Scheduler Trigger:] %s is deleted.' % uuid)
+
+def add_scheduler_job_to_trigger(trigger_uuid, job_uuid, session_uuid = None):
+    action = api_actions.AddSchedulerJobToSchedulerTriggerAction()
+    action.schedulerTriggerUuid = trigger_uuid
+    action.schedulerJobUuid = job_uuid
+    test_util.action_logger('Add [Scheduler Job:] %s to [Scheduler Trigger:] %s ' % (job_uuid, trigger_uuid))
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+    test_util.test_logger('[Scheduler Job:] %s is added to [Scheduler Trigger:] %s.' % (job_uuid, trigger_uuid))
+
+def remove_scheduler_job_from_trigger(trigger_uuid, job_uuid, session_uuid = None):
+    action = api_actions.RemoveSchedulerJobFromSchedulerTriggerAction()
+    action.schedulerTriggerUuid = trigger_uuid
+    action.schedulerJobUuid = job_uuid
+    test_util.action_logger('Remove [Scheduler Job:] %s from [Scheduler Trigger:] %s ' % (job_uuid, trigger_uuid))
+    evt = account_operations.execute_action_with_session(action, session_uuid) 
+
 def delete_scheduler(uuid, session_uuid = None):
     action = api_actions.DeleteSchedulerAction()
     action.uuid = uuid
