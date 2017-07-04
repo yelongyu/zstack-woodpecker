173584b0a58d8e4ed6a9dcf8e983f8d893ec9478
quarkonics <quarkonics@gmail.com>
2017-07-04 13:46:36 +0800

    Update virtualrouter/scheduler/test_create_start_stop_vm_simple_repeatcount_scheduler.py

--- integrationtest/vm/virtualrouter/scheduler/test_create_start_stop_vm_simple_repeatcount_scheduler.py
+++ integrationtest/vm/virtualrouter/scheduler/test_create_start_stop_vm_simple_repeatcount_scheduler.py
@@ -13,20 +13,31 @@ import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 test_stub = test_lib.lib_get_test_stub()
 vm = None
-schd1 = None
-schd2 = None
+schd_job1 = None
+schd_job2 = None
+schd_trigger1 = None
+schd_trigger2 = None
 
 
 def test():
     global vm
-    global schd1
-    global schd2
+    global schd_job1
+    global schd_job2
+    global schd_trigger1
+    global schd_trigger2
+
     vm = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
     start_date = int(time.time())
-    schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+60, 120, 3)
+    schd_job1 = schd_ops.create_scheduler_job('simple_stop_vm_scheduler', 'simple_stop_vm_scheduler', vm.get_vm().uuid, 'stopVm', None)
+    schd_trigger1 = schd_ops.create_scheduler_trigger('simple_stop_vm_scheduler', start_date+60, 3, 120, 'simple')
+    schd_ops.add_scheduler_job_to_trigger(schd_trigger1.uuid, schd_job1.uuid)
+    #schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+60, 120, 3)
 #    if schd1.stopTime != start_date + 60 + 120 * 3:
 #        test_util.test_fail('[scheduler:] %s is expected have stopTime as %s' % (schd1.uuid, start_date + 60 + 120 * 3))
-    schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+120, 120, 2)
+    schd_job2 = schd_ops.create_scheduler_job('simple_start_vm_scheduler', 'simple_start_vm_scheduler', vm.get_vm().uuid, 'startVm', None)
+    schd_trigger2 = schd_ops.create_scheduler_trigger('simple_start_vm_scheduler', start_date+120, 2, 120, 'simple')
+    schd_ops.add_scheduler_job_to_trigger(schd_trigger2.uuid, schd_job2.uuid)
+    #schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+120, 120, 2)
 #    if schd2.stopTime != start_date + 120 + 120 * 2:
 #        test_util.test_fail('[scheduler:] %s is expected have stopTime as %s' % (schd2.uuid, start_date + 120 + 120 * 2))
 
@@ -56,22 +67,31 @@ def test():
         if vm.get_vm().state != 'Starting' and vm.get_vm().state != 'Running':
             test_util.test_fail('VM is expected to start')
 
-    schd_ops.delete_scheduler(schd1.uuid)
-    schd_ops.delete_scheduler(schd2.uuid)
+    schd_ops.del_scheduler_job(schd_job1.uuid)
+    schd_ops.del_scheduler_trigger(schd_trigger1.uuid)
+    schd_ops.del_scheduler_job(schd_job2.uuid)
+    schd_ops.del_scheduler_trigger(schd_trigger2.uuid)
+
     vm.destroy()
     test_util.test_pass('Create Simple VM Stop Start Scheduler Repeat Count Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
     global vm
-    global schd1
-    global schd2
+    global schd_job1
+    global schd_job2
+    global schd_trigger1
+    global schd_trigger2
 
     if vm:
         vm.destroy()
 
-    if schd1:
-	schd_ops.delete_scheduler(schd1.uuid)
+    if schd_job1:
+	schd_ops.del_scheduler_job(schd_job1.uuid)
+    if schd_trigger1:
+	schd_ops.del_scheduler_trigger(schd_trigger1.uuid)
+    if schd_job2:
+	schd_ops.del_scheduler_job(schd_job2.uuid)
+    if schd_trigger2:
+	schd_ops.del_scheduler_trigger(schd_trigger2.uuid)
 
-    if schd2:
-	schd_ops.delete_scheduler(schd2.uuid)
