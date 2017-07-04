b7613ce521648ad5ac6bf9cbb8a36835703d5497
quarkonics <quarkonics@gmail.com>
2017-07-04 16:41:12 +0800

    Update virtualrouter/scheduler/test_change_vm_owner_simple_scheduler_state.py

--- integrationtest/vm/virtualrouter/scheduler/test_change_vm_owner_simple_scheduler_state.py
+++ integrationtest/vm/virtualrouter/scheduler/test_change_vm_owner_simple_scheduler_state.py
@@ -14,12 +14,14 @@ import zstackwoodpecker.operations.resource_operations as res_ops
 
 test_stub = test_lib.lib_get_test_stub()
 vm = None
-schd1 = None
-schd2 = None
+schd_job1 = None
+schd_job2 = None
+schd_trigger1 = None
+schd_trigger2 = None
 
 def check_scheduler_state(schd, target_state):
     conditions = res_ops.gen_query_conditions('uuid', '=', schd.uuid)
-    schd_state = res_ops.query_resource(res_ops.SCHEDULER, conditions)[0].state
+    schd_state = res_ops.query_resource(res_ops.SCHEDULERJOB, conditions)[0].state
     if schd_state != target_state:
         test_util.test_fail('check scheduler state, it is expected to be %s, but it is %s' % (target_state, schd_state))
 
@@ -37,20 +39,29 @@ def check_scheduler_msg(msg, timestamp):
 
 def test():
     global vm
-    global schd1
-    global schd2
+    global schd_job1
+    global schd_job2
+    global schd_trigger1
+    global schd_trigger2
     global new_account
 
     vm = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
     start_date = int(time.time())
-    schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+30, 60)
-    schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+60, 60)
+    schd_job1 = schd_ops.create_scheduler_job('simple_stop_vm_scheduler', 'simple_stop_vm_scheduler', vm.get_vm().uuid, 'stopVm', None)
+    schd_trigger1 = schd_ops.create_scheduler_trigger('simple_stop_vm_scheduler', start_date+30, None, 60, 'simple')
+    schd_ops.add_scheduler_job_to_trigger(schd_trigger1.uuid, schd_job1.uuid)
+    #schd1 = vm_ops.stop_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_stop_vm_scheduler', start_date+30, 60)
+
+    schd_job2 = schd_ops.create_scheduler_job('simple_start_vm_scheduler', 'simple_start_vm_scheduler', vm.get_vm().uuid, 'startVm', None)
+    schd_trigger2 = schd_ops.create_scheduler_trigger('simple_start_vm_scheduler', start_date+60, None, 60, 'simple')
+    schd_ops.add_scheduler_job_to_trigger(schd_trigger2.uuid, schd_job2.uuid)
+    #schd2 = vm_ops.start_vm_scheduler(vm.get_vm().uuid, 'simple', 'simple_start_vm_scheduler', start_date+60, 60)
 
     test_stub.sleep_util(start_date+130)
 
     test_util.test_dsc('check scheduler state after creating scheduler')
-    check_scheduler_state(schd1, 'Enabled')
-    check_scheduler_state(schd2, 'Enabled')
+    check_scheduler_state(schd_job1, 'Enabled')
+    check_scheduler_state(schd_job2, 'Enabled')
     if not check_scheduler_msg('run scheduler for job: StopVmInstanceJob', start_date+30):
         test_util.test_fail('StopVmInstanceJob not executed at expected timestamp range')
     if not check_scheduler_msg('run scheduler for job: StartVmInstanceJob', start_date+60):
@@ -66,15 +77,17 @@ def test():
     test_stub.sleep_util(except_start_time + 130)
  
     test_util.test_dsc('check scheduler state after changing the owner of vm')
-    check_scheduler_state(schd1, 'Disabled')
-    check_scheduler_state(schd2, 'Disabled')
+    check_scheduler_state(schd_job1, 'Disabled')
+    check_scheduler_state(schd_job2, 'Disabled')
     if check_scheduler_msg('run scheduler for job: StopVmInstanceJob', except_start_time+30):
         test_util.test_fail('StopVmInstanceJob executed at unexpected timestamp range')
     if check_scheduler_msg('run scheduler for job: StartVmInstanceJob', except_start_time+60):
         test_util.test_fail('StartVmInstanceJob executed at unexpected timestamp range')
 
-    schd_ops.delete_scheduler(schd1.uuid)
-    schd_ops.delete_scheduler(schd2.uuid)
+    schd_ops.del_scheduler_job(schd_job1.uuid)
+    schd_ops.del_scheduler_trigger(schd_trigger1.uuid)
+    schd_ops.del_scheduler_job(schd_job2.uuid)
+    schd_ops.del_scheduler_trigger(schd_trigger2.uuid)
     vm.destroy()
     account_operations.delete_account(new_account.uuid)
 
@@ -83,18 +96,23 @@ def test():
 #Will be called only if exception happens in test().
 def error_cleanup():
     global vm
-    global schd1
-    global schd2
+    global schd_job1
+    global schd_job2
+    global schd_trigger1
+    global schd_trigger2
     global new_account
  
     if vm:
         vm.destroy()
 
-    if schd1:
-	schd_ops.delete_scheduler(schd1.uuid)
-
-    if schd2:
-	schd_ops.delete_scheduler(schd2.uuid)
+    if schd_job1:
+	schd_ops.del_scheduler_job(schd_job1.uuid)
+    if schd_trigger1:
+	schd_ops.del_scheduler_trigger(schd_trigger1.uuid)
+    if schd_job2:
+	schd_ops.del_scheduler_job(schd_job2.uuid)
+    if schd_trigger2:
+	schd_ops.del_scheduler_trigger(schd_trigger2.uuid)
 
     if new_account:
        account_operations.delete_account(new_account.uuid) 
