c68121a5a6d92b60431bd874058dc5a3678764af
Ye Tian <ye.tian@zstack.io>
2017-07-06 20:53:59 +0800

    update test_zs_upgd_cnt_1.5_latest_on_cos7.py 1.5-1.10 2.0-to-2.0.0

--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.10_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.10_latest_on_cos7.py
@@ -45,7 +45,7 @@ def test():
     test_stub.update_iso(vm_ip, tmp_file, iso_path, upgrade_script_path)
 
     #pkg_num = 1.9
-    release_ver=['2.0']
+    release_ver=['2.0.0']
     curren_num = float(os.environ.get('releasePkgNum'))
     for pkg_num in release_ver:
     #while pkg_num <= curren_num:
--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.6_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.6_latest_on_cos7.py
@@ -46,7 +46,7 @@ def test():
     #test_stub.update_iso(vm_ip, tmp_file, old_iso_path, upgrade_script_path)
 
     #pkg_num = 1.7
-    release_ver=['1.7','1.8','1.9','1.10','2.0']
+    release_ver=['1.7','1.8','1.9','1.10','2.0.0']
     curren_num = float(os.environ.get('releasePkgNum'))
     for pkg_num in release_ver:
     #while pkg_num <= curren_num:
