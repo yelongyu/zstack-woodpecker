51e2e8d8906fe29a87ede84b7a483fe738f2b5c5
Ye Tian <ye.tian@zstack.io>
2017-07-06 11:31:32 +0800

    update test_stub.py  old_iso_path

--- integrationtest/vm/installation/test_stub.py
+++ integrationtest/vm/installation/test_stub.py
@@ -213,7 +213,7 @@ def update_iso(vm_ip, tmp_file, iso_path, upgrade_script_path):
     vm_password = os.environ['imagePassword']
     cmd = '%s "rm -rf /opt/zstack.iso"' % ssh_cmd
     process_result = execute_shell_in_process(cmd, tmp_file)
-    ssh.scp_file(iso_path, '/opt/zstack.iso', vm_ip, vm_username, vm_password)
+    ssh.scp_file(old_iso_path, '/opt/zstack.iso', vm_ip, vm_username, vm_password)
     ssh.scp_file(upgrade_script_path, '/opt/zstack-upgrade', vm_ip, vm_username, vm_password)
     cmd = '%s "mkdir -p /opt/zstack-dvd"' % ssh_cmd
     process_result = execute_shell_in_process(cmd, tmp_file)
