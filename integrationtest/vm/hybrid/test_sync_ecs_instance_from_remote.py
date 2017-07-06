0916ea0ed7f0d446cc7aaca3ce27715390333235
Lijin Xiong <legion.xiong@gmail.com>
2017-07-06 14:22:35 +0800

    Add hybrid/test_sync_ecs_instance_from_remote.py

---
+++ integrationtest/vm/hybrid/test_sync_ecs_instance_from_remote.py
@@ -0,0 +1,64 @@
+'''
+
+Test sync ECS instance from Aliyun.
+
+@author: Legion
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.hybrid_operations as hyb_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+import time
+import os
+
+date_s = time.strftime('%m%d%S', time.localtime())
+test_obj_dict = test_state.TestStateDict()
+ks_inv = None
+datacenter_inv = None
+vpc_inv = None
+
+
+def test():
+    global ks_inv
+    global datacenter_inv
+    global vpc_local
+    global vpc_inv
+    datacenter_type = os.getenv('datacenterType')
+    try:
+        ks_inv = hyb_ops.add_aliyun_key_secret('test_hybrid', 'test for hybrid', os.getenv('aliyunKey'), os.getenv('aliyunSecret'))
+    except:
+        pass
+    datacenter_list = hyb_ops.get_datacenter_from_remote(datacenter_type)
+    regions = [ i.regionId for i in datacenter_list]
+    for r in regions:
+        if 'shanghai' in r:
+            region_id = r
+#     region_id = datacenter_list[0].regionId
+    datacenter_inv = hyb_ops.add_datacenter_from_remote(datacenter_type, region_id, 'datacenter for test')
+    hyb_ops.create_ecs_vpc_remote(datacenter_inv.uuid, 'vpc_for_test_%s' % date_s, '192.168.0.0/16')
+    time.sleep(5)
+    ecs_synced = hyb_ops.sync_ecs_instance_from_remote(datacenter_inv.uuid)
+    assert ecs_synced.addList
+    ecs_local = hyb_ops.query_ecs_instance_local()
+    ecs_uuid_list = [l.uuid for l in ecs_local]
+    assert len(ecs_uuid_list) > 1
+    test_util.test_pass('Sync ECS Instance From Remote Test Success')
+
+def env_recover():
+    global vpc_inv
+    if vpc_inv:
+        time.sleep(10)
+        hyb_ops.del_ecs_vpc_remote(vpc_inv.uuid)
+    global datacenter_inv
+    if datacenter_inv:
+        hyb_ops.del_datacenter_in_local(datacenter_inv.uuid)
+    global ks_inv
+    if ks_inv:
+        hyb_ops.del_aliyun_key_secret(ks_inv.uuid)
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global test_obj_dict
+    test_lib.lib_error_cleanup(test_obj_dict)
