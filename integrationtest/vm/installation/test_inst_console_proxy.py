144ac1b4cf4df2279e6591311d317bdc6d1ddfb4
Ye Tian <ye.tian@zstack.io>
2017-07-08 11:03:07 +0800

    update test_inst_console_proxy.py image

--- integrationtest/vm/installation/test_inst_console_proxy.py
+++ integrationtest/vm/installation/test_inst_console_proxy.py
@@ -42,7 +42,7 @@ def test():
 
     test_util.test_dsc('Create test vm to test zstack installation with console proxy.')
 
-    conditions = res_ops.gen_query_conditions('name', '=', os.environ.get('imageNameBase_20_mn'))
+    conditions = res_ops.gen_query_conditions('name', '=', os.environ.get('imageNameBase_zstack'))
     image = res_ops.query_resource(res_ops.IMAGE, conditions)[0]
 
     vm_inv = create_vm(image) 
