65ebee8d322a1dab984c3edcfab01122ac0dfc64
Lijin Xiong <legion.xiong@gmail.com>
2017-07-13 09:40:27 +0800

    Change hybrid image upload region

--- integrationtest/vm/hybrid/test_create_ecs_image.py
+++ integrationtest/vm/hybrid/test_create_ecs_image.py
@@ -33,17 +33,21 @@ def test():
         pass
     datacenter_list = hyb_ops.get_datacenter_from_remote(datacenter_type)
     regions = [ i.regionId for i in datacenter_list]
-    err_list = []
-    for region_id in regions:
-        try:
-            datacenter_inv = hyb_ops.add_datacenter_from_remote(datacenter_type, region_id, 'datacenter for test')
-        except hyb_ops.ApiError, e:
-            err_list.append(e)
-            pass
-        if datacenter_inv:
-            break
-    if len(err_list) == len(regions):
-        raise hyb_ops.ApiError("Failed to add DataCenter: %s" % err_list)
+    for r in regions:
+        if 'hangzhou' in r:
+            region_id = r
+    datacenter_inv = hyb_ops.add_datacenter_from_remote(datacenter_type, region_id, 'datacenter for test')
+#     err_list = []
+#     for region_id in regions:
+#         try:
+#             datacenter_inv = hyb_ops.add_datacenter_from_remote(datacenter_type, region_id, 'datacenter for test')
+#         except hyb_ops.ApiError, e:
+#             err_list.append(e)
+#             pass
+#         if datacenter_inv:
+#             break
+#     if len(err_list) == len(regions):
+#         raise hyb_ops.ApiError("Failed to add DataCenter: %s" % err_list)
     bucket_inv = hyb_ops.create_oss_bucket_remote(region_id, 'zstack-test-%s-%s' % (date_s, region_id), 'created-by-zstack-for-test')
     hyb_ops.attach_oss_bucket_to_ecs_datacenter(bucket_inv.uuid, datacenter_inv.uuid)
     hyb_ops.update_image_guestOsType(image.uuid, guest_os_type='CentOS')
