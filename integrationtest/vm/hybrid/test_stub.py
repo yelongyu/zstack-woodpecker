ea0c1dd67c1b8a3b2e889f12c2ab8ab429c0569d
Lijin Xiong <legion.xiong@gmail.com>
2017-07-19 12:10:54 +0800

    Add hybrid/test_stub.py

---
+++ integrationtest/vm/hybrid/test_stub.py
@@ -0,0 +1,92 @@
+'''
+
+Create an unified test_stub to share test operations
+
+@author: Youyk
+'''
+
+import os
+import sys
+import time
+import threading
+
+import zstacklib.utils.ssh as ssh
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.zstack_test.zstack_test_vm as zstack_vm_header
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.net_operations as net_ops
+import zstackwoodpecker.operations.account_operations as acc_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+
+Port = test_state.Port
+
+rule1_ports = Port.get_ports(Port.rule1_ports)
+rule2_ports = Port.get_ports(Port.rule2_ports)
+rule3_ports = Port.get_ports(Port.rule3_ports)
+rule4_ports = Port.get_ports(Port.rule4_ports)
+rule5_ports = Port.get_ports(Port.rule5_ports)
+denied_ports = Port.get_denied_ports()
+#rule1_ports = [1, 22, 100]
+#rule2_ports = [9000, 9499, 10000]
+#rule3_ports = [60000, 60010, 65535]
+#rule4_ports = [5000, 5501, 6000]
+#rule5_ports = [20000, 28999, 30000]
+#test_stub.denied_ports = [101, 4999, 8990, 15000, 30001, 49999]
+target_ports = rule1_ports + rule2_ports + rule3_ports + rule4_ports + rule5_ports + denied_ports
+
+def create_vlan_vm(l3_name=None, disk_offering_uuids=None, system_tags=None, session_uuid = None, instance_offering_uuid = None):
+    image_name = os.environ.get('imageName_net')
+    image_uuid = test_lib.lib_get_image_by_name(image_name).uuid
+    if not l3_name:
+        l3_name = os.environ.get('l3VlanNetworkName1')
+
+    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    return create_vm([l3_net_uuid], image_uuid, 'vlan_vm', \
+            disk_offering_uuids, system_tags=system_tags, \
+            instance_offering_uuid = instance_offering_uuid,
+            session_uuid = session_uuid)
+
+# parameter: vmname; l3_net: l3_net_description, or [l3_net_uuid,]; image_uuid:
+def create_vm(l3_uuid_list, image_uuid, vm_name = None, \
+        disk_offering_uuids = None, default_l3_uuid = None, \
+        system_tags = None, instance_offering_uuid = None, session_uuid = None, ps_uuid=None):
+    vm_creation_option = test_util.VmOption()
+    conditions = res_ops.gen_query_conditions('type', '=', 'UserVm')
+    if not instance_offering_uuid:
+        instance_offering_uuid = res_ops.query_resource(res_ops.INSTANCE_OFFERING, conditions)[0].uuid
+    vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
+    vm_creation_option.set_l3_uuids(l3_uuid_list)
+    vm_creation_option.set_image_uuid(image_uuid)
+    vm_creation_option.set_name(vm_name)
+    vm_creation_option.set_data_disk_uuids(disk_offering_uuids)
+    vm_creation_option.set_default_l3_uuid(default_l3_uuid)
+    vm_creation_option.set_system_tags(system_tags)
+    vm_creation_option.set_session_uuid(session_uuid)
+    vm_creation_option.set_ps_uuid(ps_uuid)
+    vm = zstack_vm_header.ZstackTestVm()
+    vm.set_creation_option(vm_creation_option)
+    vm.create()
+    return vm
+
+def create_vr_vm(test_obj_dict, l3_name, session_uuid = None):
+    '''
+    '''
+    vr_l3_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    vrs = test_lib.lib_find_vr_by_l3_uuid(vr_l3_uuid)
+    temp_vm = None
+    if not vrs:
+        #create temp_vm1 for getting vlan1's vr for test pf_vm portforwarding
+        temp_vm = create_vlan_vm(l3_name, session_uuid = session_uuid)
+        test_obj_dict.add_vm(temp_vm)
+        vr = test_lib.lib_find_vr_by_vm(temp_vm.vm)[0]
+        temp_vm.destroy(session_uuid)
+        test_obj_dict.rm_vm(temp_vm)
+    else:
+        vr = vrs[0]
+        if not test_lib.lib_is_vm_running(vr):
+            test_lib.lib_robot_cleanup(test_obj_dict)
+            test_util.test_skip('vr: %s is not running. Will skip test.' % vr.uuid)
+
+    return vr
+
