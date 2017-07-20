603738903ca3ee029046d435095a0df9c36c26cf
glody <glody.guo@gmail.com>
2017-07-20 11:05:36 +0800

    Update case test_multi_nic.py

--- integrationtest/vm/virtualrouter/test_multi_nic.py
+++ integrationtest/vm/virtualrouter/test_multi_nic.py
@@ -15,7 +15,7 @@ test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
 
 def test():
-    test_util.test_dsc('Create vrouter vm and check multi nics')
+    test_util.test_dsc('Create vrouter vm and check if the second public nic attached to vrouter')
     vm1 = test_stub.create_vlan_vm(os.environ.get('l3VlanNetworkName1'))
     test_obj_dict.add_vm(vm1)
     vm1_ip = vm1.get_vm().vmNics[0].ip
@@ -29,44 +29,20 @@ def test():
 
     cond = res_ops.gen_query_conditions('name', '=', 'l3_user_defined_vlan1')
     second_public_l3network_uuid = res_ops.query_resource(res_ops.L3_NETWORK, cond)[0].uuid
-    second_public_l3network_cidr = res_ops.query_resource(res_ops.L3_NETWORK, cond)[0].ipRanges[0].networkCidr
     net_ops.attach_l3(second_public_l3network_uuid, vr1_uuid) 
-
-    cond = res_ops.gen_query_conditions('l3NetworkUuid', '=', second_public_l3network_uuid)
-    second_nic_ip = res_ops.query_resource(res_ops.VM_NIC, cond)[0].ip    
-    second_nic_uuid =  res_ops.query_resource(res_ops.VM_NIC, cond)[0].uuid
    
-    #Attach the network service to l3 network
-    cond = res_ops.gen_query_conditions('type', '=', 'vrouter')
-    service_uuid = res_ops.query_resource(res_ops.NETWORK_SERVICE_PROVIDER, cond)[0].uuid
-    network_services_json = "{'%s':['VRouterRoute']}"% service_uuid
-    net_ops.detach_network_service_from_l3network(l3network1_uuid, service_uuid)
-    net_ops.attach_network_service_to_l3network(l3network1_uuid, service_uuid)
-    net_ops.detach_network_service_from_l3network(second_public_l3network_uuid, service_uuid)
-    net_ops.attach_network_service_to_l3network(second_public_l3network_uuid, service_uuid)
+    cond = res_ops.gen_query_conditions('uuid','=',vr1_uuid)
+    nic_dict = res_ops.query_resource(res_ops.APPLIANCE_VM, cond)[0].vmNics
+    for nic in nic_dict:
+        if nic.l3NetworkUuid == second_public_l3network_uuid:
+            test_util.test_pass('The Second Public Nic Is Attached To Vrouter')
 
-    #Create route table and route entry for each vrouter
-    route_table1 = net_ops.create_vrouter_route_table('route_table1')
-    route_table1_uuid = route_table1.uuid
-    route_entry1 = net_ops.add_vrouter_route_entry(route_table1_uuid, second_public_l3network_cidr, second_nic_ip)
+    net_ops.detach_l3(nic_uuid)
 
-    #Attach the route table to vrouter and check the network
-    #net_ops.detach_vrouter_route_table_from_vrouter(route_table1_uuid, vr1_uuid)
-    net_ops.attach_vrouter_route_table_to_vrouter(route_table1_uuid, vr1_uuid)
+    vm1.check()
 
-    #Add vroute private ip to vm route
-    cmd = 'ip r del default; ip r add default via %s' %vr1_private_ip
-    rsp = test_lib.lib_execute_ssh_cmd(vm1_ip, 'root', 'password', cmd, 180)
-
-    #Dettach the route table to vrouter and check the network
-    #net_ops.detach_vrouter_route_table_from_vrouter(route_table1_uuid, vr1_uuid)
-
-    #net_ops.detach_l3(nic_uuid)
-
-    #vm1.check()
-
-    #vm1.destroy()
-    test_util.test_pass('Check Multi Nics Success')
+    vm1.destroy()
+    test_util.test_fail('The Second Public Nic Does Not Attached To Vrouter')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
