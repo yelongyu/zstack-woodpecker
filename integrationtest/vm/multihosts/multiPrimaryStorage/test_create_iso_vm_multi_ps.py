e1f054b6184adebe3eabaa357cddb4678dbb1600
ads6ads6 <ads5ads5@yeah.net>
2017-07-24 16:54:05 +0800

    Add test case create iso vm

---
+++ integrationtest/vm/multihosts/multiPrimaryStorage/test_create_iso_vm_multi_ps.py
@@ -0,0 +1,76 @@
+'''
+@author: FangSun
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.volume_operations as vol_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+import apibinding.inventory as inventory
+import apibinding.api_actions as api_actions
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.header.host as host_header
+import os
+import zstackwoodpecker.operations.primarystorage_operations as ps_ops
+import random
+
+_config_ = {
+        'timeout' : 3000,
+        'noparallel' : True
+        }
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+
+def test():
+
+    ps_list = res_ops.get_resource(res_ops.PRIMARY_STORAGE)
+    if len(ps_list) < 2:
+        test_util.test_skip("Skip test if less than two Primary Storage")
+
+    vm1 = test_stub.create_iso_vm_with_random_offering(vm_name='iso_vm')
+    test_obj_dict.add_vm(vm1)
+
+    disk_offering_uuids = [random.choice(res_ops.get_resource(res_ops.DISK_OFFERING)).uuid]
+    vm2 = test_stub.create_iso_vm_with_random_offering(vm_name='iso_vm_with_data_vol',
+                                                       disk_offering_uuids=disk_offering_uuids)
+    test_obj_dict.add_vm(vm2)
+
+    ps, another = test_stub.get_ps_vm_creation()
+
+    vm3 = test_stub.create_iso_vm_with_random_offering(vm_name='iso_vm_with_data_vol_specified_ps1',
+                                                       disk_offering_uuids=disk_offering_uuids,
+                                                       ps_uuid=ps.uuid)
+    test_obj_dict.add_vm(vm3)
+
+    root_vol_vm3 = test_lib.lib_get_root_volume(vm3.get_vm())
+    assert root_vol_vm3.primaryStorageUuid == ps.uuid
+
+    vm4 = test_stub.create_iso_vm_with_random_offering(vm_name='iso_vm_with_data_vol_specified_ps2',
+                                                       disk_offering_uuids=disk_offering_uuids,
+                                                       system_tags=['primaryStorageUuidForDataVolume::{}'.format(another.uuid)])
+    test_obj_dict.add_vm(vm4)
+
+    data_vol_list_vm4 = [vol for vol in vm4.get_vm().allVolumes if vol.type != 'Root']
+    for data_vol in data_vol_list_vm4:
+        assert data_vol.primaryStorageUuid == another.uuid
+
+    vm5 = test_stub.create_iso_vm_with_random_offering(vm_name='iso_vm_with_data_vol_specified_ps3',
+                                                       disk_offering_uuids=disk_offering_uuids,
+                                                       ps_uuid=ps.uuid,
+                                                       system_tags=['primaryStorageUuidForDataVolume::{}'.format(another.uuid)])
+    test_obj_dict.add_vm(vm5)
+
+    root_vol_vm5 = test_lib.lib_get_root_volume(vm5.get_vm())
+    assert root_vol_vm5.primaryStorageUuid == ps.uuid
+    data_vol_list_vm5 = [vol for vol in vm5.get_vm().allVolumes if vol.type != 'Root']
+    for data_vol in data_vol_list_vm5:
+        assert data_vol.primaryStorageUuid == another.uuid
+
+    test_util.test_pass('Multi PrimaryStorage Test Pass')
+
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
+
--- integrationtest/vm/multihosts/test_stub.py
+++ integrationtest/vm/multihosts/test_stub.py
@@ -103,6 +103,56 @@ def create_vm_with_iso(vm_name, l3_name, session_uuid = None):
     vm.create()
     return vm
 
+def create_iso_vm_with_random_offering(vm_name, l3_name=None, session_uuid=None, root_disk_uuid=None,
+                                       instance_offering_uuid=None, host_uuid=None, disk_offering_uuids=None,
+                                       root_password=None, ps_uuid=None, system_tags=None):
+    img_option = test_util.ImageOption()
+    img_option.set_name('fake_iso')
+    bs_uuid = res_ops.query_resource_fields(res_ops.BACKUP_STORAGE, [], session_uuid)[0].uuid
+    img_option.set_backup_storage_uuid_list([bs_uuid])
+    mn_ip = res_ops.query_resource(res_ops.MANAGEMENT_NODE)[0].hostName
+    img_option.set_url('http://%s:8080/zstack/static/zstack-dvd/ks.cfg' % (mn_ip))
+    image_uuid = img_ops.add_iso_template(img_option).uuid
+
+    if l3_name:
+        l3name = os.environ.get(l3_name)
+        l3_net_uuid = test_lib.lib_get_l3_by_name(l3name).uuid
+    else:
+        l3_net_uuid = random.choice(res_ops.get_resource(res_ops.L3_NETWORK)).uuid
+
+    if not root_disk_uuid:
+        root_disk_uuid = test_lib.lib_get_disk_offering_by_name(os.environ.get('mediumDiskOfferingName')).uuid
+
+    if not instance_offering_uuid:
+        conf = res_ops.gen_query_conditions('type', '=', 'UserVM')
+        instance_offering_uuid = random.choice(res_ops.query_resource(res_ops.INSTANCE_OFFERING, conf)).uuid
+
+    vm_creation_option = test_util.VmOption()
+    vm_creation_option.set_l3_uuids([l3_net_uuid])
+    vm_creation_option.set_image_uuid(image_uuid)
+    vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
+    vm_creation_option.set_name(vm_name)
+    vm_creation_option.set_root_disk_uuid(root_disk_uuid)
+    if system_tags:
+        vm_creation_option.set_system_tags(system_tags)
+    if disk_offering_uuids:
+        vm_creation_option.set_data_disk_uuids(disk_offering_uuids)
+    if root_password:
+        vm_creation_option.set_root_password(root_password)
+    if host_uuid:
+        vm_creation_option.set_host_uuid(host_uuid)
+    if session_uuid:
+        vm_creation_option.set_session_uuid(session_uuid)
+    if ps_uuid:
+        vm_creation_option.set_ps_uuid(ps_uuid)
+
+    vm = test_vm_header.ZstackTestVm()
+    vm.set_creation_option(vm_creation_option)
+    vm.create()
+    return vm
+
+
+
 def create_vm_with_random_offering(vm_name, image_name=None, l3_name=None, session_uuid=None,
                                    instance_offering_uuid=None, host_uuid=None, disk_offering_uuids=None,
                                    root_password=None, ps_uuid=None, system_tags=None):
