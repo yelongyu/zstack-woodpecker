584f7ae3b4800c55e68a68fde2cbd81f5e351553
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-07-24 19:22:30 +0800

    fix suite_setup of import res_ops

--- integrationtest/vm/multihosts/suite_setup.py
+++ integrationtest/vm/multihosts/suite_setup.py
@@ -13,6 +13,7 @@ import zstackwoodpecker.operations.deploy_operations as deploy_operations
 import zstackwoodpecker.operations.config_operations as config_operations
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.operations.resource_operations as res_ops
 
 USER_PATH = os.path.expanduser('~')
 EXTRA_SUITE_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_suite_setup_config.sh' % USER_PATH
