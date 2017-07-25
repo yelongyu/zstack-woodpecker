a22ba1d980b4266aa1bf347753e285bffcf70a80
ads6ads6 <ads5ads5@yeah.net>
2017-07-25 10:37:38 +0800

    check image format to use non iso image

--- integrationtest/vm/multihosts/test_stub.py
+++ integrationtest/vm/multihosts/test_stub.py
@@ -161,7 +161,7 @@ def create_vm_with_random_offering(vm_name, image_name=None, l3_name=None, sessi
         imagename = os.environ.get(image_name)
         image_uuid = test_lib.lib_get_image_by_name(imagename).uuid
     else:
-        conf = res_ops.gen_query_conditions('format', '=', 'qcow2')
+        conf = res_ops.gen_query_conditions('format', '!=', 'iso')
         image_uuid = random.choice(res_ops.query_resource(res_ops.IMAGE, conf)).uuid
 
     if l3_name:
