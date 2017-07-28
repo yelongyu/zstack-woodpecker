1bf0f449a3490a2137602ccc2fad2a315aaf266e
quarkonics <quarkonics@gmail.com>
2017-07-28 13:20:17 +0800

    Enable virtualrouter/vlan/test_set_l3_mtu.py

--- integrationtest/vm/virtualrouter/vlan/test_set_l3_mtu.py
+++ integrationtest/vm/virtualrouter/vlan/test_set_l3_mtu.py
@@ -10,11 +10,19 @@ import os
 
 test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
+default_l3_mtu = None
+_config_ = {
+        'timeout' : 1800,
+        'noparallel' : True
+        }
+
 
 def test():
+    global default_l3_mtu
     test_util.test_dsc('Create test vm and check. VR has DNS SNAT EIP PF and DHCP services')
     l3_name = os.environ.get('l3VlanNetworkName1')
     l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+    default_l3_mtu = net_ops.get_l3_mtu(l3_net_uuid)
     net_ops.set_l3_mtu(l3_net_uuid, 1200)
     
     vm = test_stub.create_vlan_vm(l3_name)
@@ -24,6 +32,13 @@ def test():
 
     vm.destroy()
     test_util.test_pass('Create VirtualRouter VM DNS DHCP SANT EIP PF Test Success')
+
+def env_recover():
+    global default_l3_mtu
+    if default_l3_mtu:
+        l3_name = os.environ.get('l3VlanNetworkName1')
+        l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+        net_ops.set_l3_mtu(l3_net_uuid, default_l3_mtu)
 
 #Will be called only if exception happens in test().
 def error_cleanup():
