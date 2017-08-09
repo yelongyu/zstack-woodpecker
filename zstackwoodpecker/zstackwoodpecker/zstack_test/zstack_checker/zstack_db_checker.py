112a11469a20e82fafb700d4ab12989c10bac5c2
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-08-09 15:10:27 +0800

    fix load balance checker
    If it separated vr, there is no other service except load balance.
    Originally, we tell if separated vr by whether there is DHCP service.
    However, as the developing of ZStack, flat DHCP is a exception.

--- zstackwoodpecker/zstackwoodpecker/zstack_test/zstack_checker/zstack_db_checker.py
+++ zstackwoodpecker/zstackwoodpecker/zstack_test/zstack_checker/zstack_db_checker.py
@@ -232,6 +232,10 @@ class zstack_alone_lb_vr_db_checker(checker_header.TestChecker):
         LBL doesn't add any VM Nic, LB VR won't be created.
 
         When LB is deleted, LB VR will be destroyed. 
+
+        If it separated vr, there is no other service except load balance.
+        Originally, we tell if separated vr by whether there is DHCP service.
+        However, as the developing of ZStack, flat DHCP is a exception.
     '''
     def check(self):
         super(zstack_alone_lb_vr_db_checker, self).check()
@@ -241,14 +245,10 @@ class zstack_alone_lb_vr_db_checker(checker_header.TestChecker):
         vr = res_ops.query_resource(res_ops.VIRTUALROUTER_VM, cond)[0]
         vr_uuid = vr.uuid
         cond = res_ops.gen_query_conditions('tag', '=', \
-                'role::DHCP')
+                'role::DNS')
         cond = res_ops.gen_query_conditions('resourceUuid', '=', \
                 vr_uuid, cond)
         system_tag = res_ops.query_resource(res_ops.SYSTEM_TAG, cond)
-        if not system_tag or len(system_tag):
-            cond = res_ops.gen_query_conditions('resourceUuid', '=', vr_uuid)
-            system_tag = res_ops.query_resource(res_ops.SYSTEM_TAG, cond)
-
         if system_tag:
             test_util.test_logger("Load Balancer: %s is not in separated VR. Its VR is %s, which is at least shared with DHCP service." % (lb_inv.uuid, vr_uuid))
             return self.judge(False)
