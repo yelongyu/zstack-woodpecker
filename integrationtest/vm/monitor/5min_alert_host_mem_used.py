0bd31e0acdd933b5373445c5b9d79b2a3bede26c
hao.chen <1030130337@qq.com>
2017-08-21 19:11:17 +0800

    Update 5min_alert_host_mem_used.py

--- integrationtest/vm/monitor/5min_alert_host_mem_used.py
+++ integrationtest/vm/monitor/5min_alert_host_mem_used.py
@@ -29,7 +29,7 @@ def test():
     hosts = res_ops.get_resource(res_ops.HOST)
     host = hosts[0]
     duration = 300
-    expression = "host.mem.util{type=\"used\"} > 0.75"
+    expression = "host.mem.util{type=\"used\"} > 0.60"
     monitor_trigger = mon_ops.create_monitor_trigger(host.uuid, duration, expression)
 
     send_email = test_stub.create_email_media()
