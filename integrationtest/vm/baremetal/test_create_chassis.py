0bb7db2387c94222a6a311130377422076a57c0c
Chao Zhou <chao.zhou@zstack.io>
2017-08-21 15:00:11 +0800

    update test_create_chassis.py

--- integrationtest/vm/baremetal/test_create_chassis.py
+++ integrationtest/vm/baremetal/test_create_chassis.py
@@ -12,11 +12,11 @@ def test():
     vm = test_stub.create_vm()
     vm.check()
     # Create Virtual BMC
-    test_stub.create_vbmc(vm = vm, port = 623)
+    test_stub.create_vbmc(vm = vm, port = 6230)
     # Create Chassis
     chassis = os.environ.get('ipminame')
     test_stub.create_chassis(chassis_name = chassis)
-    test_stub.hack_ks()
+    test_stub.hack_ks(port = 6230)
     chassis_uuid = test_lib.lib_get_chassis_by_name(chassis).uuid
     # First time Provision
     bare_operations.provision_baremetal(chassis_uuid)
@@ -24,7 +24,18 @@ def test():
     if not hwinfo:
         test_util.test_fail('Fail to get hardware info during the first provision')
     #Generate cfgItems
-    test_stub.generate_nicCfg
+    cfgItems, pxe_mac = test_stub.generate_cfgItems(chassis_uuid)
+    host_cfg =  test_stub.create_hostcfg(chassis_uuid=chassis_uuid, unattended=True, password="password", cfgItems=cfgItems)
+    
+    test_stub.hack_ks(port = 6230, ks_file = str(pxe_mac.replace(':','-')))
+    # Second time Provision to install system
+    bare_operations.provision_baremetal(chassis_uuid)
+    if not test_stub.verify_chassis_status(chassis_uuid, "Rebooting"):
+        test_util.test_fail('Chassis failed to Rebooting after the second provision')
+    if not test_stub.verify_chassis_status(chassis_uuid, "Provisioning"):
+        test_util.test_fail('Chassis failed to Provisioning after the second provision')
+    if not test_stub.verify_chassis_status(chassis_uuid, "Provisioned"):
+        test_util.test_fail('Chassis failed to Provisioned after the second provision')
     test_util.test_pass('Create PXE Test Success')
 
 def error_cleanup():
