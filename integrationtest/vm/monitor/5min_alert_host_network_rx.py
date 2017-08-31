df98d36d6d71d683bfcfce627f6eae7c0e7db5ad
hao.chen <1030130337@qq.com>
2017-08-31 09:57:49 +0800

    Update 5min_alert_host_network_rx.py

--- integrationtest/vm/monitor/5min_alert_host_network_rx.py
+++ integrationtest/vm/monitor/5min_alert_host_network_rx.py
@@ -3,6 +3,7 @@
 Create an unified test_stub to share test operations
 
 @author: Songtao
+
 '''
 
 import os
@@ -12,7 +13,6 @@ import zstacklib.utils.ssh as ssh
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.operations.monitor_operations as mon_ops
-
 
 def test():
     global vm
@@ -51,7 +51,6 @@ def test():
 
     ssh_cmd = test_stub.ssh_cmd_line(vm_ip, vm_username, vm_password, vm_port)
     test_stub.yum_install_stress_tool(ssh_cmd)
-    
 
     host.password = os.environ.get('hostPassword')
     ssh_cmd_host = test_stub.ssh_cmd_line(host.managementIp, host.username, host.password, port=int(host.sshPort))
@@ -73,9 +72,6 @@ def test():
     mon_ops.delete_monitor_trigger(trigger)
     mon_ops.delete_email_media(media)
     vm.destroy()
-    vm.expunge()
-
-
 
 def error_cleanup():
     global trigger
@@ -86,12 +82,3 @@ def error_cleanup():
     mon_ops.delete_monitor_trigger(trigger)
     mon_ops.delete_email_media(media)
     vm.destroy()
-    vm.expunge()
-
-
-
-
-
-
-
-
