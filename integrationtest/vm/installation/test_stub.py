e6a85cdbf3811477c7d60ecb1520d3c15d4a164a
Ye Tian <ye.tian@zstack.io>
2017-09-04 10:37:20 +0800

    update test_stub.py add update_repo

--- integrationtest/vm/installation/test_stub.py
+++ integrationtest/vm/installation/test_stub.py
@@ -171,6 +171,11 @@ def update_mn_hostname(vm_ip, tmp_file):
     cmd = '%s "hostnamectl set-hostname zs-test" ' % ssh_cmd
     process_result = execute_shell_in_process(cmd, tmp_file)
 
+def update_repo(vm_ip, tmp_file):
+    ssh_cmd = 'ssh -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm_ip
+    cmd = '''%s 'echo "172.20.198.214 repo.zstack.io" >> /etc/hosts' ''' % ssh_cmd
+    process_result = execute_shell_in_process(cmd, tmp_file)
+
 def update_mn_ip(vm_ip, mn_ip, tmp_file):
     ssh_cmd = 'ssh -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm_ip
     cmd = '%s "zstack-ctl change_ip --ip="%s ' % (ssh_cmd, vm_ip)
