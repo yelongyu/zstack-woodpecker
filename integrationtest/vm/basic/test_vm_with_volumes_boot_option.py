45591c8be941271da22ea247b9a73591d6ae3377
sherry546 <546102561@qq.com>
2017-09-06 21:38:46 +0800

    add vm with volumes boot option test and windows vm boot option test

---
+++ integrationtest/vm/basic/test_vm_with_volumes_boot_option.py
@@ -0,0 +1,63 @@
+'''
+
+New Integration test for testing vm with 2 additional data volumes boot option. 
+
+@author: ChenyuanXu 
+'''
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstacklib.utils.shell as shell
+import zstackwoodpecker.operations.config_operations as conf_ops
+import test_stub
+import time
+import os
+from vncdotool import api
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+node_ip = os.environ.get('node1Ip')
+
+def test():
+    global test_obj_dict
+    global vm
+    import signal
+    def handler(signum, frame):
+        raise Exception()
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(30)
+
+    test_util.test_dsc('Create test vm with 2 additional data volumes boot option')
+    conf_ops.change_global_config('vm', 'bootMenu', 'false')
+    disk_offering = test_lib.lib_get_disk_offering_by_name(os.environ.get('smallDiskOfferingName'))
+    disk_offering_uuids = [disk_offering.uuid]
+    disk_offering = test_lib.lib_get_disk_offering_by_name(os.environ.get('rootDiskOfferingName'))
+    disk_offering_uuids.append(disk_offering.uuid)
+
+    vm = test_stub.create_vm_with_volume(data_volume_uuids = disk_offering_uuids)
+    test_obj_dict.add_vm(vm)
+    vm_inv = vm.get_vm()
+    vm_ip = vm_inv.vmNics[0].ip
+    console = test_lib.lib_get_vm_console_address(vm.get_vm().uuid)
+    test_util.test_logger('[vm:] %s console is on %s:%s' % (vm.get_vm().uuid, console.hostIp, console.port))
+    display = str(int(console.port)-5900)
+
+    client = api.connect(console.hostIp+":"+display)
+    time.sleep(2)
+    client.keyPress('esc')
+    try:
+        client.expectRegion('/home/%s/zstack-woodpecker/vm_volumes_boot.png',0,100) % node_ip
+    except:
+        test_util.test_logger('Success to not enable boot menu.')
+    else:
+        test_util.test_fail('Fail to not enable boot menu.')
+
+    vm.destroy(0 
+    test_util.test_pass('VM With Volumes Boot Option Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    if vm:
+        vm.destroy()
+
---
+++ integrationtest/vm/image_store/test_win_vm_boot_option.py
@@ -0,0 +1,64 @@
+'''
+
+New Integration test for testing cloned windows vm boot option. 
+
+@author: ChenyuanXu 
+'''
+import apibinding.inventory as inventory
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstacklib.utils.shell as shell
+import test_stub
+import time
+import os
+from vncdotool import api
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+vn_prefix = 'vm-clone-%s' % time.time()
+vm_name = ['%s-vm1' % vn_prefix]
+node_ip = os.environ.get('node1Ip')
+
+def test():
+    global test_obj_dict
+    global vm
+    import signal
+    def handler(signum, frame):
+        raise Exception()
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(30)
+    test_util.test_dsc('Create test clone windows vm boot option')
+
+    image_name = os.environ.get('imageName_windows')
+    vm = test_stub.create_vm(image_name = image_name)
+    test_obj_dict.add_vm(vm)
+
+    backup_storage_list = test_lib.lib_get_backup_storage_list_by_vm(vm.vm)
+    for bs in backup_storage_list:
+        if bs.type == inventory.IMAGE_STORE_BACKUP_STORAGE_TYPE:
+            break
+    else:
+        vm.destroy()
+        test_util.test_skip('Not find image store type backup storage.')
+
+    new_vm = vm.clone(vm_name)[0]
+    test_obj_dict.add_vm(new_vm)
+    console = test_lib.lib_get_vm_console_address(new_vm.get_vm().uuid)
+    test_util.test_logger('[vm:] %s console is on %s:%s' % (new_vm.get_vm().uuid, console.hostIp, console.port))
+    display = str(int(console.port)-5900)
+
+    client = api.connect(console.hostIp+":"+display)
+    client.keyPress('esc')
+    client.expectRegion('/home/%s/zstack-woodpecker/win_boot.png',0,100) % node_ip
+
+    vm.destroy()
+    new_vm.destroy()
+
+    test_util.test_pass('VM With Volumes Boot Option Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    if vm:
+        vm.destroy()
