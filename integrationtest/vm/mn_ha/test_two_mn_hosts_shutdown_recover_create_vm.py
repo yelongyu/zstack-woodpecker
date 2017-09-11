c8159808c4736f0a3ae8a305de2845386501928f
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-09-11 20:05:56 +0800

    remove skip on shutdown case

--- integrationtest/vm/mn_ha/test_two_mn_hosts_shutdown_recover_create_vm.py
+++ integrationtest/vm/mn_ha/test_two_mn_hosts_shutdown_recover_create_vm.py
@@ -24,7 +24,6 @@ def test():
     global mn_host_list
     global test_mn_host_list
 
-    test_stub.skip_if_scenario_is_multiple_networks()
 
     mn_host_list = test_stub.get_mn_host(test_lib.all_scenario_config, test_lib.scenario_file)
     mn_host_num = len(mn_host_list)
