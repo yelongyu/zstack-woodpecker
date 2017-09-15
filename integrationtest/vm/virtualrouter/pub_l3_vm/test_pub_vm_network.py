680f71c5f7c486a7bb283e337ee419cf3dbbce05
ads6ads6 <ads5ads5@yeah.net>
2017-09-15 19:36:21 +0800

    Add IPrange check in network case

--- integrationtest/vm/virtualrouter/pub_l3_vm/test_pub_vm_network.py
+++ integrationtest/vm/virtualrouter/pub_l3_vm/test_pub_vm_network.py
@@ -7,6 +7,7 @@
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.operations.net_operations as net_ops
 
 
 test_stub = test_lib.lib_get_test_stub()
@@ -17,10 +18,16 @@ def test():
 
     pub_l3_vm, flat_l3_vm, vr_l3_vm = test_stub.generate_pub_test_vm(tbj=test_obj_dict)
 
+    ip_status_before = net_ops.get_ip_capacity_by_l3s(l3_network_list=[pub_l3_vm.get_vm().vmNics[0].l3NetworkUuid])
+
     flat_vip = test_stub.create_vip('create_flat_vip')
     test_obj_dict.add_vip(flat_vip)
     vr_vip = test_stub.create_vip('create_vr_vip')
     test_obj_dict.add_vip(vr_vip)
+
+    ip_status_after = net_ops.get_ip_capacity_by_l3s(l3_network_list=[pub_l3_vm.get_vm().vmNics[0].l3NetworkUuid])
+
+    assert ip_status_before.availableCapacity == ip_status_after.availableCapacity + 2
 
     test.flat_eip = test_stub.create_eip('create flat eip', vip_uuid=flat_vip.get_vip().uuid,
                                     vnic_uuid=flat_l3_vm.get_vm().vmNics[0].uuid, vm_obj=flat_l3_vm)
@@ -37,6 +44,10 @@ def test():
     for ip in [pub_l3_vm.get_vm().vmNics[0].ip, flat_vip.get_vip().ip, vr_vip.get_vip().ip]:
         if not test_lib.lib_check_directly_ping(ip):
             test_util.test_fail('expected to be able to ping vip while it fail')
+
+    test_lib.lib_error_cleanup(test_obj_dict)
+    ip_status_final = net_ops.get_ip_capacity_by_l3s(l3_network_list=[pub_l3_vm.get_vm().vmNics[0].l3NetworkUuid])
+    assert ip_status_final.availableCapacity == ip_status_after.availableCapacity + 3
 
     test_util.test_pass('pub vm volume network test pass')
 
