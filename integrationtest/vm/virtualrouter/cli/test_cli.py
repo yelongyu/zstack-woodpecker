03fd8dba834b26e91e780b0c332b188302db7839
Lei Liu <lei.liu@mevoco.com>
2017-09-18 13:13:06 +0800

    Add retry for cli test

--- integrationtest/vm/virtualrouter/cli/test_cli.py
+++ integrationtest/vm/virtualrouter/cli/test_cli.py
@@ -14,6 +14,7 @@ import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.operations.config_operations as con_ops
 import zstacklib.utils.ssh as ssh
 import json
+import time
 import os
 
 
@@ -21,54 +22,83 @@ def test():
 
     test_util.test_dsc('Create test vm and check')
     mn_ip = os.environ.get('node1Name')
+    test_util.test_dsc("wait 10s for start")
     nodeUserName = os.environ.get('nodeUserName')
     nodePassword = os.environ.get('nodePassword')
     ssh.make_ssh_no_password(mn_ip, nodeUserName, nodePassword)
 
     #Query image
     image_name = os.environ.get('imageName_s')
-    cmd = "zstack-cli LogInByAccount accountName=admin password=password;" 
-    fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
-    cmd = 'zstack-cli QueryImage fields=uuid name="' + image_name  + '"'
-    fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
-    test_out = fo.read()
-    query_result = json.loads(test_out)
+    for i in range(0, 4):
+        time.sleep(10)
+        try:
+            cmd = "zstack-cli LogInByAccount accountName=admin password=password;"
+            fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
+            cmd = 'zstack-cli QueryImage fields=uuid name="' + image_name  + '"'
+            fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
+            test_util.test_logger('ssh ' + mn_ip + ' ' + cmd)
+            test_out = fo.read()
+            query_result = json.loads(test_out)
+            break
+        except Exception as e:
+            test_util.test_logger(str(e))
+            test_util.test_logger('fail to query image retry: ' + str(i))
     test_util.test_logger(query_result)
     image_uuid = str(query_result['inventories'][0]['uuid'])
 
     #Query l3
     l3_1_name = os.environ.get('l3PublicNetworkName')
     test_util.test_logger(l3_1_name)
-    cmd = "zstack-cli LogInByAccount accountName=admin password=password;" 
-    fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
-    cmd = '"zstack-cli QueryL3Network fields=uuid name=\'' + l3_1_name + '\'"'
-    test_util.test_logger(cmd)
-    fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
-    test_err = fe.read()
-    test_out = fo.read()
-    query_result = json.loads(test_out)
+    for i in range(0, 4):
+        try:
+            cmd = "zstack-cli LogInByAccount accountName=admin password=password;"
+            fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
+            cmd = '"zstack-cli QueryL3Network fields=uuid name=\'' + l3_1_name + '\'"'
+            test_util.test_logger(cmd)
+            fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
+            test_err = fe.read()
+            test_out = fo.read()
+            query_result = json.loads(test_out)
+            break
+        except Exception as e:
+            test_util.test_logger(str(e))
+            test_util.test_logger('fail to query l3network retry: ' + str(i))
+        time.sleep(2)
     test_util.test_logger(query_result)
     #l3_1_uuid = str(query_result['inventories'][0]['uuid'])
     l3_1_uuid = test_lib.lib_get_l3_by_name(l3_1_name).uuid
 
     #Query instanceOffering
-    cmd = "zstack-cli QueryInstanceOffering fields=uuid"
-    fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
-    test_out = fo.read()
+    for i in range(0, 4):
+        try:
+            cmd = "zstack-cli QueryInstanceOffering fields=uuid"
+            fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
+            test_out = fo.read()
 
-    query_result = json.loads(test_out)
-    test_util.test_logger(query_result)
+            query_result = json.loads(test_out)
+            test_util.test_logger(query_result)
+            break
+        except Exception as e:
+            test_util.test_logger(str(e))
+            test_util.test_logger('fail to query l3network retry: ' + str(i))
+        time.sleep(2)
 
     instance_offering_uuid = str(query_result['inventories'][0]['uuid'])
 
-
-    test_util.test_logger('CLI query')
-    cmd = "zstack-cli LogInByAccount accountName=admin password=password;\
-           zstack-cli QueryHost"
-    fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
-    testout = fo.read()
-    query_result = json.loads(test_out)
-    test_util.test_logger(query_result)
+    for i in range(0, 4):
+        try:
+            test_util.test_logger('CLI query')
+            cmd = "zstack-cli LogInByAccount accountName=admin password=password;\
+                   zstack-cli QueryHost"
+            fi, fo, fe = os.popen3('ssh ' + mn_ip + ' ' + cmd )
+            testout = fo.read()
+            query_result = json.loads(test_out)
+            test_util.test_logger(query_result)
+            break
+        except Exception as e:
+            test_util.test_logger(str(e))
+            test_util.test_logger('fail to query l3network retry: ' + str(i))
+        time.sleep(2)
     cmd = "zstack-cli LogInByAccount accountName=admin password=password;\
            zstack-cli CreateVmInstance instanceOfferingUuid=" + instance_offering_uuid \
            + " l3NetworkUuids=" + l3_1_uuid \
@@ -85,14 +115,4 @@ def test():
 
 #Will be called only if exception happens in test().
 def error_cleanup():
-    global ps_uuid
-    if ps_uuid != None:
-        ps_ops.change_primary_storage_state(ps_uuid, 'enable')
-    global host_uuid
-    if host_uuid != None:
-        host_ops.reconnect_host(host_uuid)
-    global vr_uuid
-    if vr_uuid != None:
-        vm_ops.reconnect_vr(vr_uuid)
-    global test_obj_dict
     test_lib.lib_error_cleanup(test_obj_dict)
