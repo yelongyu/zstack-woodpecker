1797a5c4260b49f0770284e67bc36a4251cc92ba
ads6ads6 <ads5ads5@yeah.net>
2017-09-18 19:28:27 +0800

    Add no DHCP test case

---
+++ integrationtest/vm/virtualrouter/pub_l3_vm/test_pub_vm_noDHCP.py
@@ -0,0 +1,29 @@
+'''
+
+@author: FangSun
+'''
+
+
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.test_util as test_util
+
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+
+def test():
+
+    pub_l3_vm, flat_l3_vm, vr_l3_vm = test_stub.generate_pub_test_vm(tbj=test_obj_dict)
+
+    for vm in (pub_l3_vm, flat_l3_vm, vr_l3_vm):
+        for action in ('stop', 'start', 'reboot', 'suspend', 'resume'):
+            getattr(vm, action)()
+
+    test_lib.lib_error_cleanup(test_obj_dict)
+
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
+
