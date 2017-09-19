b477ed4afeb74f09a46ca947491389695f4256f4
Ye Tian <ye.tian@zstack.io>
2017-09-19 10:20:28 +0800

    add the test_create_spice_vm.py

---
+++ integrationtest/vm/basic/test_create_spice_vm.py
@@ -0,0 +1,25 @@
+'''
+
+New Integration Test for creating KVM VM.
+
+@author: ye.tian
+'''
+
+import zstackwoodpecker.test_util as test_util
+import test_stub
+
+vm = None
+
+def test():
+    global vm
+    vm = test_stub.create_spice_vm()
+    vm.check()
+    vm.destroy()
+    vm.check()
+    test_util.test_pass('Create VM with spice Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    global vm
+    if vm:
+        vm.destroy()
