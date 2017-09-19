841d70e2c627abb21c75a1a121728cdb380fa440
ads6ads6 <ads5ads5@yeah.net>
2017-09-19 13:40:48 +0800

    Add system network test case

---
+++ integrationtest/vm/virtualrouter/pub_l3_vm/test_system_network.py
@@ -0,0 +1,45 @@
+'''
+
+@author: FangSun
+'''
+
+
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.test_util as test_util
+import os
+from zstackwoodpecker.operations import net_operations as net_ops
+from zstackwoodpecker.operations import resource_operations as res_ops
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+
+def test():
+
+    with test_lib.expected_failure('create vm use system network', Exception):
+        test_stub.create_vm_with_random_offering(vm_name='test_vm',
+                                                 image_name='imageName_net',
+                                                 l3_name='l3ManagementNetworkName')
+
+    with test_lib.expected_failure('use system network to create VIP', Exception):
+        test_stub.create_vip(vip_name='test_vip',
+                             l3_uuid=test_lib.lib_get_l3_by_name(os.environ.get('l3ManagementNetworkName')).uuid)
+
+    exist_vr_offering = res_ops.get_resource(res_ops.VR_OFFERING)[0]
+
+    with test_lib.expected_failure('Create VR offering public network using system', Exception):
+        net_ops.create_virtual_router_offering(name='test_vr_offering',
+                                               cpuNum=exist_vr_offering.cpuNum,
+                                               memorySize=exist_vr_offering.memorySize,
+                                               imageUuid=exist_vr_offering.imageUuid,
+                                               zoneUuid=exist_vr_offering.zoneUuid,
+                                               managementNetworkUuid=exist_vr_offering.managementNetworkUuid,
+                                               publicNetworkUuid=exist_vr_offering.managementNetworkUuid)
+
+
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
+
+
