99b6f0ea85a1c293111982061126e23e3079c43c
glody <glody.guo@gmail.com>
2017-09-20 20:41:34 +0800

    add more checkpoints for case test_data_volume_backup_recovery.py

--- integrationtest/vm/hybrid/data_protect/test_data_volume_backup_recovery.py
+++ integrationtest/vm/hybrid/data_protect/test_data_volume_backup_recovery.py
@@ -55,24 +55,38 @@ def test():
     image_option.set_name('create_data_iso_to_image_store')
     image_option.set_backup_storage_uuid_list([dpbs_uuid])
     image = img_ops.create_data_volume_template(image_option)
-    image_uuid = image.uuid
+    dpbs_image_uuid = image.uuid
 
-    cond = res_ops.gen_query_conditions('uuid', '=', image_uuid)
+    cond = res_ops.gen_query_conditions('uuid', '=', dpbs_image_uuid)
     media_type = res_ops.query_resource(res_ops.IMAGE, cond)[0].mediaType
     if media_type != 'DataVolumeTemplate':
         test_util.test_fail('Wrong image media type, the expect is "DataVolumeTemplate", the real is "%s"' %media_type) 
 
-    recovery_image = img_ops.recovery_image_from_image_store_backup_storage(local_bs_uuid, dpbs_uuid, image_uuid) 
+    cond = res_ops.gen_query_conditions('resourceUuid', '=', dpbs_image_uuid)
+    system_tag = res_ops.query_resource(res_ops.SYSTEM_TAG, cond)[0]
+    if system_tag.tag != "remote":
+        test_util.test_fail("Here isn't 'remote' system tag for image in data protect bs")
+
+    recovery_image = img_ops.recovery_image_from_image_store_backup_storage(local_bs_uuid, dpbs_uuid, dpbs_image_uuid) 
     if recovery_image.backupStorageRefs[0].backupStorageUuid != local_bs_uuid:
         test_util.test_fail('Recovery image failed, image uuid is %s' %recovery_image.uuid)
     if recovery_image.mediaType != 'DataVolumeTemplate':
         test_util.test_fail('Wrong image media type after recovery, the expect is "DataVolumeTemplate", the real is "%s"' %media_type)
     image_uuid = recovery_image.uuid
 
-    vol_ops.delete_volume(data_volume_uuid)
-    img_ops.delete_image(image_uuid)
-    bs_ops.delete_backup_storage(dpbs_uuid) 
-    test_util.test_pass('Data volume backup to and recovery from image store backup storage success')
+    try:
+        #Try to recovery the same image again
+        recovery_image = img_ops.recovery_image_from_image_store_backup_storage(local_bs_uuid, dpbs_uuid, dpbs_image_uuid)
+
+    except Exception,e:
+        if str(e).find('already contains it') != -1:
+            test_util.test_pass('Try to recovery the same image again and get the error info expectly: %s' %str(e))
+    finally: 
+        vol_ops.delete_volume(data_volume_uuid)
+        img_ops.delete_image(image_uuid)
+        bs_ops.delete_backup_storage(dpbs_uuid) 
+        pass
+    test_util.test_fail('Try to recovery the same image second time success unexpectly')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
