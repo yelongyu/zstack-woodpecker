6cfb591b64d28ecbba31177ebb65995d275ab710
ads6ads6 <ads5ads5@yeah.net>
2017-09-21 16:42:55 +0800

    fix check scenario issue

--- integrationtest/vm/virtualrouter/suite_setup.py
+++ integrationtest/vm/virtualrouter/suite_setup.py
@@ -31,15 +31,14 @@ def test():
     nic_name = "eth0"
     if test_lib.scenario_config != None and test_lib.scenario_file != None and os.path.exists(test_lib.scenario_file):
         nic_name = "zsn0"
+        linux.create_vlan_eth(nic_name, 1010)
+        linux.create_vlan_eth(nic_name, 1011)
     #This vlan creation is not a must, if testing is under nested virt env. But it is required on physical host without enough physcial network devices and your test execution machine is not the same one as Host machine. 
     #linux.create_vlan_eth("eth0", 10, "10.0.0.200", "255.255.255.0")
     #linux.create_vlan_eth("eth0", 11, "10.0.1.200", "255.255.255.0")
     #no matter if current host is a ZStest host, we need to create 2 vlan devs for future testing connection for novlan test cases.
     linux.create_vlan_eth(nic_name, 10)
     linux.create_vlan_eth(nic_name, 11)
-    if test_lib.scenario_config != None and test_lib.scenario_file != None and not os.path.exists(test_lib.scenario_file):
-        linux.create_vlan_eth(nic_name, 1010)
-        linux.create_vlan_eth(nic_name, 1011)
     #If test execution machine is not the same one as Host machine, deploy work is needed to separated to 2 steps(deploy_test_agent, execute_plan_without_deploy_test_agent). And it can not directly call SetupAction.run()
     test_lib.setup_plan.deploy_test_agent()
     cmd = host_plugin.CreateVlanDeviceCmd()
