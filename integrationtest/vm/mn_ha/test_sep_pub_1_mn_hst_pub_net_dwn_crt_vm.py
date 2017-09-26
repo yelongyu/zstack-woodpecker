d4e59d6deab09fdc795c301e0b746fe4306aed13
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-09-26 18:36:15 +0800

    add import res_ops in all down pub case in mn_ha

--- integrationtest/vm/mn_ha/test_sep_pub_1_mn_hst_pub_net_dwn_crt_vm.py
+++ integrationtest/vm/mn_ha/test_sep_pub_1_mn_hst_pub_net_dwn_crt_vm.py
@@ -10,6 +10,7 @@ import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.operations.node_operations as node_ops
 import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm_header
+import zstackwoodpecker.operations.resource_operations as res_ops
 import test_stub
 import time
 import os
