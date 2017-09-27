8175c2669e40d49a72071fb088cc64de65578baf
glody <glody.guo@gmail.com>
2017-09-27 20:33:38 +0800

    Update case test_add_image_to_backup_recovery according new scenario

--- integrationtest/vm/hybrid/data_protect/test_add_image_to_backup_recovery.py
+++ integrationtest/vm/hybrid/data_protect/test_add_image_to_backup_recovery.py
@@ -1,6 +1,6 @@
 '''
 
-New Integration Test for data protect under hybrid.
+New Integration Test for add images to disaster backup storage. It is a negative test
 
 @author: Glody 
 '''
@@ -14,38 +14,38 @@ import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 
 test_stub = test_lib.lib_get_test_stub()
-dpbs_uuid = None
+disaster_bs_uuid = None
 
 def test():
-    global dpbs_uuid
-    vm_res = hyb_ops.get_data_protect_image_store_vm_ip(test_lib.all_scenario_config, test_lib.scenario_file, test_lib.deploy_config)
-    hostname = vm_res[0]
-    url = vm_res[1]
-    username = vm_res[2]
-    password = vm_res[3]
-    sshport = vm_res[4]
-    name = "BS-public"
-    data_protect_backup_storage = hyb_ops.add_disaster_image_store_bs(url, hostname, username, password, sshport, name)
-    dpbs_uuid = data_protect_backup_storage.uuid 
-    cond = res_ops.gen_query_conditions('resourceUuid', '=', dpbs_uuid)
-    system_tag = res_ops.query_resource(res_ops.SYSTEM_TAG, cond)[0]
-    if system_tag.tag != "remote":
-        test_util.test_fail("Here isn't 'remote' system tag for data protect bs")
-    zone = res_ops.query_resource(res_ops.ZONE)[0]
-    bs_ops.attach_backup_storage(dpbs_uuid, zone.uuid)
-
-    cond = res_ops.gen_query_conditions('name', '=', 'image_store_bs')
+    global disaster_bs_uuid
+    disaster_bs_dict = bs_ops.get_disaster_backup_storage_info(test_lib.deploy_config)
+    name = disaster_bs_dict['name']
+    description = disaster_bs_dict['description']
+    hostname = disaster_bs_dict['hostname']
+    url = disaster_bs_dict['url']
+    username = disaster_bs_dict['username']
+    password = disaster_bs_dict['password']
+    sshport = disaster_bs_dict['port']
+    #AddDisasterImageStoreBackupStorage
+    disaster_backup_storage = bs_ops.add_disaster_image_store_bs(url, hostname, username, password, sshport, name, description)
+    disaster_bs_uuid = disaster_backup_storage.uuid
+    #AttachBackupStorageToZone
+    zone_uuid = res_ops.query_resource(res_ops.ZONE)[0].uuid
+    bs_ops.attach_backup_storage(disaster_bs_uuid, zone_uuid)
+    #get a candidate image
+    cond = res_ops.gen_query_conditions('name', '=', 'sftp')
     local_bs_uuid = res_ops.query_resource(res_ops.BACKUP_STORAGE, cond)[0].uuid
     cond = res_ops.gen_query_conditions('backupStorageRefs.backupStorageUuid', '=', local_bs_uuid)
     image = res_ops.query_resource(res_ops.IMAGE, cond)[0]
-
+    #setup image option
     image_option = test_util.ImageOption()
     image_option.set_name('add_image_to_data_protect_bs')
     image_option.set_format(image.format)
     image_option.set_platform(image.platform)
     image_option.set_url(image.url)
     image_option.set_mediaType(image.mediaType)
-    image_option.set_backup_storage_uuid_list([dpbs_uuid])
+    image_option.set_backup_storage_uuid_list([disaster_bs_uuid])
+    #Try to add image to disaster bs directly
     try:
         new_img = img_ops.add_image(image_option)
     except Exception,e:
@@ -53,13 +53,12 @@ def test():
             test_util.test_pass('AddImage is forbidden in Disaster BS, Here is the expect error info: %s' %str(e))
 
     finally:
-        bs_ops.delete_backup_storage(dpbs_uuid)
+        bs_ops.delete_backup_storage(disaster_bs_uuid)
 
     test_util.test_fail('The Image is add to Disaster BS unexpectly')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
-    global dpbs_uuid
-    if dpbs_uuid != None:
-        bs_ops.delete_backup_storage(dpbs_uuid)
-    
+    global disaster_bs_uuid
+    if disaster_bs_uuid != None:
+        bs_ops.delete_backup_storage(disaster_bs_uuid)
