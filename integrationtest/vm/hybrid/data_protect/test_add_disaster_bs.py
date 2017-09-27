90f900876761d3ec29967bd595c9b51819d6cf62
glody <glody.guo@gmail.com>
2017-09-27 19:03:12 +0800

    change add_disaster_bs strategy

--- integrationtest/vm/hybrid/data_protect/test_add_disaster_bs.py
+++ integrationtest/vm/hybrid/data_protect/test_add_disaster_bs.py
@@ -1,40 +1,45 @@
 '''
 
-New Integration Test for data protect under hybrid.
+New Integration Test for add disaster backup storage.
 
 @author: Glody 
 '''
 
-import zstackwoodpecker.operations.hybrid_operations as hyb_ops
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.operations.backupstorage_operations as bs_ops
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 
 test_stub = test_lib.lib_get_test_stub()
-dpbs_uuid = None
+disaster_bs_uuid = None
 
 def test():
-    global dpbs_uuid
-    vm_res = hyb_ops.get_data_protect_image_store_vm_ip(test_lib.all_scenario_config, test_lib.scenario_file, test_lib.deploy_config)
-    hostname = vm_res[0]
-    url = vm_res[1]
-    username = vm_res[2]
-    password = vm_res[3]
-    sshport = vm_res[4]
-    name = "BS-public"
-    data_protect_backup_storage = hyb_ops.add_disaster_image_store_bs(url, hostname, username, password, sshport, name)
-    dpbs_uuid = data_protect_backup_storage.uuid 
-    cond = res_ops.gen_query_conditions('resourceUuid', '=', dpbs_uuid)
+    global disaster_bs_uuid
+    disaster_bs_dict = bs_ops.get_disaster_backup_storage_info(test_lib.deploy_config)
+    name = disaster_bs_dict.name
+    description = disaster_bs_dict.description
+    hostname = disaster_bs_dict.hostname
+    url = disaster_bs_dict.url
+    username = disaster_bs_dict.username
+    password = disaster_bs_dict.password
+    sshport = disaster_bs_dict.port
+    #AddDisasterImageStoreBackupStorage 
+    disaster_backup_storage = bs_ops.add_disaster_image_store_bs(url, hostname, username, password, sshport, name, description)
+    disaster_bs_uuid = disaster_backup_storage.uuid
+    #AttachBackupStorageToZone
+    zone_uuid = res_ops.query_resource(res_ops.ZONE)[0].uuid
+    bs_ops.attach_backup_storage(disaster_bs_uuid, zone_uuid)
+    #Check if the bs has 'remote' system tag
+    cond = res_ops.gen_query_conditions('resourceUuid', '=', disaster_bs_uuid)
     system_tag =  res_ops.query_resource(res_ops.SYSTEM_TAG, cond)[0]
     if system_tag.tag != "remote":
-        test_util.test_fail("Here isn't 'remote' system tag for data protect bs")
-    
-    bs_ops.delete_backup_storage(dpbs_uuid) 
-    test_util.test_pass('Setup data protect bs image store success, the uuid is %s' %dpbs_uuid)
+        test_util.test_fail("Here isn't 'remote' system tag for disaster bs")
+     
+    bs_ops.delete_backup_storage(disaster_bs_uuid) 
+    test_util.test_pass('Setup data protect bs image store success, the uuid is %s' %disaster_bs_uuid)
 
 #Will be called only if exception happens in test().
 def error_cleanup():
-    global dpbs_uuid
-    if dpbs_uuid != None:
-        bs_ops.delete_backup_storage(dpbs_uuid)
+    global disaster_bs_uuid
+    if disaster_bs_uuid != None:
+        bs_ops.delete_backup_storage(disaster_bs_uuid)
