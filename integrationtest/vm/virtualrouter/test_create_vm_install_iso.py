91ef0b3bd0b4e28ebd038a964cf6f8fb0280245f
quarkonics <quarkonics@gmail.com>
2017-09-30 13:02:44 +0800

    Give more time for ISO install

--- integrationtest/vm/virtualrouter/test_create_vm_install_iso.py
+++ integrationtest/vm/virtualrouter/test_create_vm_install_iso.py
@@ -81,7 +81,7 @@ def test():
     #if ssh_ok == 0:
     #    test_util.test_fail('fail to ssh to VM')
     ssh_timeout = test_lib.SSH_TIMEOUT
-    test_lib.SSH_TIMEOUT = 2400
+    test_lib.SSH_TIMEOUT = 3600
     test_lib.lib_set_vm_host_l2_ip(vm_inv)
     if not test_lib.lib_ssh_vm_cmd_by_agent_with_retry(host_ip, vm_ip, 'root', 'password', cmd):
         test_lib.SSH_TIMEOUT = ssh_timeout
