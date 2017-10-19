27408ca67a41de30e1740d49b9932d20e145c3dd
ads6ads6 <ads5ads5@yeah.net>
2017-10-19 10:20:38 +0800

    Update vpc vm connection test case

--- integrationtest/vm/vpc/test_stub.py
+++ integrationtest/vm/vpc/test_stub.py
@@ -19,6 +19,7 @@ import zstackwoodpecker.operations.image_operations as img_ops
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.operations.account_operations as acc_ops
 import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm_header
+import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.operations.scenario_operations as sce_ops
 import zstackwoodpecker.header.host as host_header
 import apibinding.inventory as inventory
@@ -36,20 +37,83 @@ from functools import wraps
 import itertools
 #import traceback
 
+l3_vlan_system_name_list=['l3VlanNetworkName1', "l3VlanNetwork3"]
+l3_vxlan_system_name_list= ["l3VxlanNetwork11", "l3VxlanNetwork12"]
+
+Port = test_state.Port
+
+rule1_ports = Port.get_ports(Port.rule1_ports)
+rule2_ports = Port.get_ports(Port.rule2_ports)
+rule3_ports = Port.get_ports(Port.rule3_ports)
+rule4_ports = Port.get_ports(Port.rule4_ports)
+rule5_ports = Port.get_ports(Port.rule5_ports)
+denied_ports = Port.get_denied_ports()
+target_ports = rule1_ports + rule2_ports + rule3_ports + rule4_ports + rule5_ports + denied_ports
+
 
 def create_vpc_vrouter():
-
+    vr_name= 'test_vpc'
+    conf = res_ops.gen_query_conditions('name', '=', 'test_vpc')
+    vr_list = res_ops.query_resource(res_ops.VIRTUALROUTER_VM, conf)
+    if vr_list:
+        return vr_list[0]
     vr_offering = res_ops.get_resource(res_ops.VR_OFFERING)[0]
-    return vpc_ops.create_vpc_vrouter(name="test_vpc", virtualrouter_offering_uuid=vr_offering.uuid)
+    return vpc_ops.create_vpc_vrouter(name=vr_name, virtualrouter_offering_uuid=vr_offering.uuid)
 
 
 def attach_all_l3_to_vpc_vr(vpc_vr):
-    l3_system_list=['l3VlanNetworkName1', "l3VlanNetwork3", "l3VxlanNetwork11", "l3VxlanNetwork12"]
-    l3_name_list = [os.environ.get(name) for name in l3_system_list]
-    l3_list = [test_lib.lib_get_l3_by_name(name) for name in l3_name_list]
+    l3_vlan_name_list = [os.environ.get(name) for name in l3_vlan_system_name_list]
+    l3_vxlan_name_list = [os.environ.get(name) for name in l3_vxlan_system_name_list]
 
+    l3_list = [test_lib.lib_get_l3_by_name(name) for name in l3_vlan_name_list + l3_vxlan_name_list]
+
+    l3_uuid_list = [nic.l3NetworkUuid for nic in vpc_vr.vmNics]
     for l3 in l3_list:
-        net_ops.attach_l3(l3.uuid, vpc_vr.uuid)
-        time.sleep(5)
+        if l3.uuid not in l3_uuid_list:
+            net_ops.attach_l3(l3.uuid, vpc_vr.uuid)
+            time.sleep(5)
 
 
+def create_vm_with_random_offering(vm_name, image_name=None, l3_name=None, session_uuid=None,
+                                   instance_offering_uuid=None, host_uuid=None, disk_offering_uuids=None,
+                                   root_password=None, ps_uuid=None, system_tags=None):
+    if image_name:
+        imagename = os.environ.get(image_name)
+        image_uuid = test_lib.lib_get_image_by_name(imagename).uuid
+    else:
+        conf = res_ops.gen_query_conditions('format', '!=', 'iso')
+        conf = res_ops.gen_query_conditions('system', '=', 'false', conf)
+        image_uuid = random.choice(res_ops.query_resource(res_ops.IMAGE, conf)).uuid
+
+    if l3_name:
+        l3name = os.environ.get(l3_name)
+        l3_net_uuid = test_lib.lib_get_l3_by_name(l3name).uuid
+    else:
+        l3_net_uuid = random.choice(res_ops.get_resource(res_ops.L3_NETWORK)).uuid
+
+    if not instance_offering_uuid:
+        conf = res_ops.gen_query_conditions('type', '=', 'UserVM')
+        instance_offering_uuid = random.choice(res_ops.query_resource(res_ops.INSTANCE_OFFERING, conf)).uuid
+
+    vm_creation_option = test_util.VmOption()
+    vm_creation_option.set_l3_uuids([l3_net_uuid])
+    vm_creation_option.set_image_uuid(image_uuid)
+    vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
+    vm_creation_option.set_name(vm_name)
+    if system_tags:
+        vm_creation_option.set_system_tags(system_tags)
+    if disk_offering_uuids:
+        vm_creation_option.set_data_disk_uuids(disk_offering_uuids)
+    if root_password:
+        vm_creation_option.set_root_password(root_password)
+    if host_uuid:
+        vm_creation_option.set_host_uuid(host_uuid)
+    if session_uuid:
+        vm_creation_option.set_session_uuid(session_uuid)
+    if ps_uuid:
+        vm_creation_option.set_ps_uuid(ps_uuid)
+
+    vm = test_vm_header.ZstackTestVm()
+    vm.set_creation_option(vm_creation_option)
+    vm.create()
+    return vm
--- integrationtest/vm/vpc/test_vpc_two_vm_connection.py
+++ integrationtest/vm/vpc/test_vpc_two_vm_connection.py
@@ -12,25 +12,50 @@ import zstackwoodpecker.operations.volume_operations as vol_ops
 import time
 
 
-flavor = dict(vm1_vm2_one_l3_vlan=dict(),
-              vm1_vm2_one_l3_vxlan=dict(),
-              vm1_l3_vlan_vm2_l3_vlan=dict(),
-              vm1_l3_vxlan_vm2_l3_vxlan=dict(),
-              vm1_l3_vlan_vm2_l3_vxlan=dict(),
-              vm1_vm2_one_l3_vlan_migrate = dict(),
-              vm1_l3_vlan_vm2_l3_vlan_migrate= dict(),
-              vm1_l3_vxlan_vm2_l3_vxlan_vrreboot=dict(),
-              )
+VLAN1_NAME, VLAN2_NAME = ['l3VlanNetworkName1', "l3VlanNetwork3"]
+VXLAN1_NAME, VXLAN2_NAME = ["l3VxlanNetwork11", "l3VxlanNetwork12"]
+
+case_flavor = dict(vm1_vm2_one_l3_vlan=               dict(vm1l3=VLAN1_NAME, vm2l3=VLAN1_NAME, migrate=False, vrreboot=False),
+                   vm1_vm2_one_l3_vxlan=              dict(vm1l3=VXLAN1_NAME, vm2l3=VXLAN1_NAME, migrate=False, vrreboot=False),
+                   vm1_l3_vlan_vm2_l3_vlan=           dict(vm1l3=VLAN1_NAME, vm2l3=VLAN2_NAME, migrate=False, vrreboot=False),
+                   vm1_l3_vxlan_vm2_l3_vxlan=         dict(vm1l3=VXLAN1_NAME, vm2l3=VXLAN2_NAME, migrate=False, vrreboot=False),
+                   vm1_l3_vlan_vm2_l3_vxlan=          dict(vm1l3=VLAN1_NAME, vm2l3=VXLAN1_NAME, migrate=False, vrreboot=False),
+                   vm1_vm2_one_l3_vlan_migrate=       dict(vm1l3=VLAN1_NAME, vm2l3=VLAN1_NAME, migrate=False, vrreboot=False),
+                   vm1_l3_vlan_vm2_l3_vlan_migrate=   dict(vm1l3=VLAN1_NAME, vm2l3=VLAN2_NAME, migrate=True, vrreboot=False),
+                   vm1_l3_vxlan_vm2_l3_vxlan_vrreboot=dict(vm1l3=VXLAN1_NAME, vm2l3=VXLAN2_NAME, migrate=False, vrreboot=True),
+                   )
 
 
 test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
 
+
 def test():
+    flavor = case_flavor[os.environ.get('CASE_FLAVOR')]
     test_util.test_dsc("create vpc vrouter and attach vpc l3 to vpc")
     vr_inv = test_stub.create_vpc_vrouter()
+    time.sleep(30)
     test_stub.attach_all_l3_to_vpc_vr(vr_inv)
 
-    test_util.test_dsc("create two vm, vm1 in , vm2 in")
+    test_util.test_dsc("create two vm, vm1 in l3 {}, vm2 in l3 {}".format(flavor['vm1l3'], flavor['vm2l3']))
+    vm1, vm2 = [test_stub.create_vm_with_random_offering(vm_name='vpc_vm_{}'.format(name), l3_name=name) for name in (flavor['vm1l3'], flavor['vm2l3']) ]
+
+    for vm in (vm1, vm2):
+        test_obj_dict.add_vm(vm)
+        vm.check()
+
+    vm1_inv = vm1.get_vm()
+    vm2_inv = vm2.get_vm()
 
     test_util.test_dsc("test two vm connectivity")
+    test_lib.lib_check_ping(vm1_inv, vm2_inv.vmNics[0].ip)
+    test_lib.lib_check_ping(vm2_inv, vm1_inv.vmNics[0].ip)
+
+    test_lib.lib_check_ports_in_a_command(vm1_inv, vm1_inv.vmNics[0].ip,
+                                          vm2_inv.vmNics[0].ip, test_stub.target_ports, [], vm2_inv)
+
+    test_lib.lib_check_ports_in_a_command(vm2_inv, vm2_inv.vmNics[0].ip,
+                                          vm1_inv.vmNics[0].ip, test_stub.target_ports, [], vm1_inv)
+
+def env_recover():
+    test_lib.lib_error_cleanup(test_obj_dict)
