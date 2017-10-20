968d36c142f7a603d6c2300613aeb89039756683
ads6ads6 <ads5ads5@yeah.net>
2017-10-20 14:13:40 +0800

    Update test cases

--- integrationtest/vm/vpc/test_vpc_one_eip_two_vm.py
+++ integrationtest/vm/vpc/test_vpc_one_eip_two_vm.py
@@ -13,9 +13,10 @@ VXLAN1_NAME, VXLAN2_NAME = ["l3VxlanNetwork11", "l3VxlanNetwork12"]
 
 vpc1_l3_list = [VLAN1_NAME, VLAN2_NAME]
 vpc2_l3_list = [VXLAN1_NAME, VXLAN2_NAME]
+
 vpc_l3_list = [vpc1_l3_list, vpc2_l3_list]
 vpc_name_list = ['vpc1','vpc2']
-vr_inv_list = []
+
 
 case_flavor = dict(vm1_vm2_one_l3_vlan=     dict(vm1l3=VLAN1_NAME, vm2l3=VLAN1_NAME),
                    vm1_vm2_one_vpc_2vlan=   dict(vm1l3=VLAN1_NAME, vm2l3=VLAN2_NAME),
@@ -25,6 +26,7 @@ case_flavor = dict(vm1_vm2_one_l3_vlan=     dict(vm1l3=VLAN1_NAME, vm2l3=VLAN1_N
 
 test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
+vr_inv_list = []
 
 
 def test():
@@ -32,14 +34,14 @@ def test():
     test_util.test_dsc("create vpc vrouter and attach vpc l3 to vpc")
     for vpc_name in vpc_name_list:
         vr_inv_list.append(test_stub.create_vpc_vrouter(vpc_name))
-    for vr_inv, l3_list in izip(vr_inv_list, vpc1_l3_list):
+    for vr_inv, l3_list in izip(vr_inv_list, vpc_l3_list):
         test_stub.attach_all_l3_to_vpc_vr(vr_inv, l3_list)
 
     test_util.test_dsc("create two vm, vm1 in l3 {}, vm2 in l3 {}".format(flavor['vm1l3'], flavor['vm2l3']))
     vm1 = test_stub.create_vm_with_random_offering(vm_name='vpc_vm_{}'.format(flavor['vm1l3']), l3_name=flavor['vm1l3'])
     test_obj_dict.add_vm(vm1)
     vm1.check()
-    vm2 = test_stub.create_vm_with_random_offering(vm_name='vpc_vm_{}'.format(flavor['vm1l3']), l3_name=flavor['vm1l3'])
+    vm2 = test_stub.create_vm_with_random_offering(vm_name='vpc_vm_{}'.format(flavor['vm2l3']), l3_name=flavor['vm2l3'])
     test_obj_dict.add_vm(vm2)
     vm2.check()
 
