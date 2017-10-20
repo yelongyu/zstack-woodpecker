fb6d227ab323d8b1166325eebcffe0c869b49aea
ads6ads6 <ads5ads5@yeah.net>
2017-10-20 11:02:34 +0800

    fix vpc attach detach test case

--- integrationtest/vm/vpc/test_vpc_l3_attach_detach.py
+++ integrationtest/vm/vpc/test_vpc_l3_attach_detach.py
@@ -8,6 +8,7 @@ import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.operations.vm_operations as vm_ops
 import zstackwoodpecker.operations.resource_operations as res_ops
 import zstackwoodpecker.operations.net_operations as net_ops
+import time
 import random
 import os
 from itertools import izip
@@ -25,7 +26,6 @@ all_vpc_l3_list = ['l3VlanNetworkName1'] + ["l3VlanNetwork{}".format(i) for i in
 @test_lib.pre_execution_action(test_stub.remove_all_vpc_vrouter)
 def test():
 
-
     test_util.test_dsc("create vpc vrouter and attach vpc l3 to vpc")
 
     vr_inv = test_stub.create_vpc_vrouter()
@@ -34,12 +34,14 @@ def test():
         test_stub.attach_all_l3_to_vpc_vr(vr_inv, all_vpc_l3_list)
 
         conf = res_ops.gen_query_conditions('uuid', '=', vr_inv.uuid)
-        vr_inv = res_ops.query_resource(res_ops.VM_INSTANCE, conf)
+        vr_inv = res_ops.query_resource(res_ops.VM_INSTANCE, conf)[0]
         nic_uuid_list = [nic.uuid for nic in vr_inv.vmNics if nic.metaData == '4']
         assert len(nic_uuid_list) == len(all_vpc_l3_list)
 
         for nic_uuid in nic_uuid_list:
             net_ops.detach_l3(nic_uuid)
+
+        time.sleep(10)
 
 
 def env_recover():
