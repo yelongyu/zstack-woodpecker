fb07e1abf1411dd2e7728ca7cf9cb1408d8689ad
ads6ads6 <ads5ads5@yeah.net>
2017-10-22 15:18:52 +0800

    update vpc test case

--- integrationtest/vm/vpc/test_vpc_two_eip_two_vm_connection.py
+++ integrationtest/vm/vpc/test_vpc_two_eip_two_vm_connection.py
@@ -43,12 +43,10 @@ def test():
     test_util.test_dsc("create two vm, vm1 in l3 {}, vm2 in l3 {}".format(flavor['vm1l3'], flavor['vm2l3']))
     vm1 = test_stub.create_vm_with_random_offering(vm_name='vpc_vm_{}'.format(flavor['vm1l3']), l3_name=flavor['vm1l3'])
     test_obj_dict.add_vm(vm1)
-
+    vm1.check()
     vm2 = test_stub.create_vm_with_random_offering(vm_name='vpc_vm_{}'.format(flavor['vm2l3']), l3_name=flavor['vm2l3'])
     test_obj_dict.add_vm(vm2)
-
-    for vm in (vm1,vm2):
-        vm.check()
+    vm2.check()
 
     vr_pub_nic = test_lib.lib_find_vr_pub_nic(vr_inv_list[0])
 
