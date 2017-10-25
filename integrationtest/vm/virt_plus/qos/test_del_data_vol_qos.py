5b90acbd382d28942c20184fc0a89e1d92692c42
glody <glody.guo@gmail.com>
2017-10-25 16:02:44 +0800

    Add case test_del_data_vol_qos.py

--- integrationtest/vm/virt_plus/qos/test_del_data_vol_qos.py
+++ integrationtest/vm/virt_plus/qos/test_del_data_vol_qos.py
@@ -1,6 +1,6 @@
 '''
 This case can not execute parallelly
-@author: glody
+@author: glody 
 '''
 import os
 import zstackwoodpecker.test_util as test_util
@@ -22,16 +22,18 @@ volume_offering_uuid = None
 
 def test():
     global volume_offering_uuid
-    test_util.test_dsc('Test VM data volume bandwidth QoS by 20MB')
+    test_util.test_dsc('Test VM disk bandwidth QoS by 20MB')
 
     #unit is KB
     volume_bandwidth = 5*1024*1024
     new_volume_offering = test_lib.lib_create_disk_offering(volume_bandwidth = volume_bandwidth)
+
     volume_offering_uuid = new_volume_offering.uuid
+
     vm = test_stub.create_vm(vm_name = 'vm_volume_qos', disk_offering_uuids = [volume_offering_uuid])
     test_obj_dict.add_vm(vm)
     vm_inv = vm.get_vm()
-    cond = res_ops.gen_query_conditions("vmInstanceUuid", '=', vm_inv.uuid) 
+    cond = res_ops.gen_query_conditions("vmInstanceUuid", '=', vm_inv.uuid)
     cond = res_ops.gen_query_conditions("type", '=', 'Data', cond)
     volume_uuid = res_ops.query_resource(res_ops.VOLUME, cond)[0].uuid
     test_lib.lib_mkfs_for_volume(volume_uuid, vm_inv)
@@ -39,10 +41,16 @@ def test():
     user_name = 'root'
     user_password = 'password'
     os.system("sshpass -p '%s' ssh %s@%s 'mount /dev/vdb1 %s'"%(user_password, user_name, vm_inv.vmNics[0].ip, path))
-    vm.check()
     test_stub.make_ssh_no_password(vm_inv)
     test_stub.install_fio(vm_inv)
     test_stub.test_fio_bandwidth(vm_inv, volume_bandwidth, path)
+    if vm_ops.get_vm_disk_qos(test_lib.lib_get_data_volumes(vm_inv)[0].uuid).volumeBandwidth != volume_bandwidth:
+        test_util.test_fail('Retrieved disk qos not match')
+    vm_ops.set_vm_disk_qos(test_lib.lib_get_data_volumes(vm_inv)[0].uuid, volume_bandwidth/2)
+    test_stub.test_fio_bandwidth(vm_inv, volume_bandwidth/2, path)
+    vm_ops.del_vm_disk_qos(test_lib.lib_get_data_volumes(vm_inv)[0].uuid)
+    if test_stub.test_fio_bandwidth(vm_inv, volume_bandwidth/2, path, raise_exception=False):
+        test_util.test_fail('disk qos is not expected to have limit after qos setting is deleted')
     vol_ops.delete_disk_offering(volume_offering_uuid)
     test_lib.lib_robot_cleanup(test_obj_dict)
 
@@ -51,5 +59,5 @@ def test():
 #Will be called only if exception happens in test().
 def error_cleanup():
     test_lib.lib_error_cleanup(test_obj_dict)
-    if new_offering_uuid:
-        vm_ops.delete_instance_offering(new_offering_uuid)
+    if volume_offering_uuid:
+        vol_ops.delete_disk_offering(volume_offering_uuid)
