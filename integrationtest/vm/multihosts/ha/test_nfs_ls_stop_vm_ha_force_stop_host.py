b80ad49b301d31f1abc49c0ba19feaf251a8b602
zsyzsyhao <zsyzsyhao@foxmail.com>
2017-12-01 21:46:17 +0800

    fix for vm ha virtual host ip is 192

--- integrationtest/vm/multihosts/ha/test_nfs_ls_stop_vm_ha_force_stop_host.py
+++ integrationtest/vm/multihosts/ha/test_nfs_ls_stop_vm_ha_force_stop_host.py
@@ -89,7 +89,7 @@ def test():
 
     host_list = test_stub.get_sce_hosts(test_lib.all_scenario_config, test_lib.scenario_file)
     for host in host_list:
-        if host.ip_ == host_ip:
+        if host.ip_ == host_ip or host.managementIp_ == host_ip:
             test_host = host
             break
     if not test_host:
