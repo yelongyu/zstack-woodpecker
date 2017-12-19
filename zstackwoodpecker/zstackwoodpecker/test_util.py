a9b6858974512007663b31199f437f61a712a571
quarkonics <quarkonics@gmail.com>
2017-12-19 23:21:14 +0800

    Revert "update util_cluster scenario_operations.py est_stub.py test_zstack_install_on_cos71.py"
    This reverts commit 15387e8caf5240a6d6ac6a4bfc7dc6ca4200f14a.

--- zstackwoodpecker/zstackwoodpecker/test_util.py
+++ zstackwoodpecker/zstackwoodpecker/test_util.py
@@ -292,7 +292,6 @@ class ClusterOption(DataOption):
     def __init__(self):
         self.hypervisor_type = None
         self.type = 'zstack'
-        self.zone_uuid = None
         super(ClusterOption, self).__init__()
 
     def set_hypervisor_type(self, hypervisor_type):
@@ -306,12 +305,6 @@ class ClusterOption(DataOption):
 
     def get_type(self):
         return self.type
-
-    def set_zone_uuid(self, zone_uuid):
-        self.zone_uuid = zone_uuid
-
-    def get_zone_uuid(self):
-        return self.zone_uuid
 
 class IpRangeOption(DataOption):
     def __init__(self):
