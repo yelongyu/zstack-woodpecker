ce6e663fb7d38640a711a863a5c31135be891fb6
zsyzsyhao <zsyzsyhao@foxmail.com>
2018-01-02 18:35:04 +0800

    amplify the time limitation for vm none recover to running

--- integrationtest/vm/multihosts/ha/test_ls_vm_ha_none_net_discon_connect_vm_status.py
+++ integrationtest/vm/multihosts/ha/test_ls_vm_ha_none_net_discon_connect_vm_status.py
@@ -112,7 +112,7 @@ def test():
 
     cond = res_ops.gen_query_conditions('name', '=', 'ls_vm_none_status')
     cond = res_ops.gen_query_conditions('uuid', '=', vm2.vm.uuid, cond)
-    for i in range(0, 120):
+    for i in range(0, 240):
         if res_ops.query_resource(res_ops.VM_INSTANCE, cond)[0].state != "Running":
             time.sleep(1)
             continue
