f56498475e4bc7f6856a47b50e99294871bc234b
quarkonics <quarkonics@gmail.com>
2018-01-15 16:23:03 +0800

    Add more debug info for basic/test_get_current_time.py

--- integrationtest/vm/basic/test_get_current_time.py
+++ integrationtest/vm/basic/test_get_current_time.py
@@ -14,6 +14,6 @@ def test():
     system_time1 = int(time.time())
     current_time = schd_ops.get_current_time().currentTime
     system_time2 = int(time.time())
-    if system_time1 != current_time.Seconds and system_time2 != current_time.Seconds:
-        test_util.test_fail('get_current_time not get expected time')
+    if system_time1 > current_time.Seconds and system_time2 < current_time.Seconds:
+	    test_util.test_fail('get_current_time not get expected time[%s, %s]: %s' % (system_time1, system_time2, current_time.Seconds))
     test_util.test_pass('Create VM Test Success')
