650c08606078c8d01a4213b82f51cacbe5805ed8
quarkonics <quarkonics@gmail.com>
2018-01-29 20:46:33 +0800

    Fix parameters for tools/setup_ceph_nodes.sh

--- tools/setup_ceph_nodes.sh
+++ tools/setup_ceph_nodes.sh
@@ -1,7 +1,7 @@
 OS_CENTOS=7.2
 cat /etc/redhat-release | grep '7.2' || OS_CENTOS=7.4
 if [ "${OS_CENTOS}" == "7.2" ]; then
-	bash -x `dirname $0`/setup_ceph_h_nodes.sh
+	bash -x `dirname $0`/setup_ceph_h_nodes.sh $@
 else
-	bash -x `dirname $0`/setup_ceph_j_nodes.sh
+	bash -x `dirname $0`/setup_ceph_j_nodes.sh $@
 fi
