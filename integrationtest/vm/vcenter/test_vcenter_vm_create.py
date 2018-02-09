191f7b95edfec558439d268f92e68aeca205606f
moyu <w13880213920@live.com>
2018-02-09 16:30:44 +0800

    fix some mistakes in old vcenter cases

--- integrationtest/vm/vcenter/test_vcenter_vm_create.py
+++ integrationtest/vm/vcenter/test_vcenter_vm_create.py
@@ -14,27 +14,15 @@ import zstacklib.utils.ssh as ssh
 import test_stub
 import os
 
-
-vcenter_uuid = None
 vm = None
 
 def test():
-    global vcenter_uuid, vm
+    global vm
 
-    vcenter1_name = os.environ['vcenter2_name']
-    vcenter1_domain_name = os.environ['vcenter2_ip']
-    vcenter1_username = os.environ['vcenter2_domain_name']
-    vcenter1_password = os.environ['vcenter2_password']
-    ova_image_name = os.environ['vcenter2_template_exist']
-    network_pattern1 = os.environ['vcenter2_network_pattern1']
+    ova_image_name = os.environ['vcenterDefaultmplate']
+    network_pattern1 = os.environ['vcenterDefaultNetwork']
 
 
-    zone_uuid = res_ops.get_resource(res_ops.ZONE)[0].uuid
-    inv = vct_ops.add_vcenter(vcenter1_name, vcenter1_domain_name, vcenter1_username, vcenter1_password, True, zone_uuid)
-    vcenter_uuid = inv.uuid
-
-    if vcenter_uuid == None:
-        test_util.test_fail("vcenter_uuid is None")
 
     vm = test_stub.create_vm_in_vcenter(vm_name = 'vm-create-test', image_name = ova_image_name, l3_name = network_pattern1)
     vm.check()
@@ -43,16 +31,12 @@ def test():
     vm.check()
     vm.expunge()
 
-    vct_ops.delete_vcenter(vcenter_uuid)
     test_util.test_pass("Creating vm of vcenter test passed.")
 
 
 
 def error_cleanup():
-    global vcenter_uuid, vm
+    global vm
     if vm:
-        vm.delete()
+        vm.destroy()
         vm.expunge()
-
-    if vcenter_uuid:
-        vct_ops.delete_vcenter(vcenter_uuid)
