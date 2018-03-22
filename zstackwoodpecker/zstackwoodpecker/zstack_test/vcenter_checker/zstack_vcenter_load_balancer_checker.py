49dbf8f8b7e621f1625da5902cdeb321590ca15f
moyu <w13880213920@live.com>
2018-03-22 19:07:23 +0800

    update vcenter lb chechker

--- zstackwoodpecker/zstackwoodpecker/zstack_test/vcenter_checker/zstack_vcenter_load_balancer_checker.py
+++ zstackwoodpecker/zstackwoodpecker/zstack_test/vcenter_checker/zstack_vcenter_load_balancer_checker.py
@@ -13,6 +13,7 @@ import zstacktestagent.plugins.vm as vm_plugin
 import zstacktestagent.plugins.host as host_plugin
 import zstacktestagent.testagent as testagent
 import apibinding.inventory as inventory
+import zstacklib.utils.shell as shell
 
 class zstack_vcenter_lbl_checker(checker_header.TestChecker):
     '''check virtual router load balancer listener. '''
@@ -24,13 +25,18 @@ class zstack_vcenter_lbl_checker(checker_header.TestChecker):
     def get_ssh_ip_result(self):
         vm = self.vm_list[0]
         host = test_lib.lib_get_vm_host(vm)
-        vm_command = '/sbin/ip a|grep inet'
         port = self.lbl.get_creation_option().get_load_balancer_port()
-        vm_cmd_result = test_lib.lib_execute_ssh_cmd(self.vip_ip, \
+        iport = self.lbl.get_creation_option().get_instance_port()
+        if iport == 22:
+            vm_command = '/sbin/ip a|grep inet'
+            vm_cmd_result = test_lib.lib_execute_ssh_cmd(self.vip_ip, \
                 test_lib.lib_get_vm_username(vm), \
                 test_lib.lib_get_vm_password(vm), \
                 vm_command,\
                 port = port)
+        if iport == 80:
+            vm_command = 'curl %s:%s' % (self.vip_ip, port)
+            vm_cmd_result = shell.call('%s' % vm_command)
 
         if not vm_cmd_result:
             test_util.test_logger('Checker result: FAIL to execute test ssh command in vip: %s for lb: %s.' % (self.vip_ip, self.lbl_uuid))
