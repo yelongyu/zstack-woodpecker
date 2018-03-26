a0e307d6e92879b63f5a7b00a8da588ee54833ba
zsyzsyhao <zsyzsyhao@foxmail.com>
2018-03-26 15:06:12 +0800

    enhance check_mn_start.sh with start time filter

--- tools/check_mn_start.sh
+++ tools/check_mn_start.sh
@@ -11,12 +11,36 @@ Plugin system has been initialized successfully
 Management node: managementNode.* starts successfully
 EOF`
 
+reboot_key=$(last reboot|head -n 1|awk '{print $7,$8}') #26 13:42
+auto_find_reboot_line_num(){
+    machine_start_time=$1
+    min=$(echo $reboot_key|awk '{print $2}'|cut -d: -f1)
+    sec=$(echo $reboot_key|awk '{print $2}'|cut -d: -f2)
+    day=$(echo $reboot_key|awk '{print $1}')
+    for i in `seq 5`
+    do
+        let min_sec_raw_cnt=min*60+sec
+        let min_sec_raw_cnt=min_sec_raw_cnt+1
+        let min=min_sec_raw_cnt/60
+        let sec=min_sec_raw_cnt%60
+        grep "$day $min:$sec" $MN_LOG >/dev/null
+        if [ $? -eq 0 ];then
+            #echo "$day $min:$sec"
+            line_num=$(grep "$day $min:$sec" $MN_LOG -n|head -n 1|cut -d: -f1)
+            echo $line_num
+            exit 0
+        fi
+    done
+    echo ""
+    exit 1
+}
+reboot_line_num=$(auto_find_reboot_line_num $reboot_key)
 
 cnt=1
 while read line
 do
     #echo $line
-    cat $MN_LOG|grep "$line" >/tmp/mn_filtered.log
+    tail -n +$reboot_line_num $MN_LOG|grep "$line" >/tmp/mn_filtered.log
     ret=$?
     if [ $ret -ne 0 -a $cnt -le 3 ];then
         echo "@@@NOT FIND MANDATORY KEY IN MN LOG@@@->KEY= "${line}
