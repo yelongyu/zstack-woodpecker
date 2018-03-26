5029943d9b2daeac5b8b46ee295782f5e42b962c
zsyzsyhao <zsyzsyhao@foxmail.com>
2018-03-26 20:51:49 +0800

    replace repo folder with woodpecker ip

--- integrationtest/vm/mn_ha/test_stub.py
+++ integrationtest/vm/mn_ha/test_stub.py
@@ -460,7 +460,8 @@ def deploy_ha_env(scenarioConfig, scenarioFile, deploy_config, config_json, depl
             ceph_node_ip = get_host_by_index_in_scenario_file(scenarioConfig, scenarioFile, 0).ip_ 
             mn_image_path = "/home/%s/mn.qcow2" % ceph_node_ip
             ssh.scp_file(mn_img, mn_image_path, ceph_node_ip, test_host_config.imageUsername_, test_host_config.imagePassword_)
-            qemu_kvm_repo_path = "/home/%s/qemu-kvm-ev.repo" % ceph_node_ip
+            woodpecker_vm_ip = shell.call("ip r | grep src | head -1 | awk '{print $NF}'").strip()
+            qemu_kvm_repo_path = "/home/%s/qemu-kvm-ev.repo" % woodpecker_vm_ip
             ssh.scp_file(qemu_kvm_repo_path, "/etc/yum.repos.d/", ceph_node_ip, test_host_config.imageUsername_, test_host_config.imagePassword_)
             cmd0="yum install -y --disablerepo=* --enablerepo=zstack-local,qemu-kvm-ev qemu-img"
             test_util.test_logger("[%s] %s" % (ceph_node_ip, cmd0))
