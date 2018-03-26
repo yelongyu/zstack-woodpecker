4c29bc63a9e63d83bf5e5a3e68bca5cadf2f38f4
zsyzsyhao <zsyzsyhao@foxmail.com>
2018-03-26 20:25:08 +0800

    fix shell invoke when mn start slow walk around

--- integrationtest/vm/mn_ha/test_stub.py
+++ integrationtest/vm/mn_ha/test_stub.py
@@ -889,21 +889,23 @@ def auto_set_mn_ip(scenario_file):
 
 def modify_startup_script():
     mn_ip = os.environ['zstackHaVip']
-    cmd = "sed -i \"s:zstack-ctl start:zstack-ctl start_node --timeout 3000:g\" /etc/init.d/zstack-server"
+    cmd = r'sed -i "s:zstack-ctl start:zstack-ctl start_node --timeout 3000:g" /etc/init.d/zstack-server'
     test_lib.lib_execute_ssh_cmd(mn_ip, "root", "password", cmd)
     time.sleep(1)
-    cmd = "sed -i /zstack-ctl start_node --timeout 3000/ a \'    ZSTACK_HOME=$zstack_app zstack-ctl start_ui\' /etc/init.d/zstack-server"
+    cmd = r'sed -i "/zstack-ctl start_node --timeout 3000/ a\    ZSTACK_HOME=$zstack_app zstack-ctl start_ui" /etc/init.d/zstack-server'
     test_lib.lib_execute_ssh_cmd(mn_ip, "root", "password", cmd)
 
 
 def restart_mn_node_with_long_timeout():
     mn_ip = os.environ['zstackHaVip']
+    test_lib.lib_wait_target_up(mn_ip, '22', 120)
     check_mn_tool_path = "%s/%s" %(os.environ.get('woodpecker_root_path'), '/tools/check_mn_start.sh')
+    test_util.test_logger("check_mn_tool_path:[%s],mn_ip:[%s]" %(check_mn_tool_path, mn_ip))
     ssh.scp_file(check_mn_tool_path, "/home/", mn_ip, "root", "password")
     cmd = "bash /home/check_mn_start.sh"
     ret1, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", True, 22)
     test_util.test_logger("check_mn_start.sh output:[%s],stderr:[%s]" %(output,stderr))
-    cmd = "zstack-ctl status|grep 'MN status'|cut -d: -f2"
+    cmd = "zstack-ctl status|grep 'MN status'|awk '{print $3}'"
     ret2, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", True, 22)
     if lower(stdout.strip().strip('\n')) != "running" and str(ret1) == "0" :
         cmd = "zstack-ctl stop"
