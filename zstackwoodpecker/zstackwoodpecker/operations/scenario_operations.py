a238c82ea819aa95dfbbf3d6e1f94f12a5ea884d
zsyzsyhao <zsyzsyhao@foxmail.com>
2018-05-08 17:32:28 +0800

    fix var invoking in reboot optimize

--- zstackwoodpecker/zstackwoodpecker/operations/scenario_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/scenario_operations.py
@@ -889,7 +889,7 @@ def setup_iscsi_initiator(zstack_management_ip, vm_inv, vm_config, deploy_config
             time.sleep(180) #This is a must, or host will not find mpatha and mpatha2 uuid
             recover_after_host_vm_reboot(vm_inv, vm_config, deploy_config)
             
-        thread_list = []
+        thd_list = []
         for vm_ip,vm_config in zip(HOST_INITIATOR_IP_LIST, HOST_INITIATOR_VM_CONFIG_LIST):
             #vm_inv = get_vm_inv_by_vm_ip(zstack_management_ip, vm_ip)
             #vm_uuid = vm_inv.uuid
@@ -902,7 +902,7 @@ def setup_iscsi_initiator(zstack_management_ip, vm_inv, vm_config, deploy_config
             thd.daemon = True
             thd.start()
 
-        for thd in thread_list:
+        for thd in thd_list:
             thd.join()
 
 
