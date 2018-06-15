9a8a57c23c3f239d0f0e36dcd82074932fcac718
quarkonics <quarkonics@gmail.com>
2018-06-15 11:19:35 +0800

    Update simulator/iam2/test_iam2_project_basic_ops_image.py

--- integrationtest/vm/simulator/iam2/test_iam2_project_basic_ops_image.py
+++ integrationtest/vm/simulator/iam2/test_iam2_project_basic_ops_image.py
@@ -36,31 +36,63 @@ virtual_id_uuid = None
 project_admin_uuid = None
 test_stub = test_lib.lib_get_test_stub()
 
+case_flavor = dict(project_admin=                   dict(target_role='project_admin'),
+                   project_operator=                dict(target_role='project_operator'),
+                   project_member=                  dict(target_role='project_member'),
+                   )
 
 def test():
     global project_uuid, project_admin_uuid, virtual_id_uuid
 
+    flavor = case_flavor[os.environ.get('CASE_FLAVOR')]
     # 1 create project
     project_name = 'test_project'
     project = iam2_ops.create_iam2_project(project_name)
     project_uuid = project.uuid
     project_linked_account_uuid = project.linkedAccountUuid
 
-    # 2 create virtual id
-    project_admin_name = 'username'
-    project_admin_password = 'password'
-    project_admin_uuid = iam2_ops.create_iam2_virtual_id(project_admin_name, project_admin_password).uuid
-    virtual_id_uuid = iam2_ops.create_iam2_virtual_id('usernametwo', 'password').uuid
+    if flavor['target_role'] == 'project_admin':
+        # 2 create virtual id
+        project_admin_name = 'username'
+        project_admin_password = 'password'
+        project_admin_uuid = iam2_ops.create_iam2_virtual_id(project_admin_name, project_admin_password).uuid
+        virtual_id_uuid = iam2_ops.create_iam2_virtual_id('usernametwo', 'password').uuid
+    
+        # 3 create project admin
+        iam2_ops.add_iam2_virtual_ids_to_project([project_admin_uuid],project_uuid)
+        attributes = [{"name": "__ProjectAdmin__", "value": project_uuid}]
+        iam2_ops.add_attributes_to_iam2_virtual_id(project_admin_uuid, attributes)
 
-    # 3 create project admin
-    iam2_ops.add_iam2_virtual_ids_to_project([project_admin_uuid],project_uuid)
-    attributes = [{"name": "__ProjectAdmin__", "value": project_uuid}]
-    iam2_ops.add_attributes_to_iam2_virtual_id(project_admin_uuid, attributes)
+        # login in project by project admin
+        project_admin_session_uuid = iam2_ops.login_iam2_virtual_id(project_admin_name, project_admin_password)
+        project_login_uuid = iam2_ops.login_iam2_project(project_name, session_uuid=project_admin_session_uuid).uuid
+        # iam2_ops.remove_attributes_from_iam2_virtual_id(virtual_id_uuid, attributes)
+    elif flavor['target_role'] == 'project_operator':
+        project_operator_name = 'username2'
+        project_operator_password = 'password'
+        attributes = [{"name": "__ProjectOperator__", "value": project_uuid}]
+        project_operator_uuid = iam2_ops.create_iam2_virtual_id(project_operator_name,project_operator_password,attributes=attributes).uuid
+        virtual_id_uuid = iam2_ops.create_iam2_virtual_id('usernamethree','password').uuid
 
-    # 4 login in project by project admin
-    project_admin_session_uuid = iam2_ops.login_iam2_virtual_id(project_admin_name, project_admin_password)
-    project_login_uuid = iam2_ops.login_iam2_project(project_name, session_uuid=project_admin_session_uuid).uuid
-    # iam2_ops.remove_attributes_from_iam2_virtual_id(virtual_id_uuid, attributes)
+        # login in project by project operator
+        iam2_ops.add_iam2_virtual_ids_to_project([project_operator_uuid],project_uuid)
+        project_operator_session_uuid = iam2_ops.login_iam2_virtual_id(project_operator_name,project_operator_password)
+        project_login_uuid = iam2_ops.login_iam2_project(project_name,session_uuid=project_operator_session_uuid).uuid
+    elif flavor['target_role'] == 'project_member':
+	plain_user_name = 'username'
+	plain_user_password = 'password'
+	plain_user_uuid = iam2_ops.create_iam2_virtual_id(plain_user_name, plain_user_password,
+	                                                  project_uuid=project_uuid).uuid
+	# 3 add virtual id to project
+	iam2_ops.add_iam2_virtual_ids_to_project([plain_user_uuid],project_uuid)
+
+	# 4 login in project by plain user
+	plain_user_session_uuid = iam2_ops.login_iam2_virtual_id(plain_user_name, plain_user_password)
+
+	# 4 login in project
+	#project_inv=iam2_ops.get_iam2_projects_of_virtual_id(plain_user_session_uuid)
+	project_login_uuid = iam2_ops.login_iam2_project(project_name, plain_user_session_uuid).uuid
+
 
     # Image related ops: Add, Delete, Expunge, sync image size, Update QGA, delete, expunge
     bs = res_ops.query_resource(res_ops.BACKUP_STORAGE)[0]
