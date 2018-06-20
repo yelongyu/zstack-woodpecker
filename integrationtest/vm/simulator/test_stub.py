94f7e69b98c17c82426aa8de6809ff02b8839636
quarkonics <quarkonics@gmail.com>
2018-06-20 15:36:12 +0800

    Fix l3 name

--- integrationtest/vm/simulator/test_stub.py
+++ integrationtest/vm/simulator/test_stub.py
@@ -36,7 +36,7 @@ def create_vm(vm_creation_option=None, volume_uuids=None, root_disk_uuid=None,
         cond = res_ops.gen_query_conditions('mediaType', '!=', 'ISO')
         cond = res_ops.gen_query_conditions('platform', '=', 'Linux', cond)
         l3net_uuid = test_lib.lib_get_l3_by_name(
-            os.environ.get('l3VlanNetwork3')).uuid
+            os.environ.get('l3VlanNetworkName3')).uuid
 	if image_uuid:
 	    image_uuid = image_uuid
 	else:
@@ -77,7 +77,7 @@ def create_windows_vm(vm_creation_option=None, volume_uuids=None, root_disk_uuid
         image_uuid = res_ops.query_resource(
             res_ops.IMAGE, cond, session_uuid)[0].uuid
         l3net_uuid = test_lib.lib_get_l3_by_name(
-            os.environ.get('l3VlanNetwork3')).uuid
+            os.environ.get('l3VlanNetworkName3')).uuid
         vm_creation_option = test_util.VmOption()
         vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
         vm_creation_option.set_image_uuid(image_uuid)
