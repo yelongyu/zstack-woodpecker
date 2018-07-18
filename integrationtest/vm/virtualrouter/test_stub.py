299c270268b4a2ac37e0442ee260b4e645d4ad0a
Lijin Xiong <legion.xiong@gmail.com>
2018-07-18 11:27:18 +0800

    Increase the timeout of creating windows vm

--- integrationtest/vm/virtualrouter/test_stub.py
+++ integrationtest/vm/virtualrouter/test_stub.py
@@ -115,7 +115,7 @@ def create_windows_vm_2(l3_name=None, disk_offering_uuids=None, session_uuid = N
         l3_name = os.environ.get('l3VlanNetworkName1')
 
     l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
-    return create_vm([l3_net_uuid], image_uuid, 'windows_vm', disk_offering_uuids, instance_offering_uuid = instance_offering_uuid, session_uuid = session_uuid)
+    return create_vm([l3_net_uuid], image_uuid, 'windows_vm', disk_offering_uuids, instance_offering_uuid = instance_offering_uuid, session_uuid = session_uuid, timeout=1200000)
 
 def create_other_vm(l3_name=None, disk_offering_uuids=None, session_uuid = None):
     '''
@@ -180,7 +180,7 @@ def create_vm_with_user_args(system_tags = None, session_uuid = None):
 # parameter: vmname; l3_net: l3_net_description, or [l3_net_uuid,]; image_uuid:
 def create_vm(l3_uuid_list, image_uuid, vm_name = None, \
         disk_offering_uuids = None, default_l3_uuid = None, \
-        system_tags = None, instance_offering_uuid = None, session_uuid = None, ps_uuid=None):
+        system_tags = None, instance_offering_uuid = None, session_uuid = None, ps_uuid=None, timeout=None):
     vm_creation_option = test_util.VmOption()
     conditions = res_ops.gen_query_conditions('type', '=', 'UserVm')
     if not instance_offering_uuid:
@@ -194,6 +194,8 @@ def create_vm(l3_uuid_list, image_uuid, vm_name = None, \
     vm_creation_option.set_system_tags(system_tags)
     vm_creation_option.set_session_uuid(session_uuid)
     vm_creation_option.set_ps_uuid(ps_uuid)
+    if timeout:
+        vm_creation_option.set_timeout(timeout)
     vm = zstack_vm_header.ZstackTestVm()
     vm.set_creation_option(vm_creation_option)
     vm.create()
