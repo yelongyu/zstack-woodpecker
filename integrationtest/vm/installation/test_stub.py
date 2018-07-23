36ff34c1b236242d0d6e16018a917edb13f459d9
Ye Tian <ye.tian@zstack.io>
2018-07-23 13:21:16 +0800

    fix the upgrade c74 iso repo

--- integrationtest/vm/installation/test_stub.py
+++ integrationtest/vm/installation/test_stub.py
@@ -269,6 +269,23 @@ def update_iso(vm_ip, tmp_file, iso_path, upgrade_script_path):
     else:
        test_util.test_logger('update the iso success')
 
+def update_c74_iso(vm_ip, tmp_file, c74_iso_path, upgrade_script_path):
+    ssh_cmd = 'ssh -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm_ip
+    vm_username = os.environ['imageUsername']
+    vm_password = os.environ['imagePassword']
+    cmd = '%s "rm -f /opt/zstack.iso"' % ssh_cmd
+    process_result = execute_shell_in_process(cmd, tmp_file)
+    ssh.scp_file(iso_path, '/opt/zstack-c74.iso', vm_ip, vm_username, vm_password)
+    ssh.scp_file(upgrade_script_path, '/opt/zstack-upgrade', vm_ip, vm_username, vm_password)
+    cmd = '%s "mkdir -p /opt/zstack-dvd"' % ssh_cmd
+    process_result = execute_shell_in_process(cmd, tmp_file)
+    cmd = '%s "bash /opt/zstack-upgrade -r /opt/zstack-c74.iso"' % ssh_cmd
+    process_result = execute_shell_in_process(cmd, tmp_file)
+    if process_result != 0:
+         test_util.test_fail('zstack upgrade iso failed')
+    else:
+       test_util.test_logger('update the iso success')
+
 def upgrade_by_iso(vm_ip, tmp_file, iso_path, upgrade_script_path):
     ssh_cmd = 'ssh -oStrictHostKeyChecking=no -oCheckHostIP=no -oUserKnownHostsFile=/dev/null %s' % vm_ip
     vm_username = os.environ['imageUsername']
