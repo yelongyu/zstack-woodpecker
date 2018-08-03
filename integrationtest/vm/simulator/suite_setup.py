33ef1c79c89ec8880b28e867681ab2cd9aad4ab3
quarkonics <quarkonics@gmail.com>
2018-08-03 15:50:24 +0800

    Enable test rest server for simulator

--- integrationtest/vm/simulator/suite_setup.py
+++ integrationtest/vm/simulator/suite_setup.py
@@ -4,6 +4,7 @@
 '''
 
 import os
+import subprocess
 import zstackwoodpecker.operations.scenario_operations as scenario_operations
 import zstackwoodpecker.operations.deploy_operations as deploy_operations
 import zstackwoodpecker.operations.config_operations as config_operations
@@ -13,6 +14,9 @@ import zstackwoodpecker.test_util as test_util
 USER_PATH = os.path.expanduser('~')
 EXTRA_SUITE_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_suite_setup_config.sh' % USER_PATH
 def test():
+    os.system('pkill -f ./test_rest_server.py')
+    process = subprocess.Popen("./test_rest_server.py", cwd=os.environ.get('woodpecker_root_path')+'/dailytest/', universal_newlines=True)
+    test_util.test_pass('Suite Setup Success')
     if test_lib.scenario_config != None and test_lib.scenario_file != None and not os.path.exists(test_lib.scenario_file):
         scenario_operations.deploy_scenario(test_lib.all_scenario_config, test_lib.scenario_file, test_lib.deploy_config)
         test_util.test_skip('Suite Setup Success')
