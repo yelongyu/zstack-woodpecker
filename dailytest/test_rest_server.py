26a21eb4ff112c12a5006914ef6eaa593ad1cd35
quarkonics <quarkonics@gmail.com>
2018-08-03 15:33:50 +0800

    Fix test_rest_server.py

--- dailytest/test_rest_server.py
+++ dailytest/test_rest_server.py
@@ -81,8 +81,8 @@ def not_found(error):
 
 def run_server():
         app.config['TEMPLATES_AUTO_RELOAD'] = False
-	app.run(host="0.0.0.0", port="8888", debug=True)
+	app.run(host="0.0.0.0", port=8888, debug=True)
 
 if __name__ == '__main__':
         app.config['TEMPLATES_AUTO_RELOAD'] = False
-	app.run(host="0.0.0.0", port="8888", debug=True)
+	app.run(host="0.0.0.0", port=8888, debug=True)
