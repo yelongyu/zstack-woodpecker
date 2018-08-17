6da21d1110ba95486500ffe21207272642584e81
glody <glody.guo@gmail.com>
2018-08-17 17:01:43 +0800

    update case baremetal test_chassis.py

--- integrationtest/vm/baremetal/test_chassis.py
+++ integrationtest/vm/baremetal/test_chassis.py
@@ -17,7 +17,8 @@ def test():
     global vm
     mn_ip = res_ops.query_resource(res_ops.MANAGEMENT_NODE)[0].hostName
     cond = res_ops.gen_query_conditions('managementIp', '=', mn_ip) 
-    host_uuid = res_ops.query_resource(res_ops.HOST, cond)[0].uuid
+    host = res_ops.query_resource(res_ops.HOST, cond)[0]
+    host_uuid = host.uuid
     instance_offering_uuid = res_ops.query_resource(res_ops.INSTANCE_OFFERING)[0].uuid
     cond = res_ops.gen_query_conditions('name', '=', 'medium-image')
     image_uuid = res_ops.query_resource(res_ops.IMAGE, cond)[0].uuid
@@ -36,22 +37,27 @@ def test():
     vm.set_creation_option(vm_creation_option)
     vm.create()
 
-    # Create VM
-    #vm = test_stub.create_vm()
-    #vm.check()
-    # Create Virtual BMC
-    test_stub.create_vbmc(vm = vm, port = 6230)
+    cond = res_ops.gen_query_conditions('hypervisorType', '=', 'baremetal')
+    baremetal_cluster_uuid = res_ops.query_resource(res_ops.CLUSTER, cond)[0].uuid
+    test_stub.create_vbmc(vm, host.managementIp, 6230)
     # Create Chassis
-    chassis = os.environ.get('ipminame')
-    test_stub.create_chassis(chassis_name = chassis)
-    test_stub.hack_ks(port = 6230)
-    chassis_uuid = test_lib.lib_get_chassis_by_name(chassis).uuid
+    chassis_option = test_util.ChassisOption()
+    chassis_option.set_name(os.environ.get('ipminame'))
+    chassis_option.set_ipmi_address('127.0.0.1')
+    chassis_option.set_ipmi_username(os.environ.get('ipmiusername'))
+    chassis_option.set_ipmi_password(os.environ.get('ipmipassword'))
+    chassis_option.set_ipmi_port('6230')
+    chassis_option.set_cluster_uuid(baremetal_cluster_uuid)
+    chassis = baremetal_operations.create_chassis(chassis_option)
+
+    #test_stub.hack_ks(port = 6230)
+    chassis_uuid = chassis.uuid 
     # First time Provision
-    baremetal_operations.provision_baremetal(chassis_uuid)
-    hwinfo = test_stub.check_hwinfo(chassis_uuid)
-    if not hwinfo:
-        test_util.test_fail('Fail to get hardware info during the first provision')
-    test_stub.delete_vbmc(vm = vm)
+    baremetal_operations.inspect_chassis(chassis_uuid)
+    #hwinfo = test_stub.check_hwinfo(chassis_uuid)
+    #if not hwinfo:
+    #    test_util.test_fail('Fail to get hardware info during the first provision')
+    test_stub.delete_vbmc(vm, host.managementIp)
     baremetal_operations.delete_chassis(chassis_uuid)
     vm.destroy()
     test_util.test_pass('Create chassis Test Success')
