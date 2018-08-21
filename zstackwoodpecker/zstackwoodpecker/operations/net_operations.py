69c6c4efdd66a9ae6476478492c26ba0b2d3cafb
glody <glody.guo@gmail.com>
2018-08-21 14:49:56 +0800

    update attach l2 function in net_ops to support systemtags

--- zstackwoodpecker/zstackwoodpecker/operations/net_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/net_operations.py
@@ -342,10 +342,11 @@ def delete_l3(l3_uuid, session_uuid = None):
     test_util.action_logger("[L3:] %s is deleted" % l3_uuid)
     return evt
 
-def attach_l2(l2_uuid, cluster_uuid, session_uuid = None):
+def attach_l2(l2_uuid, cluster_uuid, systemTags = None, session_uuid = None):
     action = api_actions.AttachL2NetworkToClusterAction()
     action.clusterUuid = cluster_uuid
     action.l2NetworkUuid = l2_uuid
+    action.systemTags = systemTags
     evt = acc_ops.execute_action_with_session(action, session_uuid)
     test_util.action_logger("Attach [L2:] %s to [Cluster:] %s " \
             % (l2_uuid, cluster_uuid))
