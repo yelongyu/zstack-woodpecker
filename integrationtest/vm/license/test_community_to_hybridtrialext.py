a6211ab26afb870a4063ac2b4752089557a3c9bb
Ye Tian <ye.tian@zstack.io>
2018-10-05 21:18:30 +0800

    add test_community_to_hybridtrialext.py

---
+++ integrationtest/vm/license/test_community_to_hybridtrialext.py
@@ -0,0 +1,33 @@
+'''
+
+New Integration Test for License.
+
+@author: ye.tian 20181005
+'''
+
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.operations.license_operations as lic_ops
+import time
+import datetime
+import os
+
+test_stub = test_lib.lib_get_test_stub()
+
+def test():
+    test_stub.reload_default_license()
+    test_util.test_logger('Check default community license')
+    test_stub.check_license(None, None, 2147483647, False, 'Community')
+
+    test_util.test_logger('Load and Check TrialExt license with 5 day and 5 HOST')
+    file_path = test_stub.gen_license('woodpecker', 'woodpecker@zstack.io', '5', 'HybridTrialExt', '', '5')
+    test_stub.load_license(file_path)
+    test_stub.check_license("woodpecker@zstack.io", None, 5, False, 'HybridTrialExt')
+
+
+    test_util.test_pass('Check License Test Success')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    test_lib.lib_error_cleanup(test_obj_dict)
