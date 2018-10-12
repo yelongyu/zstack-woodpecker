2a030f69935d8e1058066b85a575cf66ea972230
Ye Tian <ye.tian@zstack.io>
2018-10-12 11:41:06 +0800

    fix community add addons license

--- integrationtest/vm/license/test_addons_license.py
+++ integrationtest/vm/license/test_addons_license.py
@@ -1,6 +1,6 @@
 '''
 
-New Integration Test for License addons baremetal.
+New Integration Test for License addons project-management.
 
 @author: yetian
 '''
@@ -22,8 +22,15 @@ def test():
     test_util.test_logger('Check default community license')
     test_stub.check_license(None, None, 2147483647, False, 'Community')
 
-    test_util.test_logger('load and check addon license baremetal with 2 day ')
-    file_path = test_stub.gen_addons_license('woodpecker', 'woodpecker@zstack.io', '2', 'Addon', '', '', 'baremetal')
+    test_util.test_logger('Load and Check Prepaid license with 1 day and 1 CPU')
+    file_path = test_stub.gen_license('woodpecker', 'woodpecker@zstack.io', '1', 'Prepaid', '1', '')
+    test_stub.load_license(file_path)
+    issued_date = test_stub.get_license_info().issuedDate
+    expired_date = test_stub.license_date_cal(issued_date, 86400 * 1)
+    test_stub.check_license("woodpecker@zstack.io", 1, None, False, 'Paid', issued_date=issued_date, expired_date=expired_date)
+
+    test_util.test_logger('load and check addon license project-management with 2 day ')
+    file_path = test_stub.gen_addons_license('woodpecker', 'woodpecker@zstack.io', '2', 'Addon', '', '', 'project-management')
     node_uuid = res_ops.query_resource(res_ops.MANAGEMENT_NODE)[0].uuid
     test_stub.update_license(node_uuid, file_path)
     issued_date = test_stub.get_license_addons_info().issuedDate
@@ -42,8 +49,8 @@ def test():
     expired_date = test_stub.license_date_cal(issued_date, 86400 * 2)
     test_stub.check_license("woodpecker@zstack.io", None, 1, False, 'Hybrid', issued_date=issued_date, expired_date=expired_date)
 
-    test_util.test_logger('load and check addon license baremetal with 2 day ')
-    file_path = test_stub.gen_addons_license('woodpecker', 'woodpecker@zstack.io', '2', 'Addon', '', '', 'baremetal')
+    test_util.test_logger('load and check addon license project-management with 2 day ')
+    file_path = test_stub.gen_addons_license('woodpecker', 'woodpecker@zstack.io', '2', 'Addon', '', '', 'project-management')
     with open(file_path.strip('\n'), 'r') as file_license2:
         file_license1 = file_license2.read()
         file_license = base64.b64encode('%s' % file_license1)
@@ -85,7 +92,7 @@ def test():
     lic_ops.delete_license(node_uuid, uuid)
     test_util.test_logger('delete the license [uuid:] %s' % uuid)
 
-    test_util.test_pass('Check License for baremetal Test Success')
+    test_util.test_pass('Check License for project-management Test Success')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
