e81d9fb5e13c4de203445661b4c19f5e4c3871a6
Antony WeiJiang <wei.jiang@zstack.io>
2018-11-28 10:51:22 +0800

    add load balance to suite_setup.py
     Signed-by-off: Antony WeiJiang <wei.jiang@zstack.io>

--- integrationtest/vm/autoscaling/suite_setup.py
+++ integrationtest/vm/autoscaling/suite_setup.py
@@ -18,6 +18,7 @@ import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.operations.config_operations as conf_ops
+import zstackwoodpecker.zstack_test.zstack_test_load_balancer as zstack_lb_header
 
 USER_PATH = os.path.expanduser('~')
 EXTRA_SUITE_SETUP_SCRIPT = '%s/.zstackwoodpecker/extra_suite_setup_config.sh' % USER_PATH
@@ -76,6 +77,21 @@ def test():
     vm1 = test_stub.create_vlan_vm(os.environ.get('l3NoVlanNetworkName2'))
     test_obj_dict.add_vm(vm1)
     vm1.destroy()
+
+    test_util.test_dsc("create load balance")
+    l3_public_name = os.environ.get('l3PublicNetworkName')
+    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_public_name).uuid
+
+    vip = test_stub.create_vip('vip_for_lb_test', l3_net_uuid)
+    test_obj_dict.add_vip(vip)
+
+    lb = zstack_lb_header.ZstackTestLoadBalancer()
+    lb.create('autoscaling lb test', vip.get_vip().uuid)
+    test_obj_dict.add_load_balancer(lb)
+
+    lb_creation_option = test_lib.lib_create_lb_listener_option('check vm http healthy','tcp',22,80)
+    lbl = lb.create_listener(lb_creation_option)
+
 
     delete_policy = test_lib.lib_set_delete_policy('vm', 'Direct')
     delete_policy = test_lib.lib_set_delete_policy('volume', 'Direct')
