e484c33df947be81f558b29f6f3513800956b865
glody <glody.guo@gmail.com>
2018-12-20 15:07:20 +0800

    Update pxe server state according new api

--- integrationtest/vm/baremetal/test_pxe_server.py
+++ integrationtest/vm/baremetal/test_pxe_server.py
@@ -18,7 +18,7 @@ def test():
     if pxe_servers != None:
         for pxe in pxe_servers:
             baremetal_ops.delete_pxe(pxe.uuid)
- 
+
     test_util.test_dsc('Create pxe server and stop/start it')
     pxe_uuid = test_stub.create_pxe(zoneUuid = zone_uuid).uuid
     #baremetal_operations.attach_pxe_to_cluster(pxe_uuid, baremetal_cluster_uuid)
@@ -28,12 +28,12 @@ def test():
         test_util.test_fail('Create PXE Server Failed')
     baremetal_ops.stop_pxe(pxe_uuid)
     pxe = res_ops.query_resource(res_ops.PXE_SERVER)[0]
-    if pxe.status != "Stopped":
-        test_util.test_fail('Stop PXE Server Failed, PXE status: %s' %pxe.status)
+    if pxe.state != "Disabled":
+        test_util.test_fail('Disable PXE Server Failed, PXE state: %s' %pxe.status)
     baremetal_ops.start_pxe(pxe_uuid)
     pxe = res_ops.query_resource(res_ops.PXE_SERVER)[0]
-    if pxe.status != "Running":
-        test_util.test_fail('Start PXE Server Failed, PXE status: %s' %pxe.status)
+    if pxe.state != "Enabled":
+        test_util.test_fail('Enable PXE Server Failed, PXE state: %s' %pxe.status)
 
     baremetal_ops.delete_pxe(pxe_uuid)
     test_util.test_pass('Test PXE Server Success')
