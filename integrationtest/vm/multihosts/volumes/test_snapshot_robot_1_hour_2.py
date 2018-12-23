3674b6d768bab8a5e86909e8839792b720895b64
quarkonics <quarkonics@gmail.com>
2018-12-23 22:51:55 +0800

    Use flavor for multihosts/volumes/test_snapshot_robot_1_hour_2.py

--- integrationtest/vm/multihosts/volumes/test_snapshot_robot_1_hour_2.py
+++ integrationtest/vm/multihosts/volumes/test_snapshot_robot_1_hour_2.py
@@ -23,8 +23,14 @@ _config_ = {
 
 test_dict = test_state.TestStateDict()
 TestAction = ts_header.TestAction
+
+case_flavor = dict(create_stop_start_attach_detach_destory_expunge_vm=           dict(path_list=[TestAction.create_vm, TestAction.stop_vm, TestAction.start_vm, TestAction.attach_volume, TestAction.detach_volume, TestAction.destroy_vm, TestAction.expunge_vm]),
+                   create_reboot_attach_detach_destory_expunge_vm=               dict(path_list=[TestAction.create_vm, TestAction.reboot_vm, TestAction.attach_volume, TestAction.detach_volume, TestAction.destroy_vm, TestAction.expunge_vm]),
+                  )
     
 def test():
+    flavor = case_flavor[os.environ.get('CASE_FLAVOR')]
+    path_list = flavor['path_list']
     test_util.test_dsc('''Will mainly doing random test for all kinds of snapshot operations. VM, Volume and Image operations will also be tested. If reach 1 hour successful running condition, testing will success and quit.  SG actions, and VIP actions are removed in this robot test.
         VM resources: a special Utility vm is required to do volume attach/detach operation. 
         ''')
@@ -61,7 +67,7 @@ def test():
     robot_test_obj.set_public_l3(public_l3)
     robot_test_obj.set_utility_vm(utility_vm)
     robot_test_obj.set_random_type(action_select.resource_path_strategy)
-    path_list = [TestAction.create_vm, TestAction.stop_vm, TestAction.start_vm, TestAction.attach_volume, TestAction.detach_volume, TestAction.destroy_vm, TestAction.expunge_vm]
+    #path_list = [TestAction.create_vm, TestAction.stop_vm, TestAction.start_vm, TestAction.attach_volume, TestAction.detach_volume, TestAction.destroy_vm, TestAction.expunge_vm]
     robot_test_obj.set_required_path_list(path_list)
 
     rounds = 1
