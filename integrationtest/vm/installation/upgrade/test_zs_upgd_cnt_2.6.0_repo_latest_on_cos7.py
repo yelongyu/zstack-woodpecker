907d283598f99d5e32e9db705fef17f2182a2eb2
Ye Tian <ye.tian@zstack.io>
2019-01-02 19:46:18 +0800

    update the cnt 2.6.0 add the 3.1.3 3.2.0

--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_2.6.0_repo_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_2.6.0_repo_latest_on_cos7.py
@@ -46,7 +46,7 @@ def test():
     #test_stub.update_iso(vm_ip, tmp_file, iso_path, upgrade_script_path)
 
     #pkg_num = 1.9
-    release_ver=['3.0.0','3.1.0','3.1.1']
+    release_ver=['3.0.0','3.1.0','3.1.1','3.1.3','3.2.0']
     curren_num = float(os.environ.get('releasePkgNum'))
     for pkg_num in release_ver:
     #while pkg_num <= curren_num:
