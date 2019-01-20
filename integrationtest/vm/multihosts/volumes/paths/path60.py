8480879e8119597c1bf2f77377c30698190f18b5
quarkonics <quarkonics@gmail.com>
2019-01-20 09:37:50 +0800

    Add multihosts/volumes/paths/path60.py

---
+++ integrationtest/vm/multihosts/volumes/paths/path60.py
@@ -0,0 +1,4 @@
+import zstackwoodpecker.test_state as ts_header
+TestAction = ts_header.TestAction
+def path():
+    return dict(initial_formation="template2", path_list=[[TestAction.create_data_volume_from_image, "volume1", "=scsi"], [TestAction.attach_volume, "vm1", "volume1"], [TestAction.create_data_volume_from_image, "volume2", "=scsi"], [TestAction.attach_volume, "vm1", "volume2"], [TestAction.create_data_volume_from_image, "volume3", "=scsi"], [TestAction.attach_volume, "vm1", "volume3"], [TestAction.delete_volume, "volume1"], [TestAction.create_volume_snapshot, "vm1-root", "snapshot1"], [TestAction.create_volume_snapshot, "volume2", "snapshot2"], [TestAction.delete_volume, "volume3"], [TestAction.reboot_vm, "vm1"], [TestAction.stop_vm, "vm1"], [TestAction.use_volume_snapshot, "snapshot2"], [TestAction.attach_volume, "vm1", "volume2"], [TestAction.use_volume_snapshot, "snapshot1"], [TestAction.use_volume_snapshot, "snapshot2"], [TestAction.reboot_vm, "vm1"]])
