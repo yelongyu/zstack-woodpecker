efa9cad204f2b9f677818e05131ba90819d588e8
quarkonics <quarkonics@gmail.com>
2019-01-21 20:11:25 +0800

    Add multihosts/volumes/paths/path70.py

---
+++ integrationtest/vm/multihosts/volumes/paths/path70.py
@@ -0,0 +1,4 @@
+import zstackwoodpecker.test_state as ts_header
+TestAction = ts_header.TestAction
+def path():
+    return dict(initial_formation="template2", path_list=[[TestAction.create_volume, "volume1", "=scsi,shareable"], [TestAction.create_volume, "volume2", "=scsi,shareable"], [TestAction.detach_volume, "volume1"], [TestAction.migrate_vm, "vm1"], [TestAction.start_vm, "vm1"], [TestAction.create_vm_backup, "vm1", "backup1"], [TestAction.ps_migrate_volume, "volume1"], [TestAction.attach_volume, "vm1", "volume1"], [TestAction.stop_vm, "vm1"], [TestAction.ps_migrate_volume, "vm1-root"], [TestAction.create_data_vol_template_from_volume, "volume1", "image1"], [TestAction.delete_volume, "volume1"], [TestAction.reboot_vm, "vm1"], [TestAction.stop_vm, "vm1"], [TestAction.use_vm_backup, "backup1"], [TestAction.start_vm, "vm1"], [TestAction.reboot_vm, "vm1"]])
