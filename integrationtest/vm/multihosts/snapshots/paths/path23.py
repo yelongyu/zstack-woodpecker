da1df216c2871095ed9ccf4ffa78a52a720af5ea
zsyzsyhao <zsyzsyhao@foxmail.com>
2019-01-23 20:18:45 +0800

    snapshot tree run path test

---
+++ integrationtest/vm/multihosts/snapshots/paths/path23.py
@@ -0,0 +1,19 @@
+import zstackwoodpecker.test_state as ts_header
+TestAction = ts_header.TestAction
+def path():
+
+    return dict(initial_formation="template1", \
+           path_list=[[TestAction.stop_vm, "vm1"], \
+                      [TestAction.reinit_vm, "vm1"], \
+                      [TestAction.start_vm, "vm1"] \
+                      [TestAction.stop_vm, "vm1"], \
+                      [TestAction.reinit_vm, "vm1"], \
+                      [TestAction.start_vm, "vm1"] \
+                      [TestAction.create_volume_snapshot, "vm1-root", "snapshot1"], \
+                      [TestAction.stop_vm, "vm1"], \
+                      [TestAction.start_vm, "vm1"], \
+                      [TestAction.create_volume_snapshot, "vm1-root", "snapshot2"], \
+                      [TestAction.stop_vm, "vm1"], \
+                      [TestAction.use_volume_snapshot, "snapshot2"], \
+                      [TestAction.start_vm, "vm1"] \
+                     ])
