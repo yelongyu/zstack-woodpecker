5482dbad0be475dce1c9f52c57afd7751e366cd7
zsyzsyhao <zsyzsyhao@foxmail.com>
2019-01-24 15:17:51 +0800

    add delete_volume_snapshot in constant path state machine

--- zstackwoodpecker/zstackwoodpecker/test_lib.py
+++ zstackwoodpecker/zstackwoodpecker/test_lib.py
@@ -5829,6 +5829,42 @@ def lib_robot_constant_path_operation(robot_test_obj):
     
             test_util.test_dsc('Robot Action Result: %s; new SP: %s' % \
                 (next_action, new_snapshot.get_snapshot().uuid))
+        elif next_action == TestAction.delete_volume_snapshot:
+            target_volume_snapshots = None
+            target_snapshot = None
+            target_snapshot_name = None
+            if len(constant_path_list[0]) > 1:
+                target_snapshot_name = constant_path_list[0][1]
+           
+                all_volume_snapshots = test_dict.get_all_available_snapshots()
+                for candidate_snapshots in all_volume_snapshots:
+                    for candidate_snapshot in candidate_snapshots.get_primary_snapshots():
+                        if candidate_snapshot.get_snapshot().name == target_snapshot_name:
+                            target_volume_snapshots = candidate_snapshots
+                            target_snapshot = candidate_snapshot
+                            break
+
+            if not target_snapshot:
+                test_util.test_fail("no resource available for next action: %s" % (next_action))
+
+            target_volume_snapshots.delete_snapshot(target_snapshot)
+            test_util.test_dsc('Robot Action: %s; on Volume: %s; on SP: %s' % \
+                (next_action, \
+                target_volume_snapshots.get_target_volume().get_volume().uuid, \
+                target_snapshot.get_snapshot().uuid))
+
+            robot_test_obj.add_resource_action_history(target_snapshot.get_snapshot().uuid, next_action)
+            robot_test_obj.add_resource_action_history(target_volume_snapshots.get_target_volume().get_volume().uuid, next_action)
+            #If both volume and snapshots are deleted, volume_snapshot obj could be 
+            # removed.
+            if not target_volume_snapshots.get_backuped_snapshots():
+                target_volume_obj = target_volume_snapshots.get_target_volume()
+                if target_volume_obj.get_state() == vol_header.EXPUNGED \
+                        or (target_volume_snapshots.get_volume_type() == \
+                            vol_header.ROOT_VOLUME \
+                            and target_volume_obj.get_target_vm().get_state() == \
+                                vm_header.EXPUNGED):
+                    test_dict.rm_volume_snapshot(target_volume_snapshots)
         elif next_action == TestAction.use_volume_snapshot:
             target_volume_snapshots = None
             target_snapshot = None
