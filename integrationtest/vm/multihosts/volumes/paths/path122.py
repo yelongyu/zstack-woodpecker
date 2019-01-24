de47afecd28efb4568aa08e6779b6f2093bf388d
Antony WeiJiang <wei.jiang@zstack.io>
2019-01-24 22:29:36 +0800

    [robot]repair path123.py [robot]add path122.py case [robot]add path121.py case
    Signed-off-by:Antony WeiJiang <wei.jiang@zstack.io>

--- integrationtest/vm/multihosts/volumes/paths/path122.py
+++ integrationtest/vm/multihosts/volumes/paths/path122.py
@@ -2,7 +2,7 @@ import zstackwoodpecker.test_state as ts_header
 TestAction = ts_header.TestAction
 def path():
         return dict(initial_formation="template2",\
-                path_list=[[TestAction.create_volume, "volume1"], \
+		path_list=[[TestAction.create_volume, "volume1"], \
                 [TestAction.attach_volume, "vm1", "volume1"], \
                 [TestAction.create_volume, "volume2"], \
                 [TestAction.attach_volume, "vm1", "volume2"], \
@@ -18,7 +18,7 @@ def path():
                 [TestAction.attach_volume, "vm1", "volume7"], \
                 [TestAction.create_volume, "volume8"], \
                 [TestAction.attach_volume, "vm1", "volume8"], \
-                [TestAction.detach_volume, "volume1"], \
+		[TestAction.detach_volume, "volume1"], \
                 [TestAction.detach_volume, "volume2"], \
                 [TestAction.detach_volume, "volume3"], \
                 [TestAction.detach_volume, "volume4"], \
@@ -26,23 +26,21 @@ def path():
                 [TestAction.detach_volume, "volume6"], \
                 [TestAction.detach_volume, "volume7"], \
                 [TestAction.detach_volume, "volume8"], \
-		[TestAction.stop_vm, "vm1"], \
-                [TestAction.reinit_vm, "vm1"], \
-		[TestAction.start_vm, "vm1"], \
-		[TestAction.create_volume, "volume9"], \
-		[TestAction.create_data_vol_template_from_volume, "volume9", "image1"],\
-		[TestAction.attach_volume, "vm1", "volume9"], \
-		[TestAction.detach_volume, "volume9"], \
 		[TestAction.clone_vm, "vm1", "vm2", "=full"], \
-		[TestAction.stop_vm, "vm2"], \
-		[TestAction.ps_migrate_volume, "vm2-root"], \
+		[TestAction.create_volume, "volume9"], \
+		[TestAction.attach_volume, "vm1", "volume9"], \
+		[TestAction.create_volume_backup, "volume9", "backup1"], \
+		[TestAction.create_volume_snapshot, "vm1-root", 'snapshot1'], \
+		[TestAction.stop_vm, "vm1"], \
+		[TestAction.use_volume_snapshot, "snapshot1"], \
+		[TestAction.start_vm, "vm1"], \
+		[TestAction.detach_volume, "volume9"], \
+		[TestAction.ps_migrate_volume, "volume9"], \
 		[TestAction.create_volume, "volume10"], \
 		[TestAction.attach_volume, "vm1", "volume10"], \
-		[TestAction.create_volume_backup, "volume10", "backup1"], \
-		[TestAction.detach_volume, "volume10"], \
-		[TestAction.cleanup_ps_cache], \
-		[TestAction.attach_volume, "vm1", "volume10"], \
+		[TestAction.create_volume_backup, "volume10", "backup2"], \
 		[TestAction.stop_vm, "vm1"], \
-		[TestAction.use_volume_backup, "backup1"], \
+		[TestAction.use_volume_backup, "backup2"], \
 		[TestAction.start_vm, "vm1"], \
                 [TestAction.reboot_vm, "vm1"]])
+
