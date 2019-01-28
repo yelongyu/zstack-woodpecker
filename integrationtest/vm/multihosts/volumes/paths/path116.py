3618c592c09d74d9f5dce47421b56cd5797027e8
Antony WeiJiang <wei.jiang@zstack.io>
2019-01-28 17:29:26 +0800

    [robot]repair path116.py case
    Signed-off-by:Antony WeiJiang <wei.jiang@zstack.io>

--- integrationtest/vm/multihosts/volumes/paths/path116.py
+++ integrationtest/vm/multihosts/volumes/paths/path116.py
@@ -2,7 +2,7 @@ import zstackwoodpecker.test_state as ts_header
 TestAction = ts_header.TestAction
 def path():
     return dict(initial_formation="template2",\
-                path_list=[[TestAction.create_volume, "volume1", "=scsi,shareable"], \		
+                path_list=[[TestAction.create_volume, "volume1", "=scsi"], \
 		[TestAction.attach_volume, "vm1", "volume1"], \
 		[TestAction.reboot_vm, "vm1"], \
 		[TestAction.create_volume_backup, "volume1", "backup1"], \
@@ -15,7 +15,8 @@ def path():
 		[TestAction.stop_vm, "vm1"], \
 		[TestAction.change_vm_image, "vm1"], \
 		[TestAction.start_vm, "vm1"], \
-		[TestAction.create_volume, "volume3", "=scsi,shareable"], \
+		[TestAction.create_volume, "volume3", "=scsi"], \
+		[TestAction.create_volume, "volume4", "=scsi,shareable"], \
 		[TestAction.attach_volume, "vm1", "volume3"], \
 		[TestAction.create_volume_backup, "volume3", "backup2"], \
 		[TestAction.stop_vm, "vm1"], \
