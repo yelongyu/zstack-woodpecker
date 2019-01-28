4edae23d4cffc2d72fcb67136ff81b346525a448
Lijin Xiong <legion.xiong@gmail.com>
2019-01-28 19:21:06 +0800

    skip image trash cleanup test for none-ceph bs

--- integrationtest/vm/multiclusters/data_migration/test_cleanup_single_image_trash.py
+++ integrationtest/vm/multiclusters/data_migration/test_cleanup_single_image_trash.py
@@ -8,12 +8,20 @@ New Integration Test for migrate between clusters
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_state as test_state
 import zstackwoodpecker.test_lib as test_lib
+import apibinding.inventory as inventory
+import zstackwoodpecker.operations.resource_operations as res_ops
 
 test_obj_dict = test_state.TestStateDict()
 test_stub = test_lib.lib_get_test_stub()
 data_migration = test_stub.DataMigration()
 
 def test():
+    bs_list = res_ops.query_resource(res_ops.BACKUP_STORAGE)
+    for bs in bs_list:
+        if bs.type == inventory.CEPH_BACKUP_STORAGE_TYPE:
+            break
+    else:
+        test_util.test_logger('BS is type %s, skip.' % bs.type)
     data_migration.migrate_image()
     data_migration.check_origin_image_exist()
     data_migration.clean_up_single_image_trash()
