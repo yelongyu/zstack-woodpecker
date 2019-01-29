0d3536330129fe0acde3bf0fd427bc01fa1aa8ca
Antony WeiJiang <wei.jiang@zstack.io>
2019-01-29 11:23:54 +0800

    [robot]add path108.py case [robot]add path109.py case
     Signed-off-by:Antony WeiJiang <wei.jiang@zstack.io>

---
+++ integrationtest/vm/multihosts/volumes/paths/path108.py
@@ -0,0 +1,24 @@
+import zstackwoodpecker.test_state as ts_header
+TestAction = ts_header.TestAction
+def path():
+    return dict(initial_formation="template2", \
+                path_list=[[TestAction.create_data_volume_from_image, "volume1"],\
+		[TestAction.delete_volume, "volume1"], \
+		[TestAction.stop_vm, "vm1"], \
+		[TestAction.reinit_vm, "vm1"], \
+		[TestAction.start_vm, "vm1"], \
+		[TestAction.create_volume, "volume2"], \
+		[TestAction.attach_volume, "vm1", "volume2"], \
+		[TestAction.create_volume_backup, "volume2", "backup1"], \
+		[TestAction.create_volume, "volume3"], \
+		[TestAction.delete_volume, "volume3"], \
+		[TestAction.create_volume_snapshot, "vm1-root", 'snapshot1'], \
+		[TestAction.resize_data_volume, "volume2", 5*1024*1024], \
+		[TestAction.detach_volume, "volume2"], \
+		[TestAction.attach_volume, "vm1", "volume2"], \
+		[TestAction.detach_volume, "volume2"], \
+		[TestAction.stop_vm, "vm1"], \
+		[TestAction.use_volume_snapshot, "snapshot1"], \
+		[TestAction.use_volume_backup, "backup1"], \
+		[TestAction.start_vm, "vm1"], \
+		[TestAction.reboot_vm, "vm1"]])	
