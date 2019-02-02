924e2dd2dc4a729ecf27f203fcdd4ad95c4a3438
quarkonics <quarkonics@gmail.com>
2019-02-02 16:48:27 +0800

    Fix multihosts/volumes/paths/path78.py

--- integrationtest/vm/multihosts/volumes/paths/path78.py
+++ integrationtest/vm/multihosts/volumes/paths/path78.py
@@ -19,13 +19,13 @@ def path():
                            [TestAction.attach_volume, "vm1", "volume7"],
                            [TestAction.attach_volume, "vm1", "volume8"],
                            [TestAction.detach_volume, "volume1", "vm1"],
-                           [TestAction.clone_vm, "vm1", "vm2", "=full"],
+                           [TestAction.clone_vm, "vm1", "vm2"],
                            [TestAction.create_data_vol_template_from_volume, "volume1", "image1"],
                            [TestAction.delete_volume, "volume1"],
-                           [TestAction.clone_vm, "vm1", "vm3", "=full"],
+                           [TestAction.clone_vm, "vm1", "vm3"],
                            [TestAction.resize_volume, "vm1", 5*1024*1024],
                            [TestAction.detach_volume, "volume2", "vm1"],
-                           [TestAction.clone_vm, "vm1", "vm4", "=full"],
+                           [TestAction.clone_vm, "vm1", "vm4"],
                            [TestAction.resize_volume, "vm1", 5*1024*1024],
                            [TestAction.reboot_vm, "vm1"]
                           ]
