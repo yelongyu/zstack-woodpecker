ef3312234a5c00940572b259635e8450f0e9affd
Pengtao Zhang <pengtao.zhang@zstack.io>
2019-02-19 17:59:05 +0800

    update get_fail_case.sh
    Signed-off-by: Pengtao Zhang <pengtao.zhang@zstack.io>

--- tools/get_fail_case.sh
+++ tools/get_fail_case.sh
@@ -7,7 +7,7 @@ nfs_dir=/nfs/dir-1/disk1/mirror
 root_dir=${nfs_dir}/${ci_target}/${buildid}
 cd $root_dir
 find . | grep errlogs/ > fail_case.log
-echo "{" > reproduce_case.py
+echo "cast_job_dict = {" > reproduce_case.py
 for i in `cat fail_case.log`
 do
 	case_name=`basename $i | awk -F- '{print $NF}' | awk -F"_id" '{print $1}'`
@@ -18,10 +18,11 @@ do
 	else
 		testsuite=$ts
 	fi
+	if [ $testsuite == "virtual_router" ];then
+		testsuite=`echo $testsuite | sed 's/_//g'`
+	fi
 	sub_dir=`basename $i`
 	sub_dir2=`echo $sub_dir | awk -F- '{print $1}' | awk -F0 '{print $NF}' | cut -b 2-`
-	#sub_dir1=`echo ${sub_dir#*0_}`
-	#sub_dir2=`echo ${sub_dir1%-*}`
 	echo "sub_dir2 is : $sub_dir2"
 	case_dir=`dirname $i`
 	config_dir="$case_dir/../../../../"
@@ -32,12 +33,20 @@ do
 	echo " config_dir is :$config_dir"
 	echo " config_file is :$config_file"
 	echo " sceniro_file is :$sceniro_file"
-	if [ ! $sub_dir2 ];then
-		echo "'$case_name' : 'TestSuiteJob(\"$ci_target\", \"nightly\", \"qwer\", \"$testsuite\", \"$config_file\", \"2\", reproduce_case=\"${testsuite}/${case_name}.py\", buildid=\"$buildid\", woodpecker=\"latest\", user=\"pengtao.zhang\",scenarioconfig=\"$sceniro_file\")'," >> reproduce_case.py
+	if [ $sceniro_file ];then
+		if [ ! $sub_dir2 ];then
+			echo "'$case_name' : '    TestSuiteJob(\'$ci_target\', \'nightly\', \'asdf\', \'$testsuite\', \'$config_file\', 2, reproduce_case=\'${testsuite}/${case_name}.py\', buildid=\'$buildid\', woodpecker=\'latest\', user=\'pengtao.zhang\', scenarioconfig=\'$sceniro_file\')'," >> reproduce_case.py
+		else
+			echo "'$case_name' : '    TestSuiteJob(\'$ci_target\', \'nightly\', \'asdf\', \'$testsuite\', \'$config_file\', 2, reproduce_case=\'${testsuite}/${sub_dir2}/${case_name}.py\', buildid=\'$buildid\', woodpecker=\'latest\', user=\'pengtao.zhang\', scenarioconfig=\'$sceniro_file\')'," >> reproduce_case.py
+		fi
 	else
-		echo "'$case_name' : 'TestSuiteJob(\"$ci_target\", \"nightly\", \"qwer\", \"$testsuite\", \"$config_file\", \"2\", reproduce_case=\"${testsuite}/${sub_dir2}/${case_name}.py\", buildid=\"$buildid\", woodpecker=\"latest\", user=\"pengtao.zhang\",scenarioconfig=\"$sceniro_file\")'," >> reproduce_case.py
+		if [ ! $sub_dir2 ];then
+			echo "'$case_name' : '    TestSuiteJob(\'$ci_target\', \'nightly\', \'asdf\', \'$testsuite\', \'$config_file\', 2, reproduce_case=\'${testsuite}/${case_name}.py\', buildid=\'$buildid\', woodpecker=\'latest\', user=\'pengtao.zhang\')'," >> reproduce_case.py
+		else
+			echo "'$case_name' : '    TestSuiteJob(\'$ci_target\', \'nightly\', \'asdf\', \'$testsuite\', \'$config_file\', 2, reproduce_case=\'${testsuite}/${sub_dir2}/${case_name}.py\', buildid=\'$buildid\', woodpecker=\'latest\', user=\'pengtao.zhang\')'," >> reproduce_case.py
+		fi
 	fi
 done
 echo "}" >> reproduce_case.py
 
-sshpass -p password scp $root_dir/reproduce_case.py root@172.20.198.87:/home/jenkins/test_script
+sshpass -p password scp $root_dir/reproduce_case.py root@172.20.198.87:/home/jenkins/jenkins_script
