81e11f09e234a6a8caa977b327772924de97e08c
moyu <w13880213920@live.com>
2019-02-26 10:35:09 +0800

    add checker time

--- zstackwoodpecker/zstackwoodpecker/header/checker.py
+++ zstackwoodpecker/zstackwoodpecker/header/checker.py
@@ -5,7 +5,7 @@ Zstack checker header classes
 @author: YYK                   
 '''
 import zstackwoodpecker.test_util as test_util
-
+import time
 class TestChecker(object):
     def __init__(self):
         import os
@@ -13,6 +13,8 @@ class TestChecker(object):
         self.real_result = None
         self.retry_count = 5
         self.test_obj = None
+        self.start = None
+        self.end = None
         retry_env = os.environ.get('WOODPECKER_TEST_FAILURE_RETRY')
         if retry_env and retry_evn.isdigit():
             self.retry_count = int(retry_env)
@@ -36,33 +38,36 @@ class TestChecker(object):
 
         The inheritance class, should not add more params to this function.
         '''
+        self.start = time.time()
         test_util.test_logger('Checker: [%s] begins.'% self.__class__.__name__)
 
     def judge(self, result):
+        self.end = time.time()
+        dlt = self.end - self.start
+
         self.real_result = result
         if self.exp_result == result:
             test_util.test_logger('\
-Checker: [%s] PASS. Expected result: %s. Test result: %s.' \
-                % (self.__class__.__name__, self.exp_result, self.real_result))
+                    Checker: [%s] PASS. Expected result: %s. Test result: %s. Spent: %.5f s.' \
+                % (self.__class__.__name__, self.exp_result, self.real_result, dlt))
 
             return True
 
         else:
             if self.retry_count:
                 test_util.test_logger('\
-Checker: [%s] FAIL. Expected result: %s. Test result: %s. Try again. The left \
+                        Checker: [%s] FAIL. Expected result: %s. Test result: %s. Spent: %.5f s. Try again. The left \
 retry times: %s' % (self.__class__.__name__, self.exp_result, \
-                        self.real_result, str(self.retry_count)))
+                        self.real_result, dlt, str(self.retry_count)))
                 self.retry_count -= 1
-                import time
                 time.sleep(1)
                 self.test_obj.update()
                 self.check()
             else:
                 test_util.test_fail('\
-Checker: [%s] FAIL. Expected result: %s. Test result: %s. No Retry' \
+                        Checker: [%s] FAIL. Expected result: %s. Test result: %s. Spent: %.5f s. No Retry' \
                     % (self.__class__.__name__, \
-                        self.exp_result, self.real_result))
+                        self.exp_result, self.real_result, dlt))
 
 class CheckerFactory(object):
     def create_checker(self):
