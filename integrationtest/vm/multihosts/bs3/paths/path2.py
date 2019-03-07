3fd1356fd609507243e4422f17f55c7f81787637
zsyzsyhao <zsyzsyhao@foxmail.com>
2019-03-07 16:32:10 +0800

    add path1 and path2

--- integrationtest/vm/multihosts/bs3/paths/path2.py
+++ integrationtest/vm/multihosts/bs3/paths/path2.py
@@ -3,7 +3,7 @@ TestAction = ts_header.TestAction
 def path():
     return dict(initial_formation="template1", \
            path_list=[
-                      [TestAction.add_image, "sftp", "image1", "qcow2", "LOCAL"], \
+                      [TestAction.add_image, "sftp", "image1", "raw", "SFTP"], \
                       [TestAction.create_vm_by_image, "image1", "vm1"], \
                       [TestAction.stop_vm, "vm1"], \
                       [TestAction.start_vm, "vm1"]
