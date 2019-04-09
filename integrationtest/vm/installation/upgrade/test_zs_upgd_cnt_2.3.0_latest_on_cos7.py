d2ee1bd0fa768c598c129f63abe7f66d4d0a2286
Ye Tian <ye.tian@zstack.io>
2019-04-09 15:55:44 +0800

    fix the 230 iso

--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_2.3.0_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_2.3.0_latest_on_cos7.py
@@ -1,6 +1,7 @@
 '''
 
-@author: Yetian update(1) 2018-11-26
+@author: MengLai
+Updater: YeTian 2019-03-04  use the local iso and bin upgrade the latest version by iso
 '''
 import os
 import tempfile
@@ -10,6 +11,8 @@ import time
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
 import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.volume_operations as vol_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
 import zstacklib.utils.ssh as ssh
 
 test_stub = test_lib.lib_get_test_stub()
@@ -22,68 +25,106 @@ def test():
     global vm_inv
     test_util.test_dsc('Create test vm to test zstack upgrade by -u.')
     image_name = os.environ.get('imageNameBase_230_mn')
+    data_image_name = os.environ.get('data_volume_template')
+
+    conditions = res_ops.gen_query_conditions('name', '=', data_image_name)
+    data_image_uuid = res_ops.query_resource(res_ops.IMAGE, conditions)[0].uuid
     iso_path = os.environ.get('iso_path')
-    iso_232_path = os.environ.get('iso_232_path')
-    iso_240_path = os.environ.get('iso_240_path')
-    iso_250_path = os.environ.get('iso_250_path')
-    iso_260_path = os.environ.get('iso_260_path')
-    iso_301_path = os.environ.get('iso_301_path')
-    iso_310_path = os.environ.get('iso_310_path')
+
     zstack_latest_version = os.environ.get('zstackLatestVersion')
     zstack_latest_path = os.environ.get('zstackLatestInstaller')
     vm_name = os.environ.get('vmName')
     upgrade_script_path = os.environ.get('upgradeScript')
-
+    #host_name = 'hpe-sh27-ls'
+    #vm_inv = test_stub.create_vm_scenario(image_name, vm_name, host_name)
     vm_inv = test_stub.create_vm_scenario(image_name, vm_name)
+    vm_uuid = vm_inv.uuid
+    ps_uuid = vm_inv.allVolumes[0].primaryStorageUuid
+    print ps_uuid
+    host_uuid = vm_inv.hostUuid
+    print host_uuid
+    print data_image_uuid
     vm_ip = vm_inv.vmNics[0].ip
     test_lib.lib_wait_target_up(vm_ip, 22)
 
     test_stub.make_ssh_no_password(vm_ip, tmp_file)
+    test_util.test_dsc('create data volume from template') 
+    data_volume_name='Test_installation_data_volume_for_nightly'
+    data_volume_inv = vol_ops.create_volume_from_template(data_image_uuid, ps_uuid, data_volume_name, host_uuid)
+    data_volume_uuid = data_volume_inv.uuid
 
+    #test_util.test_dsc('query data volume') 
+    #data_volume_name = 'Test_installation_data_volume_for_nightly'
+    #conditions = res_ops.gen_query_conditions('name', '=', data_volume_name)
+    #data_volume_uuid = res_ops.query_resource(res_ops.VOLUME, conditions)[0].uuid
+
+    test_util.test_dsc('attach the data volume to vm') 
+    vol_ops.attach_volume(data_volume_uuid, vm_uuid)
+    
+    test_util.test_dsc('mount the disk in vm') 
+    mount_point = '/testpath'
+    test_stub.mount_volume(vm_ip, mount_point, tmp_file)
+
+    iso_232_path = '%s/iso/zstack_230.iso' % mount_point
+    iso_240_path = '%s/iso/zstack_240.iso' % mount_point
+    iso_250_path = '%s/iso/zstack_250.iso' % mount_point
+    iso_260_path = '%s/iso/zstack_260.iso' % mount_point
+    iso_301_path = '%s/iso/zstack_301.iso' % mount_point
+    iso_310_path = '%s/iso/zstack_310.iso' % mount_point
+    iso_320_path = '%s/iso/zstack_320.iso' % mount_point
+    iso_330_path = '%s/iso/zstack_330.iso' % mount_point
 
     test_util.test_dsc('Update MN IP')
     test_stub.update_mn_hostname(vm_ip, tmp_file)
     test_stub.update_mn_ip(vm_ip, tmp_file)
-    #test_stub.reset_rabbitmq(vm_ip, tmp_file)
+    test_stub.reset_rabbitmq(vm_ip, tmp_file)
     test_stub.start_mn(vm_ip, tmp_file)
-    test_stub.check_installation(vm_ip, tmp_file)
+    test_stub.stop_mn(vm_ip, tmp_file)
+    #test_stub.check_installation(vm_ip, tmp_file)
 
-    #test_stub.update_iso(vm_ip, tmp_file, iso_path, upgrade_script_path)
+    test_stub.update_local_iso(vm_ip, tmp_file, iso_19_path, upgrade_script_path)
 
-    #pkg_num = 1.9
-    release_ver=['2.3.1','2.3.2','2.4.0','2.5.0','2.6.0','3.0.0','3.0.1','3.1.1']
+    release_ver=['2.3.1','2.3.2','2.4.0','2.5.0','2.6.0','3.0.0','3.0.1','3.1.0','3.1.3','3.2.0','3.3.0']
     curren_num = float(os.environ.get('releasePkgNum'))
     for pkg_num in release_ver:
-    #while pkg_num <= curren_num:
 	if str(pkg_num) == '2.3.2':
-		test_stub.update_iso(vm_ip, tmp_file, iso_232_path, upgrade_script_path)
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_232_path, upgrade_script_path)
 	if str(pkg_num) == '2.4.0':
-		test_stub.update_iso(vm_ip, tmp_file, iso_240_path, upgrade_script_path)
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_240_path, upgrade_script_path)
 	if str(pkg_num) == '2.5.0':
-		test_stub.update_iso(vm_ip, tmp_file, iso_250_path, upgrade_script_path)
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_250_path, upgrade_script_path)
 	if str(pkg_num) == '2.6.0':
-		test_stub.update_iso(vm_ip, tmp_file, iso_260_path, upgrade_script_path)
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_260_path, upgrade_script_path)
 	if str(pkg_num) == '3.0.1':
-		test_stub.update_iso(vm_ip, tmp_file, iso_301_path, upgrade_script_path)
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_301_path, upgrade_script_path)
 	if str(pkg_num) == '3.1.0':
-		test_stub.update_iso(vm_ip, tmp_file, iso_310_path, upgrade_script_path)
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_310_path, upgrade_script_path)
+	if str(pkg_num) == '3.2.0':
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_320_path, upgrade_script_path)
+	if str(pkg_num) == '3.3.0':
+		test_stub.update_local_iso(vm_ip, tmp_file, iso_330_path, upgrade_script_path)
         test_util.test_logger('Upgrade zstack to %s' % pkg_num)
-        upgrade_pkg = os.environ.get('zstackPkg_%s' % pkg_num)
-        test_stub.upgrade_zstack(vm_ip, upgrade_pkg, tmp_file) 
+        upgrade_pkg = '%s/installation-package/zstack-installer-%s.bin' % (mount_point, pkg_num)
+        test_stub.upgrade_old_zstack(vm_ip, upgrade_pkg, tmp_file) 
+        test_stub.start_node(vm_ip, tmp_file)
         test_stub.start_mn(vm_ip, tmp_file)
         test_stub.check_zstack_version(vm_ip, tmp_file, str(pkg_num))
+        test_stub.stop_mn(vm_ip, tmp_file)
 
     test_util.test_dsc('Upgrade zstack to latest') 
+
     test_stub.update_iso(vm_ip, tmp_file, iso_path, upgrade_script_path)
     test_stub.upgrade_zstack(vm_ip, zstack_latest_path, tmp_file) 
+    test_stub.start_node(vm_ip, tmp_file)
     test_stub.start_mn(vm_ip, tmp_file)
     test_stub.check_mn_running(vm_ip, tmp_file)
     test_stub.check_zstack_version(vm_ip, tmp_file, zstack_latest_version)
     test_stub.check_installation(vm_ip, tmp_file)
 
     os.system('rm -f %s' % tmp_file)
+    vol_ops.detach_volume(data_volume_inv.uuid)
     test_stub.destroy_vm_scenario(vm_inv.uuid)
-    test_util.test_pass('ZStack upgrade Test Success')
+    test_util.test_pass('''ZStack upgrade '2.3.1','2.3.2','2.4.0','2.5.0','2.6.0','3.0.0','3.0.1','3.1.0','3.1.3','3.2.0','3.3.0'Test Success''')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
