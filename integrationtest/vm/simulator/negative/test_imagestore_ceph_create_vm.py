e8d7a0c84b172acd14cddeb1eef6429d221ca849
quarkonics <quarkonics@gmail.com>
2019-04-15 19:31:24 +0800

    Update simulator/negative/test_imagestore_ceph_create_vm.py

--- integrationtest/vm/simulator/negative/test_imagestore_ceph_create_vm.py
+++ integrationtest/vm/simulator/negative/test_imagestore_ceph_create_vm.py
@@ -26,10 +26,7 @@ CHECK_BITS = "/ceph/primarystorage/snapshot/checkbits"
 CREATE_SNAPSHOT = "/ceph/primarystorage/snapshot/create"
 PROTECT_SNAPSHOT = "/ceph/primarystorage/snapshot/protect"
 VOLUME_CLONE = "/ceph/primarystorage/volume/clone"
-FLAT_DHCP_PREPARE = "/flatnetworkprovider/dhcp/prepare"
-FLAT_DHCP_APPLY = "/flatnetworkprovider/dhcp/apply"
 VM_START = "/vm/start"
-FLAT_DHCP_RELEASE = "/flatnetworkprovider/dhcp/release"
 CEPH_DELETE = "/ceph/primarystorage/delete"
 
 _config_ = {
@@ -50,14 +47,11 @@ case_flavor = dict(normal=             dict(agent_url=None),
                    create_snapshot=    dict(agent_url=CREATE_SNAPSHOT),
                    protect_snapshot=   dict(agent_url=PROTECT_SNAPSHOT),
                    volume_clone=       dict(agent_url=VOLUME_CLONE),
-                   dhcp_prepare=       dict(agent_url=FLAT_DHCP_PREPARE),
-                   dhcp_apply=         dict(agent_url=FLAT_DHCP_APPLY),
                    vm_start=           dict(agent_url=VM_START),
-                   dhcp_release=       dict(agent_url=FLAT_DHCP_RELEASE),
                    ceph_delete=        dict(agent_url=CEPH_DELETE),
                    )
 
-db_tables_white_list = ['VmInstanceSequenceNumberVO', 'TaskProgressVO', 'RootVolumeUsageVO', 'ImageCacheVO', 'SecurityGroupFailureHostVO']
+db_tables_white_list = ['VmInstanceSequenceNumberVO', 'TaskProgressVO', 'RootVolumeUsageVO', 'ImageCacheVO', 'SecurityGroupFailureHostVO', 'ResourceVO', 'AccountResourceRefVO']
 
 def test():
     global agent_url
@@ -100,14 +94,13 @@ def test():
         deploy_operations.remove_simulator_agent_script(agent_url)
         deploy_operations.deploy_simulator_agent_script(agent_url, script)
 
-    if agent_url == FLAT_DHCP_RELEASE or agent_url == CEPH_DELETE:
+    if agent_url == CEPH_DELETE:
         agent_url2 = VOLUME_CLONE
         deploy_operations.remove_simulator_agent_script(agent_url2)
         deploy_operations.deploy_simulator_agent_script(agent_url2, script)
 
     saved_db_stats = test_stub.get_db_stats(dhcp_ip)
-    test_stub.print_table('VolumeEO')
-    test_stub.print_table('ResourceVO')
+    test_stub.print_table('AccountResourceRefVO')
     create_vm_failure = False
     try:
         vm = test_stub.create_vm(image_uuid=image_uuid, ps_uuid=ps_uuid)
@@ -117,8 +110,6 @@ def test():
     if agent_url != None and not create_vm_failure:
         test_util.test_fail("Expect failure during creating VM while it passed. Test Exception handling for Create VM FAIL")
 
-    test_stub.print_table('VolumeEO')
-    test_stub.print_table('ResourceVO')
     if agent_url != None:
         if is_flat:
             try:
@@ -128,6 +119,7 @@ def test():
         else:
             dhcp_ip = None
         saved_db_stats2 = test_stub.get_db_stats(dhcp_ip)
+        test_stub.print_table('AccountResourceRefVO')
         test_stub.compare_db_stats(saved_db_stats, saved_db_stats2, db_tables_white_list)
 
 def env_recover():
