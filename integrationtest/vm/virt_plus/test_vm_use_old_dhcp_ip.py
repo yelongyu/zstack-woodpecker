ce5f61ec3a5c9642775f4b33bab0edb3eb9c054f
hengguo.ge <hengguo.ge@zstack.io>
2019-04-18 18:48:47 +0800

    update case test_vm_use_old_dhcp_ip.py

--- integrationtest/vm/virt_plus/test_vm_use_old_dhcp_ip.py
+++ integrationtest/vm/virt_plus/test_vm_use_old_dhcp_ip.py
@@ -20,23 +20,25 @@ test_stub = test_lib.lib_get_test_stub()
 test_obj_dict = test_state.TestStateDict()
 
 public_ip="223.5.5.5"
+ext_host_ip="172.20.1.106"
+ext_host_pwd="zstack.org"
 
 def test():
+    global test_obj_dict
     cond_l3 = res_ops.gen_query_conditions('name', '=', os.environ.get('l3PublicNetworkName'))
     l3_uuid = res_ops.query_resource(res_ops.L3_NETWORK, cond_l3)[0].uuid
     ip_range_uuid = res_ops.query_resource(res_ops.L3_NETWORK, cond_l3)[0].ipRanges[0].uuid
     vm = test_stub.create_vm()
-    test_obj_dict.add_vm(vm)
 
     #Get dhcp ip
     dhcp_ip = net_ops.get_l3network_dhcp_ip(l3NetworkUuid=l3_uuid)
     sys_tag = ["staticIp::%s::%s" % (l3_uuid, dhcp_ip)]
     vm.destroy()
     vm.expunge()
-
+    time.sleep(10)
     ip_range=test_lib.lib_get_ip_range_by_l3_uuid(l3NetworkUuid=l3_uuid)
     net_ops.delete_ip_range(ip_range_uuid)
-
+    time.sleep(2)
     ip_range_option = test_util.IpRangeOption()
     ip_range_option.set_l3_uuid(l3_uuid=l3_uuid)
     ip_range_option.set_startIp(startIp=ip_range.startIp)
@@ -45,18 +47,23 @@ def test():
     ip_range_option.set_netmask(netmask=ip_range.netmask)
     ip_range_option.set_name(name=ip_range.name)
     net_ops.add_ip_range(ip_range_option=ip_range_option)
+    time.sleep(2)
     vm = test_stub.create_vm(vm_name='vm-dhcp-ip', system_tags=sys_tag)
     vm.check()
+    test_obj_dict.add_vm(vm)
+    time.sleep(60)
+
     vm_ip = vm.vm.vmNics[0].ip
-    cmd = "ping -c 4 %s" % (public_ip)
-    (retcode, output, erroutput) = ssh.execute(cmd, vm_ip, "root", "password", True, 22)
+    #test_lib.lib_wait_target_up(ext_host_ip, 22, timeout=10)
+    cmd = "ping -c 5 %s" % (vm_ip)
+    (retcode, output, erroutput) = ssh.execute(cmd, ext_host_ip, "root", ext_host_pwd, True, 22)
     print "retcode is: %s; output is : %s.; erroutput is: %s" % (retcode, output, erroutput)
+    vm.destroy()
+    vm.expunge()
     if retcode != 0:
         test_util.test_fail('Cannot ping public IP, test Failed.')
 
-    vm.destroy()
-    vm.expunge()
-
 #Will be called only if exception happens in test().
 def error_cleanup():
+    global test_obj_dict
     test_lib.lib_error_cleanup(test_obj_dict)
