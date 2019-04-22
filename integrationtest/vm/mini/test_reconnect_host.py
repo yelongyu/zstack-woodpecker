0d79b842721356d500461eb57d0df72e7ac2def7
glody <glody.guo@gmail.com>
2019-04-22 20:19:16 +0800

    migrate reconnect host case to mini

---
+++ integrationtest/vm/mini/test_reconnect_host.py
@@ -0,0 +1,96 @@
+'''
+This case can not execute parallelly
+@author: Youyk
+'''
+import os
+import zstackwoodpecker.test_util as test_util
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_state as test_state
+import zstackwoodpecker.operations.host_operations as host_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+import time
+
+_config_ = {
+        'timeout' : 1000,
+        'noparallel' : True
+        }
+
+test_stub = test_lib.lib_get_test_stub()
+test_obj_dict = test_state.TestStateDict()
+
+def compare_capacity(res1, res2, cpu_free = 0, memory_free = 0):
+    if res1.availableCpu != res2.availableCpu - cpu_free:
+        test_util.test_logger('available CPU are different. res1: %s, res2: %s\
+                ' % (res1.availableCpu, res2.availableCpu))
+        return False
+
+    if res1.totalCpu != res2.totalCpu:
+        test_util.test_logger('total CPU are different. res1: %s, res2: %s\
+                ' % (res1.totalCpu, res2.totalCpu))
+        return False
+
+    if res1.availableMemory != res2.availableMemory - memory_free:
+        test_util.test_logger('available Memory are different. res1: %s, res2: %s\
+                ' % (res1.availableMemory, res2.availableMemory))
+        return False
+
+    if res1.totalMemory != res2.totalMemory:
+        test_util.test_logger('available Memory are different. res1: %s, res2: %s\
+                ' % (res1.totalMemory, res2.totalMemory))
+        return False
+
+    return True
+
+def test():
+    test_util.test_dsc('Test Host Reconnect function and check if the available CPU and memory number are aligned between before and after reconnect action')
+    vm = test_stub.create_basic_vm()
+    test_obj_dict.add_vm(vm)
+
+    zone_uuid = vm.get_vm().zoneUuid
+    host = test_lib.lib_get_vm_host(vm.get_vm())
+    host_uuid = host.uuid
+
+    tot_res1 = test_lib.lib_get_cpu_memory_capacity([zone_uuid])
+    
+    host_ops.reconnect_host(host_uuid)
+    time.sleep(5)
+
+    tot_res2 = test_lib.lib_get_cpu_memory_capacity([zone_uuid])
+
+    if compare_capacity( tot_res1, tot_res2):
+        test_util.test_logger("the resource consumption are same after reconnect host")
+    else:
+        test_util.test_fail("the resource consumption are different after reconnect host: %s " % host_uuid)
+
+    vm_offering_uuid = vm.get_vm().instanceOfferingUuid
+    cond = res_ops.gen_query_conditions('uuid', '=', vm_offering_uuid)
+    vm_offering = res_ops.query_resource(res_ops.INSTANCE_OFFERING, cond)[0]
+    vm_cpu = vm_offering.cpuNum
+    vm_memory = vm_offering.memorySize
+    
+    vm.destroy()
+    test_obj_dict.rm_vm(vm)
+
+    tot_res3 = test_lib.lib_get_cpu_memory_capacity([zone_uuid])
+    if compare_capacity(tot_res1, tot_res3, vm_cpu, vm_memory):
+        test_util.test_logger("the resource consumption are aligned after destroy a vm")
+    else:
+        test_util.test_fail("the resource consumption are not aligned after destroy vm: %s on host: %s" % (vm.get_vm().uuid, host_uuid))
+
+    vm = test_stub.create_basic_vm()
+    test_obj_dict.add_vm(vm)
+
+    tot_res4 = test_lib.lib_get_cpu_memory_capacity([zone_uuid])
+
+    if compare_capacity(tot_res1, tot_res4):
+        test_util.test_logger("the resource consumption are aligned after create a new vm")
+    else:
+        test_util.test_fail("the resource consumption are not aligned after create a new vm: %s " % vm.get_vm().uuid)
+
+    vm.destroy()
+
+    test_util.test_pass('Reconnect Host and Test CPU/Memory Capacity Pass')
+
+#Will be called only if exception happens in test().
+def error_cleanup():
+    test_lib.lib_error_cleanup(test_obj_dict)
---
+++ integrationtest/vm/mini/test_stub.py
@@ -0,0 +1,235 @@
+'''
+test_stub to share test operations
+@author: SyZhao
+'''
+import os
+import time
+import zstacklib.utils.ssh as ssh
+import zstackwoodpecker.test_lib as test_lib
+import zstackwoodpecker.test_util as test_util
+import zstacklib.utils.shell as shell
+import zstacklib.utils.xmlobject as xmlobject
+import zstackwoodpecker.operations.scenario_operations as sce_ops
+import zstackwoodpecker.operations.deploy_operations as dpy_ops
+import zstackwoodpecker.operations.resource_operations as res_ops
+import zstackwoodpecker.zstack_test.zstack_test_vm as test_vm_header
+import zstackwoodpecker.operations.ha_operations as ha_ops
+import zstackwoodpecker.operations.vm_operations as vm_ops
+import zstackwoodpecker.operations.backupstorage_operations as bs_ops
+import zstackwoodpecker.operations.primarystorage_operations as ps_ops
+import zstackwoodpecker.operations.host_operations as host_ops
+
+
+def exec_zsha2_version(host_ip, username, password):
+    cmd = "zsha2 version"
+    version_info = test_lib.lib_execute_ssh_cmd(host_ip, username, password, cmd)
+    test_util.test_logger("current zsha2 version is %s" %(version_info))
+
+
+def exec_zsha2_demote(host_ip, username, password):
+    cmd = "zsha2 demote"
+    test_lib.lib_execute_ssh_cmd(host_ip, username, password, cmd)
+
+
+def exec_upgrade_mn(host_ip, username, password, zstack_bin_path):
+    cmd = "TERM=xterm zsha2 upgrade-mn -yes -peerpass password " + zstack_bin_path
+    if not test_lib.lib_execute_ssh_cmd(host_ip, username, password, cmd, 1800):
+        test_util.test_fail('%s failed' %(cmd))
+
+
+def exec_upgrade_iso(host_ip, username, password, zstack_iso_path):
+    cmd = "TERM=xterm zsha2 upgrade-mn -yes -peerpass password " + zstack_iso_path
+    if not test_lib.lib_execute_ssh_cmd(host_ip, username, password, cmd, 1800):
+        test_util.test_fail('%s failed' %(cmd))
+
+
+def exec_upgrade_zsha2(host_ip, username, password, zsha2_path):
+    cmd = "chmod a+x " + zsha2_path
+    test_lib.lib_execute_ssh_cmd(host_ip, username, password, cmd)
+    cmd = zsha2_path + " upgrade-ha"
+    test_lib.lib_execute_ssh_cmd(host_ip, username, password, cmd)
+
+
+
+def stop_host(host_vm, scenarioConfig, force=None):
+    host_vm_uuid = host_vm.uuid_
+    mn_ip = scenarioConfig.basicConfig.zstackManagementIp.text_
+    try:
+        host_inv = sce_ops.stop_vm(mn_ip, host_vm_uuid,force=force)
+        return host_inv
+    except:
+        test_util.test_logger("Fail to stop host [%s]" % host_vm.ip_)
+        return False
+
+def start_host(host_vm, scenarioConfig):
+    host_vm_uuid = host_vm.uuid_
+    mn_ip = scenarioConfig.basicConfig.zstackManagementIp.text_
+    try:
+        host_inv = sce_ops.start_vm(mn_ip, host_vm_uuid)
+        return host_inv
+    except:
+        test_util.test_logger("Fail to start host [%s]" % host_vm.ip_)
+        return False
+
+def query_host(host_ip, scenarioConfig):
+    mn_ip = scenarioConfig.basicConfig.zstackManagementIp.text_
+    try:
+        host_inv = sce_ops.get_vm_inv_by_vm_ip(mn_ip, host_ip)
+        return host_inv
+    except:
+        test_util.test_logger("Fail to query host [%s]" % host_ip)
+        return False
+
+def recover_host(host_vm, scenarioConfig, deploy_config):
+    stop_host(host_vm, scenarioConfig)
+    host_inv = start_host(host_vm, scenarioConfig)
+    if not host_inv:
+       return False
+    host_ip = host_vm.ip_
+    test_lib.lib_wait_target_up(host_ip, '22', 120)
+    host_config = sce_ops.get_scenario_config_vm(host_inv.name,scenarioConfig)
+    for l3network in xmlobject.safe_list(host_config.l3Networks.l3Network):
+        if hasattr(l3network, 'l2NetworkRef'):
+            for l2networkref in xmlobject.safe_list(l3network.l2NetworkRef):
+                nic_name = sce_ops.get_ref_l2_nic_name(l2networkref.text_, deploy_config)
+                if nic_name.find('.') >= 0 :
+                    vlan = nic_name.split('.')[1]
+                    test_util.test_logger('[vm:] %s %s is created.' % (host_ip, nic_name.replace("eth","zsn")))
+                    cmd = 'vconfig add %s %s' % (nic_name.split('.')[0].replace("eth","zsn"), vlan)
+                    test_lib.lib_execute_ssh_cmd(host_ip, host_config.imageUsername_, host_config.imagePassword_, cmd)
+    return True
+
+
+def recover_vlan_in_host(host_ip, scenarioConfig, deploy_config):
+    test_util.test_logger("func: recover_vlan_in_host; host_ip=%s" %(host_ip))
+
+    host_inv = query_host(host_ip, scenarioConfig)
+    test_lib.lib_wait_target_up(host_ip, '22', 120)
+    host_config = sce_ops.get_scenario_config_vm(host_inv.name,scenarioConfig)
+    for l3network in xmlobject.safe_list(host_config.l3Networks.l3Network):
+        test_util.test_logger("loop in for l3network")
+        if hasattr(l3network, 'l2NetworkRef'):
+            test_util.test_logger("below if l2NetworkRef")
+            for l2networkref in xmlobject.safe_list(l3network.l2NetworkRef):
+                test_util.test_logger("loop in l2networkref")
+                nic_name = sce_ops.get_ref_l2_nic_name(l2networkref.text_, deploy_config)
+                test_util.test_logger("nic_name=%s; l2networkref.text_=%s" %(nic_name, l2networkref.text_))
+                if nic_name.find('.') >= 0 :
+                    vlan = nic_name.split('.')[1]
+                    test_util.test_logger('[vm:] %s %s is created.' % (host_ip, nic_name.replace("eth","zsn")))
+                    cmd = 'vconfig add %s %s' % (nic_name.split('.')[0].replace("eth","zsn"), vlan)
+                    test_util.test_logger("vconfig cmd=%s" %(cmd))
+                    test_lib.lib_execute_ssh_cmd(host_ip, host_config.imageUsername_, host_config.imagePassword_, cmd)
+    return True
+
+def create_vm(l3_uuid_list, image_uuid, vm_name = None, \
+              disk_offering_uuids = None, default_l3_uuid = None, \
+              system_tags = None, instance_offering_uuid = None, session_uuid = None):
+    vm_creation_option = test_util.VmOption() 
+    conditions = res_ops.gen_query_conditions('type', '=', 'UserVm')
+    if not instance_offering_uuid:     
+        instance_offering_uuid = res_ops.query_resource(res_ops.INSTANCE_OFFERING, conditions)[0].uuid
+        vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
+    vm_creation_option.set_l3_uuids(l3_uuid_list)
+    vm_creation_option.set_image_uuid(image_uuid)
+    vm_creation_option.set_name(vm_name)
+    vm_creation_option.set_data_disk_uuids(disk_offering_uuids)
+    vm_creation_option.set_default_l3_uuid(default_l3_uuid)
+    vm_creation_option.set_system_tags(system_tags)
+    vm_creation_option.set_session_uuid(session_uuid)
+    vm = test_vm_header.ZstackTestVm()
+    vm.set_creation_option(vm_creation_option)
+    vm.create()
+    return vm
+
+def create_basic_vm(disk_offering_uuids=None, session_uuid = None, wait_vr_running = True):
+    image_name = os.environ.get('imageName_net')
+    image_uuid = test_lib.lib_get_image_by_name(image_name).uuid
+    l3_name = os.environ.get('l3VlanNetworkName1')
+    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
+
+    if wait_vr_running:
+        ensure_vr_is_running_connected(l3_net_uuid)
+
+    return create_vm([l3_net_uuid], image_uuid, 'basic_no_vlan_vm', disk_offering_uuids, session_uuid = session_uuid)
+
+def create_ha_vm(disk_offering_uuids=None, session_uuid = None):
+    vm = create_basic_vm(disk_offering_uuids=disk_offering_uuids, session_uuid=session_uuid)
+    ha_ops.set_vm_instance_ha_level(vm.get_vm().uuid, "NeverStop")
+    return vm
+
+def ensure_hosts_connected(exclude_host=[]):
+    for i in range(300):
+        #time.sleep(1)
+        host_list = res_ops.query_resource(res_ops.HOST)
+        for exh in exclude_host:
+            for host in host_list:
+                if exh.managementIp_ == host.managementIp or exh.ip_ == host.managementIp:
+                    host_list.remove(host)
+
+        for host in host_list:
+            try:
+                host_ops.reconnect_host(host.uuid)
+            except Exception, e:
+                test_util.test_logger("time: %s reconnect host failed: %s" %(str(i), host.uuid))
+                break
+            cond = res_ops.gen_query_conditions('uuid', '=', host.uuid)
+            hosts = res_ops.query_resource_fields(res_ops.HOST, cond, None)
+            if not "connected" in hosts[0].status.lower():
+                test_util.test_logger("time %s found not connected ps status: %s" %(str(i), hosts[0].status))
+                break
+        else:
+            return
+    else:
+        test_util.test_fail("host status didn't change to Connected within 300s, therefore, failed")
+
+def ensure_host_disconnected(test_host, wait_time):
+    cond = res_ops.gen_query_conditions('managementIp', '=', test_host.managementIp_)
+    for i in range(wait_time):
+        time.sleep(1)
+        host_list = res_ops.query_resource(res_ops.HOST, cond)
+        if "disconnected" in host_list[0].status.lower():
+            test_util.test_logger("successfully got host disconnected: %s" %(host_list[0].status))
+            return
+
+def restart_mn_node_with_long_timeout():
+
+    mn_ip = os.environ['zstackHaVip']
+
+    test_lib.lib_wait_target_up(mn_ip, '22', 120)
+
+    cmd = "zstack-ctl status|grep 'MN status'|awk '{print $3}'"
+    ret, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", False, 22)
+
+    if stdout.strip().strip('\n').lower() != "running":
+
+        check_mn_tool_path = "%s/%s" %(os.environ.get('woodpecker_root_path'), '/tools/check_mn_start.sh')
+        test_util.test_logger("check_mn_tool_path:[%s],mn_ip:[%s]" %(check_mn_tool_path, mn_ip))
+        ssh.scp_file(check_mn_tool_path, "/home/check_mn_start.sh", mn_ip, "root", "password")
+
+        cmd = "bash /home/check_mn_start.sh"
+        ret1, stdout1, stderr1 = ssh.execute(cmd, mn_ip, "root", "password", False, 22)
+        test_util.test_logger("check_mn_start.sh stdout1:[%s],stderr1:[%s]" %(stdout1,stderr1))
+
+        if str(ret1) == "0" :
+            cmd = "zstack-ctl stop"
+            ret, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", True, 22)
+            cmd = "zstack-ctl configure ThreadFacade.maxThreadNum=200"
+            ret, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", True, 22)
+            cmd = "zstack-ctl start_node --timeout 3000"
+            ret, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", True, 22)
+            cmd = "zstack-ctl start_ui"
+            ret, stdout, stderr = ssh.execute(cmd, mn_ip, "root", "password", True, 22)
+
+            #modify zstack start script
+            cmd = r'sed -i "s:zstack-ctl start:zstack-ctl start_node --timeout 3000:g" /etc/init.d/zstack-server'
+            test_lib.lib_execute_ssh_cmd(mn_ip, "root", "password", cmd)
+            time.sleep(1)
+            cmd = r'sed -i "/zstack-ctl start_node --timeout 3000/ a\    ZSTACK_HOME=\$zstack_app zstack-ctl start_ui" /etc/init.d/zstack-server'
+            test_lib.lib_execute_ssh_cmd(mn_ip, "root", "password", cmd)
+
+        else:
+            test_util.test_logger("find mn not self-started as expected, checked by /home/check_mn_start.sh")
+
+    else:
+        test_util.test_logger("find zstack MN is running.")
