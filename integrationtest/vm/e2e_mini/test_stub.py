463bf54fbc2ae43b2ca785ac7522d8bd4a26bb8a
ForatLuo <foratluo@gmail.com>
2019-04-29 16:32:00 +0800

    add e2e_mini/vms/test_create_volume

--- integrationtest/vm/e2e_mini/test_stub.py
+++ integrationtest/vm/e2e_mini/test_stub.py
@@ -39,10 +39,11 @@ class MINI(E2E):
         if elem.text != u'首页':
             test_util.test_logger(elem.err_msg)
             test_util.test_fail('Login failed! %s')
-    def create_vm(self, name=None, dsc=None, image=os.getenv('imageName_s'), cpu=2, mem='2 GB', data_size=None,user_data=None,
+    def create_vm(self, name=None, dsc=None, image=os.getenv('imageName_s'), cpu=2, mem='2 GB', data_size='2 GB', user_data=None,
                   network=os.getenv('l3PublicNetworkName'), cluster=os.getenv('clusterName'), provisioning=u'厚置备'):
         self.get_element('a[href="/web/vm"]').click()
         self.wait_for_element(REFRESHCSS)
+        time.sleep(3)
         self.click_button(u'创建云主机')
         self.vm_name = name if name else 'vm-' + POSTFIX
         vm_dict = {u'名称': self.vm_name,
@@ -57,7 +58,7 @@ class MINI(E2E):
                    u'置备类型': provisioning}
         for k, v in vm_dict.iteritems():
             if v:
-                if k == u'内存':
+                if k == u'内存' or k == u'数据盘容量':
                     val = v.split()
                     val.reverse()
                     self.input(k, val)
@@ -76,6 +77,42 @@ class MINI(E2E):
         assert attr_elems[1].text == vm_offering, "Excepted: %s, actual: %s" % (vm_offering, attr_elems[1].text)
         assert attr_elems[2].text == 'Linux', "Excepted: 'Linux', actual: %s" % attr_elems[2].text
         assert len(attr_elems[3].text.split('.')) == 4, "Actual vm ip is [%s]" % attr_elems[3].text
+    def create_volume(self, name=None, dsc=None, size='2 GB', cluster=os.getenv('clusterName'), vm=None, provisioning=u'厚置备'):
+        self.get_element('a[href="/web/volume"]').click()
+        self.wait_for_element(REFRESHCSS)
+        time.sleep(5)
+        self.click_button(u'创建数据盘')
+        self.volume_name = name if name else 'volume-' + POSTFIX
+        volume_dict = {
+            u'名称': self.volume_name,
+            u'简介': dsc,
+            u'容量': size,
+            u'一体机': cluster,
+            u'云主机' : vm,
+            u'置备类型': provisioning
+        }
+        for k, v in volume_dict.iteritems():
+            if v:
+                if k == u'容量':
+                    val = v.split()
+                    val.reverse()
+                    self.input(k, val)
+                else:
+                    self.input(k, v)
+        self.confirm()
+        self.wait_for_element(MESSAGETOAST, timeout=60, target='disappear')
+        for volume_elem in self.get_elements(CARDCONTAINER):
+            if self.volume_name in volume_elem.text:
+                break
+        else:
+            test_util.test_fail('Not found the volume [name: %s]' % self.volume_name)
+        attr_elems = volume_elem.get_elements('labelContainer___10VVH')
+        assert attr_elems[0].text == u'就绪', "Excepted: u'就绪', actual: %s" % attr_elems[0].text
+        if vm:
+            assert attr_elems[1].text == vm, "Excepted: %s, actual: %s" % (vm, attr_elems[1].text)
+        else:
+            assert attr_elems[1].text == u'未加载', "Excepted: u'未加载', actual: %s" % attr_elems[1].text
+        assert attr_elems[2].text == size, "Excepted: %s, actual: %s" % (size, attr_elems[2].text)
     def delete_vm(self, vm_name=None):
         self.get_element('a[href="/web/vm"]').click()
         self.wait_for_element(CARDCONTAINER)
@@ -93,4 +130,22 @@ class MINI(E2E):
         for vm_elem in self.get_elements(CARDCONTAINER):
             if vm_elem.text == vm_name:
                 test_util.test_fail('VM [%s] is still displayed!' % vm_name)
+    def delete_volume(self, volume_name=None):
+        self.get_element('a[href="/web/volume"]').click()
+        self.wait_for_element(CARDCONTAINER)
+        volume_name = volume_name if volume_name else self.volume_name
+        for volume_elem in self.get_elements(CARDCONTAINER):
+            if volume_name in volume_elem.text:
+                break;
+        else:
+            test_util.test_fail('Not found the volume [name: %s]' % volume_name)
+        volume_elem.get_element('input[type="checkbox"]').click()
+        self.click_button(u"更多操作")
+        self.operate(u'删除')
+        self.confirm()
+        self.wait_for_element(MESSAGETOAST, timeout=60, target='disappear')
+        for volume_elem in self.get_elements(CARDCONTAINER):
+            if vm_elem in self.get_elements(CARDCONTAINER):
+                if volume_elem.text == volume_name:
+                    test_util.test_fail('Volume [%s] is still displayed!' % volume_name)
 
