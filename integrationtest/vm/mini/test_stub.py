7dd8cd6f67187dfa8604cb368a1fea8fa48dfb83
zsyzsyhao <zsyzsyhao@foxmail.com>
2019-05-05 14:44:30 +0800

    fix create_vm in mini

--- integrationtest/vm/mini/test_stub.py
+++ integrationtest/vm/mini/test_stub.py
@@ -613,17 +613,26 @@ def create_basic_vm(disk_offering_uuids=None, session_uuid = None, wait_vr_runni
 
     return create_vm([l3_net_uuid], image_uuid, 'basic_no_vlan_vm', disk_offering_uuids, session_uuid = session_uuid)
 
-def create_vm(disk_offering_uuids=None, session_uuid = None, vm_name = None):
-    image_name = os.environ.get('imageName_net')
-    image_uuid = test_lib.lib_get_image_by_name(image_name).uuid
-    l3_name = os.environ.get('l3VlanNetworkName1')
-    l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
-    if not vm_name:
-        vm_name = "vm_for_test"
-    if wait_vr_running:
-        ensure_vr_is_running_connected(l3_net_uuid)
 
-    return create_vm([l3_net_uuid], image_uuid, vm_name, disk_offering_uuids, session_uuid = session_uuid)
+def create_vm(l3_uuid_list, image_uuid, vm_name = None, \
+              disk_offering_uuids = None, default_l3_uuid = None, \
+              system_tags = None, instance_offering_uuid = None, session_uuid = None):
+    vm_creation_option = test_util.VmOption() 
+    conditions = res_ops.gen_query_conditions('type', '=', 'UserVm')
+    if not instance_offering_uuid:     
+        instance_offering_uuid = res_ops.query_resource(res_ops.INSTANCE_OFFERING, conditions)[0].uuid
+        vm_creation_option.set_instance_offering_uuid(instance_offering_uuid)
+    vm_creation_option.set_l3_uuids(l3_uuid_list)
+    vm_creation_option.set_image_uuid(image_uuid)
+    vm_creation_option.set_name(vm_name)
+    vm_creation_option.set_data_disk_uuids(disk_offering_uuids)
+    vm_creation_option.set_default_l3_uuid(default_l3_uuid)
+    vm_creation_option.set_system_tags(system_tags)
+    vm_creation_option.set_session_uuid(session_uuid)
+    vm = test_vm_header.ZstackTestVm()
+    vm.set_creation_option(vm_creation_option)
+    vm.create()
+    return vm
 
 def create_ha_vm(disk_offering_uuids=None, session_uuid = None):
     vm = create_basic_vm(disk_offering_uuids=disk_offering_uuids, session_uuid=session_uuid)
