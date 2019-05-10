a3a747b5205103a8b266d880a7635297374ab61a
moyu <w13880213920@live.com>
2019-05-10 17:38:47 +0800

    add mini path6/7/8/9/11/12/13/14

---
+++ integrationtest/vm/mini/paths/path12.py
@@ -0,0 +1,43 @@
+import zstackwoodpecker.test_state as ts_header
+TestAction = ts_header.TestAction
+def path():
+
+    return dict(initial_formation="template5", path_list=[
+        [TestAction.create_vm_by_image, "iso-image", "iso", "vm1"],
+        [TestAction.change_vm_ha, "vm1"],
+        [TestAction.create_volume, "volume1", "=scsi,thin"],
+        [TestAction.attach_volume, "vm1", "volume1"],
+        [TestAction.create_volume_backup, "volume1", "backup1"],
+        [TestAction.create_mini_vm, "vm2", 'data_volume=false', 'cpu=2', 'memory=2', 'provisiong=thin'],
+        [TestAction.create_volume, "volume2", "=scsi,thin"],
+        [TestAction.attach_volume, "vm2", "volume2"],
+        [TestAction.create_mini_vm, "vm3", 'data_volume=false', 'cpu=random', 'memory=2', 'network=random',
+         'provisiong=thin'],
+        [TestAction.delete_volume, "volume2"],
+        [TestAction.add_image, "image1", 'root', "http://172.20.1.28/mirror/diskimages/centos_vdbench.qcow2"],
+        [TestAction.create_volume_backup, "vm2-root", "backup2"],
+        [TestAction.stop_vm, "vm2"],
+        [TestAction.use_volume_backup, "backup2"],
+        [TestAction.start_vm, "vm2"],
+        [TestAction.delete_image, "image1"],
+        [TestAction.expunge_image, "image1"],
+        [TestAction.create_volume_backup, "volume1", "backup3"],
+        [TestAction.reboot_vm, "vm1"],
+        [TestAction.reboot_vm, "vm2"],
+        [TestAction.reboot_vm, "vm3"],
+        [TestAction.create_volume, "volume3", "=scsi,thin"],
+        [TestAction.attach_volume, "vm3", "volume3"],
+        [TestAction.delete_volume, "volume3"],
+        [TestAction.create_mini_vm, "vm4", 'data_volume=false', 'cpu=random', 'memory=2', 'provisiong=thin'],
+        [TestAction.add_image, "image2", 'root', "http://172.20.1.28/mirror/diskimages/centos_vdbench.qcow2"],
+        [TestAction.expunge_volume, "volume3"],
+        [TestAction.delete_vm, "vm4"],
+        [TestAction.recover_vm, "vm4"],
+        [TestAction.create_volume_backup, "volume1", "backup4"],
+        [TestAction.delete_image, "image2"],
+        [TestAction.recover_image, "image2"],
+        [TestAction.create_volume_backup, "vm1-root", "backup5"],
+        [TestAction.delete_volume_backup, "backup5"],
+        [TestAction.add_image, "image3", 'root', "http://172.20.1.28/mirror/diskimages/centos_vdbench.qcow2"],
+        [TestAction.detach_volume, "volum1"]])
+
