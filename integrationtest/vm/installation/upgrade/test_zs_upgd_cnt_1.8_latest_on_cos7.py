b454524c0892769352ec4ca05336fee4f6a16d36
Ye Tian <ye.tian@zstack.io>
2019-05-16 14:07:51 +0800

    fix the no delete data_volume

--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.8_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_1.8_latest_on_cos7.py
@@ -22,7 +22,7 @@ vm_inv = None
 
 
 def test():
-    global vm_inv
+    global vm_inv, data_volume_uuid
     test_util.test_dsc('Create test vm to test zstack upgrade by -u.')
     image_name = os.environ.get('imageName_i_c7_z_1.8')
     data_image_name = os.environ.get('data_volume_template')
@@ -135,12 +135,16 @@ def test():
     test_stub.check_installation(vm_ip, tmp_file)
 
     os.system('rm -f %s' % tmp_file)
-    vol_ops.detach_volume(data_volume_inv.uuid)
+    vol_ops.detach_volume(data_volume_uuid)
+    vol_ops.delete_volume(data_volume_uuid)
+    vol_ops.expunge_volume(data_volume_uuid)
     test_stub.destroy_vm_scenario(vm_inv.uuid)
     test_util.test_pass('''ZStack upgrade 1.9','1.10','2.0.0','2.1.0','2.2.0','2.3.0','2.3.1','2.3.2','2.4.0','2.5.0','2.6.0','3.0.0','3.0.1','3.1.0','3.1.3','3.2.0','3.3.0'Test Success''')
 
 #Will be called only if exception happens in test().
 def error_cleanup():
-    global vm_inv
+    global vm_inv,data_volume_uuid
     os.system('rm -f %s' % tmp_file)
+    vol_ops.delete_volume(data_volume_uuid)
+    vol_ops.expunge_volume(data_volume_uuid)
     test_stub.destroy_vm_scenario(vm_inv.uuid)
