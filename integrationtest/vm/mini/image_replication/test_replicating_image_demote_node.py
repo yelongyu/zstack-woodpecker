7fda043b0c784eb9bb0855cb4b63d474ed7c5195
Lijin Xiong <legion.xiong@gmail.com>
2019-06-20 17:34:01 +0800

    add image replication tests

--- integrationtest/vm/mini/image_replication/test_replicating_image_demote_node.py
+++ integrationtest/vm/mini/image_replication/test_replicating_image_demote_node.py
@@ -1,6 +1,8 @@
 '''
 
 New Integration test for image replication.
+Check Image Replication after BS recovering from powered off,
+Target BS would be powered off during replicating new image 
 
 @author: Legion
 '''
@@ -8,40 +10,42 @@ New Integration test for image replication.
 import os
 import time
 import random
+import zstacklib.utils.ssh as ssh
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
-import zstackwoodpecker.operations.config_operations as conf_ops
 
-image_name = 'iso-image-replication-test-' + time.strftime('%y%m%d%H%M%S', time.localtime())
+
+image_name = 'image-replication-test-' + time.strftime('%y%m%d%H%M%S', time.localtime())
 test_stub = test_lib.lib_get_test_stub()
 img_repl = test_stub.ImageReplication()
 
 
 def test():
     os.environ['ZSTACK_BUILT_IN_HTTP_SERVER_IP'] = os.getenv('zstackHaVip')
-    conf_ops.change_global_config('imagestore', 'cleanOnExpunge', 'true')
     bs_list = img_repl.get_bs_list()
     bs = random.choice(bs_list)
+    bs_list.remove(bs)
+    bs2 = bs_list[0]
 
-    img_repl.add_image(image_name, bs_uuid=bs.uuid, img_format='iso')
+    img_repl.add_image(image_name, bs_uuid=bs.uuid, url=os.getenv('imageUrl_raw'))
+    img_repl.wait_for_downloading(image_name)
+    cmd = 'zsha2 demote'
+    try:
+        ssh.execute(cmd, bs.hostname, 'root', 'password')
+    except:
+        ssh.execute(cmd, bs2.hostname, 'root', 'password')
+
     img_repl.wait_for_image_replicated(image_name)
-    img_repl.check_image_data(image_name)
-
-    img_repl.delete_image()
-    img_repl.expunge_image()
-
-    time.sleep(30)
-
-    img_repl.check_image_data(image_name, expunged=True)
-
-    test_util.test_pass('Global config cleanOnExpunge Test Success')
+    img_repl.create_vm(image_name)
+    test_util.test_pass('Demote node during Image Replicating Test Success')
+    img_repl.clean_on_expunge()
 
 
 def env_recover():
+    img_repl.delete_image()
+    img_repl.expunge_image()
     img_repl.reclaim_space_from_bs()
     try:
-        img_repl.delete_image()
-        img_repl.expunge_image()
         img_repl.vm.destroy()
     except:
         pass
--- integrationtest/vm/mini/image_replication/test_replicating_image_expunge_image.py
+++ integrationtest/vm/mini/image_replication/test_replicating_image_expunge_image.py
@@ -1,6 +1,8 @@
 '''
 
 New Integration test for image replication.
+Check Image Replication after BS recovering from powered off,
+Target BS would be powered off during replicating new image 
 
 @author: Legion
 '''
@@ -10,22 +12,20 @@ import time
 import random
 import zstackwoodpecker.test_util as test_util
 import zstackwoodpecker.test_lib as test_lib
-import zstackwoodpecker.operations.config_operations as conf_ops
 
-image_name = 'iso-image-replication-test-' + time.strftime('%y%m%d%H%M%S', time.localtime())
+
+image_name = 'image-replication-test-' + time.strftime('%y%m%d%H%M%S', time.localtime())
 test_stub = test_lib.lib_get_test_stub()
 img_repl = test_stub.ImageReplication()
 
 
 def test():
     os.environ['ZSTACK_BUILT_IN_HTTP_SERVER_IP'] = os.getenv('zstackHaVip')
-    conf_ops.change_global_config('imagestore', 'cleanOnExpunge', 'true')
     bs_list = img_repl.get_bs_list()
     bs = random.choice(bs_list)
 
-    img_repl.add_image(image_name, bs_uuid=bs.uuid, img_format='iso')
-    img_repl.wait_for_image_replicated(image_name)
-    img_repl.check_image_data(image_name)
+    img_repl.add_image(image_name, bs_uuid=bs.uuid, url=os.getenv('imageUrl_raw'))
+    img_repl.wait_for_downloading(image_name)
 
     img_repl.delete_image()
     img_repl.expunge_image()
@@ -33,15 +33,13 @@ def test():
     time.sleep(30)
 
     img_repl.check_image_data(image_name, expunged=True)
-
-    test_util.test_pass('Global config cleanOnExpunge Test Success')
+    test_util.test_pass('Create VM during Image Replicating Test Success')
+    img_repl.clean_on_expunge()
 
 
 def env_recover():
     img_repl.reclaim_space_from_bs()
     try:
-        img_repl.delete_image()
-        img_repl.expunge_image()
         img_repl.vm.destroy()
     except:
         pass
