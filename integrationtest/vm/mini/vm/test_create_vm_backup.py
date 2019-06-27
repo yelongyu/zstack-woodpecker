ecf937bcf9163441ac4e88e59e27ac47a84a07d3
zhaohao.chen <cartkbackville@gmail.com>
2019-06-27 10:30:40 +0800

    fix case mini/vm/test_create_vm_backup

--- integrationtest/vm/mini/vm/test_create_vm_backup.py
+++ integrationtest/vm/mini/vm/test_create_vm_backup.py
@@ -13,12 +13,10 @@ import time
 import os
 import random
 
-vm = None
-def test():
-    global vm
+test_obj_dict = test_state.TestStateDict()
 
+def test():
     VM_CPU= 8
-    #VM_MEM = 17179869184 #16GB 
     VM_MEM = 2147483648 #2GB 
 
     vm_creation_option = test_util.VmOption()
@@ -27,36 +25,53 @@ def test():
     image_uuid = test_lib.lib_get_image_by_name(image_name).uuid
     l3_name = os.environ.get('l3VlanNetworkName1')
     l3_net_uuid = test_lib.lib_get_l3_by_name(l3_name).uuid
-    
+    #1.create VM with thick provision root volume
     vm_creation_option.set_l3_uuids([l3_net_uuid])
     vm_creation_option.set_image_uuid(image_uuid)
     vm_creation_option.set_cpu_num(VM_CPU)
     vm_creation_option.set_memory_size(VM_MEM)
-    vm_creation_option.set_name('MINI_Backup_test')
-    vm = test_vm_header.ZstackTestVm()
-    vm.set_creation_option(vm_creation_option)
-    vm.create()
-    vm_root_volume_uuid = vm.vm.rootVolumeUuid
+    vm_creation_option.set_name('MINI_Backup_test_thick')
+    vm_thick = test_vm_header.ZstackTestVm()
+    vm_thick.set_creation_option(vm_creation_option)
+    vm_thick.create()
+    vm_thick.check()
+    test_obj_dict.add_vm(vm_thick)
+    vm_thick_root_volume_uuid = vm_thick.vm.rootVolumeUuid
+
+    #2.create VM with thin provision root volume
+    vm_creation_option.set_rootVolume_systemTags(["volumeProvisioningStrategy::ThinProvisioning"])
+    vm_thin = test_vm_header.ZstackTestVm()
+    vm_thin.set_creation_option(vm_creation_option)
+    vm_creation_option.set_name('MINI_Backup_test_thin')
+    vm_thin.create()
+    vm_thin.check()
+    test_obj_dict.add_vm(vm_thin)
+    vm_thin_root_volume_uuid = vm_thin.vm.rootVolumeUuid
+    
+    #3.backup thick/thin vm test
     bs_uuid = res_ops.query_resource(res_ops.BACKUP_STORAGE)[0].uuid
     backup_creation_option.set_backupStorage_uuid(bs_uuid)
-    backup_creation_option.set_volume_uuid(vm_root_volume_uuid)
-    backup_name = "mini_vm_backup%s" % time.time()
-    backup_creation_option.set_name(backup_name)
-    cond = res_ops.gen_query_conditions('name', '=', backup_name)
-    #Create VM rootvolume backup
     try:
-        invs = vm_ops.create_vm_backup(backup_creation_option)
+        backup_creation_option.set_volume_uuid(vm_thick_root_volume_uuid)
+        backup_name_thick = "mini_thick_vm_backup%s" % time.time()
+        backup_name_thin = "mini_thin_vm_backup%s" % time.time()
+        backup_creation_option.set_name(backup_name_thick)
+        backup_thick = vm_ops.create_vm_backup(backup_creation_option)
+        backup_creation_option.set_name(backup_name_thin)
+        backup_creation_option.set_volume_uuid(vm_thin_root_volume_uuid)
+        backup_thin = vm_ops.create_vm_backup(backup_creation_option)
+        cond_thick = res_ops.gen_query_conditions('name', '=', backup_name_thick)
+        cond_thin = res_ops.gen_query_conditions('name', '=', backup_name_thin)
     except:
-        test_util.test_fail("Fail to create VM[%s] backup" % vm.vm.uuid)
-    if not invs:
-        test_util.test_fail("Fail to create VM[%s] backup" % vm.vm.uuid)
-    elif not res_ops.query_resource(res_ops.VOLUME_BACKUP, cond):
-        test_util.test_fail("Fail to create VM[%s] backup" % vm.vm.uuid)
+        test_util.test_fail("Fail to create VM backup")
+    if not res_ops.query_resource(res_ops.VOLUME_BACKUP, cond_thick):
+        test_util.test_fail("Fail to create thick VM[%s] backup" % vm_thick.vm.uuid)
+    elif not res_ops.query_resource(res_ops.VOLUME_BACKUP, cond_thin):
+        test_util.test_fail("Fail to create thin VM[%s] backup" % vm_thin.vm.uuid)
     else:
-        test_util.test_pass("Create VM[%s] backup Test Pass" % vm.vm.uuid)
+        test_util.test_pass("Create thick&thin VM backup Test Pass")
 
 def error_cleanup():
-    global vm
     global test_obj_dict
     test_lib.lib_error_cleanup(test_obj_dict)
 
