a135d33c47aa70fefe218b39c7fdff6e99fa9819
chenyuan.xu <chenyuan.xu@zstack.io>
2019-07-07 20:05:28 +0800

    fix gen serial script for vm

--- zstackwoodpecker/zstackwoodpecker/test_lib.py
+++ zstackwoodpecker/zstackwoodpecker/test_lib.py
@@ -3454,7 +3454,7 @@ fi
         try:
             ssh.scp_file(script_file.name, "/tmp/serial_log_gen.sh", host.managementIp, 'root', 'password', port=22)
         except:
-            ssh.scp_file(script_file.name, "/tmp/serial_log_gen.sh", host.managementIp, 'root', 'password', port=2222
+            ssh.scp_file(script_file.name, "/tmp/serial_log_gen.sh", host.managementIp, 'root', 'password', port=2222)
         os.unlink(script_file.name)
         _rsp = lib_execute_ssh_cmd(host.managementIp, host.username, os.environ.get('hostPassword'), cmd)
         if _rsp:
