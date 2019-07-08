f934c0581323e34e7eb4a21cab588493bc49e759
Lijin Xiong <legion.xiong@gmail.com>
2019-07-08 13:46:58 +0800

    update sce ops

--- zstackwoodpecker/zstackwoodpecker/operations/scenario_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/scenario_operations.py
@@ -2621,8 +2621,9 @@ def deploy_scenario(scenario_config, scenario_file, deploy_config):
                     cmd = "bash %s" % (output)
                     ssh.execute(cmd, vm_ip, 'root', 'password', True, 22)
                     print "Mini Host need install drbd"
-                    cmd = "yum install kmod-drbd84 ipmitool OpenIPMI-modalias MegaCli lsof drbd84-utils -y;depmod -a && modprobe drbd;systemctl enable drbd;systemctl start drbd"
-                    ssh.execute(cmd, vm_ip, 'root', 'password', True, 22)
+                    # Uncomment below 2 lines as a temporary workaround if "http://jira.zstack.io/browse/MINI-904" was reproduced again
+#                     cmd = "yum install kmod-drbd84 ipmitool OpenIPMI-modalias MegaCli lsof drbd84-utils -y;depmod -a && modprobe drbd;systemctl enable drbd;systemctl start drbd"
+#                     ssh.execute(cmd, vm_ip, 'root', 'password', True, 22)
 
                 if vm.dataDiskOfferingUuid__:
                     volume_option = test_util.VolumeOption()
