8c65132b5d3c8360e0bb3d4bb7f8c36e5663eb99
moyu <w13880213920@live.com>
2019-07-08 10:05:06 +0800

    add mini reobot paths

--- integrationtest/vm/mini/paths/path107.py
+++ integrationtest/vm/mini/paths/path107.py
@@ -5,7 +5,7 @@ def path():
 
     return dict(repeat=1, initial_formation="template5", path_list=[
         [TestAction.create_mini_vm, "vm1", 'data_volume=false', 'cpu=2', 'memory=2', 'provisiong=thick'],
-        [TestAction.create_volume, "volume1", "flag=scsi,thin,large,nocheck"],
+        [TestAction.create_volume, "volume1", "size=random", "flag=scsi,thin,large,nocheck"],
         [TestAction.attach_volume, "vm1", "volume1"],
         [TestAction.run_workloads, "target_vms=vm1", "FIO", "fio -filename=/dev/sda -direct=1 -iodepth 32 -thread -rw=randrw -rwmixread=70 -ioengine=libaio -bs=4k -size=90% -numjobs=8 -runtime=3600 -group_reporting -name=fio_test.txt", 60],
         [TestAction.change_vm_ha, "vm1"]] + [[TestAction.idel, "600"], [TestAction.reboot_host, "all", "soft_crash"]]*5000)
