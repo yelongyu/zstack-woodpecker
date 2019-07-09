dc4e9bcccd4d1ff0ec90f4a98abac09ad26275a1
pengtao.zhang <pengtao.zhang@zstack.io>
2019-07-09 17:08:57 +0800

    update suite_setup.py

--- integrationtest/vm/vpc/suite_setup.py
+++ integrationtest/vm/vpc/suite_setup.py
@@ -58,6 +58,9 @@ def test():
     conf_ops.change_global_config("applianceVm", "agent.deployOnStart", 'true')
     if os.path.exists(EXTRA_SUITE_SETUP_SCRIPT):
         os.system("bash %s" % EXTRA_SUITE_SETUP_SCRIPT)
+    child1 = os.popen('ifconfig eth1 | grep 192.168 | awk \'{print $2}\'')
+    management_ip = child1.read()
+    os.system('zstack-ctl change_ip --ip %s'% management_ip)
     deploy_operations.deploy_initial_database(test_lib.deploy_config, test_lib.all_scenario_config, test_lib.scenario_file)
     for host in hosts:
         os.system("bash %s %s" % (EXTRA_HOST_SETUP_SCRIPT, host.managementIp_))
