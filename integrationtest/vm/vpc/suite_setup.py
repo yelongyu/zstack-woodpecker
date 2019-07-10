422cb6ad9ad0a83ab732a618857eda78deb7ff3a
pengtao.zhang <pengtao.zhang@zstack.io>
2019-07-10 12:32:10 +0800

    modify nic name

--- integrationtest/vm/vpc/suite_setup.py
+++ integrationtest/vm/vpc/suite_setup.py
@@ -59,7 +59,7 @@ def test():
     if os.path.exists(EXTRA_SUITE_SETUP_SCRIPT):
         os.system("bash %s" % EXTRA_SUITE_SETUP_SCRIPT)
     deploy_operations.deploy_initial_database(test_lib.deploy_config, test_lib.all_scenario_config, test_lib.scenario_file)
-    child1 = os.popen('ifconfig eth1 | grep 192.168 | awk \'{print $2}\'')
+    child1 = os.popen('ifconfig br_eth1 | grep 192.168 | awk \'{print $2}\'')
     management_ip = child1.read()
     os.system('zstack-ctl change_ip --ip %s'% management_ip)
     os.system('zstack-ctl stop')
