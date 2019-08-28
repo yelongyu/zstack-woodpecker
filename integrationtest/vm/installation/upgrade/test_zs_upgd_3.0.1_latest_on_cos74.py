d67b8ad2becc6cb59138d632f08046445735752d
Ye Tian <ye.tian@zstack.io>
2019-08-28 09:52:26 +0800

    fix start node timeout

--- integrationtest/vm/installation/upgrade/test_zs_upgd_3.0.1_latest_on_cos74.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_3.0.1_latest_on_cos74.py
@@ -41,6 +41,7 @@ def test():
     test_util.test_logger('Update MN IP')
     test_stub.update_mn_hostname(vm_ip, tmp_file)
     test_stub.update_mn_ip(vm_ip, tmp_file)
+    test_stub.start_node(vm_ip, tmp_file)
     test_stub.start_mn(vm_ip, tmp_file)
     test_stub.check_installation(vm_ip, tmp_file)
 
@@ -49,6 +50,7 @@ def test():
     #test_stub.updatei_21_iso(vm_ip, tmp_file, iso_21_path, upgrade_script_path)
     test_stub.upgrade_zstack(vm_ip, zstack_latest_path, tmp_file) 
     test_stub.check_zstack_version(vm_ip, tmp_file, zstack_latest_version)
+    test_stub.start_node(vm_ip, tmp_file)
     test_stub.start_mn(vm_ip, tmp_file)
     test_stub.check_mn_running(vm_ip, tmp_file)
     test_stub.check_installation(vm_ip, tmp_file)
