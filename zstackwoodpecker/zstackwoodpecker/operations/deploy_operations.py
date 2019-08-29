5d93a6c887cc23554ef4b37c7bbddf1221661bb6
Lijin Xiong <legion.xiong@gmail.com>
2019-08-29 16:53:22 +0800

    add migrate network for mini cluster

--- zstackwoodpecker/zstackwoodpecker/operations/deploy_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/deploy_operations.py
@@ -1513,7 +1513,8 @@ def add_cluster(scenarioConfig, scenarioFile, deployConfig, session_uuid, cluste
                     action.sessionUuid = session_uuid
                     action.name = generate_dup_name(generate_dup_name(cluster.name_, zone_ref, 'z'), cluster_ref, 'c')
                     action.description = generate_dup_name(generate_dup_name(cluster.description__, zone_ref, 'z'), cluster_ref, 'c')
-        
+                    if cluster.description_ == 'MiniCluster':
+                        action.systemTags = ["cluster::migrate::network::cidr::99.99.99.0/24"]
                     action.hypervisorType = cluster.hypervisorType_
                     action.zoneUuid = zinv.uuid
                     thread = threading.Thread(target=_add_cluster, args=(action, zone_ref, cluster, cluster_ref, ))
