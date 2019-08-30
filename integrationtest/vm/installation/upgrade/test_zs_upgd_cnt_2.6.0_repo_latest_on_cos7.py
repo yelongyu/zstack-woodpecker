57e50d6ed332a86512795f63e8c60b409a689999
Ye Tian <ye.tian@zstack.io>
2019-08-30 10:42:35 +0800

    fix start mn timeout

--- integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_2.6.0_repo_latest_on_cos7.py
+++ integrationtest/vm/installation/upgrade/test_zs_upgd_cnt_2.6.0_repo_latest_on_cos7.py
@@ -40,6 +40,7 @@ def test():
     test_stub.update_mn_hostname(vm_ip, tmp_file)
     test_stub.update_mn_ip(vm_ip, tmp_file)
     #test_stub.reset_rabbitmq(vm_ip, tmp_file)
+    test_stub.start_node(vm_ip, tmp_file)
     test_stub.start_mn(vm_ip, tmp_file)
     test_stub.check_installation(vm_ip, tmp_file)
 
