9bac84f3f70e03457e78cf6021edbd43af86b231
Chao Zhou <chao.zhou@zstack.io>
2019-09-05 16:00:40 +0800

    judge mini deploy

--- zstackwoodpecker/zstackwoodpecker/operations/deploy_operations.py
+++ zstackwoodpecker/zstackwoodpecker/operations/deploy_operations.py
@@ -1579,12 +1579,13 @@ def add_cluster(scenarioConfig, scenarioFile, deployConfig, session_uuid, cluste
         else:
             _deploy_cluster(zone)
     wait_for_thread_done()
-    vm_ip_list = get_vm_ip_from_scenariofile(scenarioFile)
-    for ip in vm_ip_list:
-    cmd = "wget http://172.20.1.27/mirror/mini/MegaCli64 -O /opt/MegaRAID/MegaCli/MegaCli64;chmod a+x /opt/MegaRAID/MegaCli/MegaCli64"
-    ssh.execute(cmd, ip, 'root', 'password')
-    cmd = "wget http://172.20.1.27/mirror/mini/smartctl -O /usr/sbin/smartctl;chmod a+x /usr/sbin/smartctl"
-    ssh.execute(cmd, ip, 'root', 'password')
+    if xmlobject.has_element(deployConfig, 'backupStorages.miniBackupStorage'):
+        vm_ip_list = get_vm_ip_from_scenariofile(scenarioFile)
+        for ip in vm_ip_list:
+            cmd = "wget http://172.20.1.27/mirror/mini/MegaCli64 -O /opt/MegaRAID/MegaCli/MegaCli64;chmod a+x /opt/MegaRAID/MegaCli/MegaCli64"
+            ssh.execute(cmd, ip, 'root', 'password')
+            cmd = "wget http://172.20.1.27/mirror/mini/smartctl -O /usr/sbin/smartctl;chmod a+x /usr/sbin/smartctl"
+            ssh.execute(cmd, ip, 'root', 'password')
 
     for zone in xmlobject.safe_list(deployConfig.zones.zone):
         if zone_name and zone_name != zone.name_:
